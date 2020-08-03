b12e5c7cedcf2df3af133ee9a1b418482c511536
Anthony Hallak <ahall012@ucr.edu>
2020-08-03 15:39:46 -0700

    Added story for Bold, ran yarn storybook and it worked. I did need to change sotrybook script in package.json

---
+++ src/components/Bold/Bold.stories.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom'
+
+import Bold from '.';
+
+export default { title: 'Components/Bold' };
+
+const toBoldString = "test_asdASD_123_=-0\"\'";
+const toBoldJSX = <p>this should look bold</p>;
+const toBoldJSXArr = 
+    <div>
+        <p>test_jsxArr</p>
+        <p>testingJSX[]</p>
+    </div>;
+
+export function BoldText() {
+    return (
+        <MemoryRouter initialEntries={['/']}>
+            <Bold 
+                children={toBoldString}
+            />
+            <Bold 
+                children={toBoldJSX}
+            />
+            <Bold 
+                children={toBoldJSXArr}
+            />
+            <p>this is not bold for reference</p>
+        </MemoryRouter>
+    );
+}
