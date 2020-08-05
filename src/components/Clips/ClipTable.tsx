d0fde96589245c3d9c23e2056c5c75e4e03298f9
Stanley J. Muzhuthettu <smuzh001@ucr.edu>
2020-08-05 14:51:17 -0700

    added row select/deselect with additional logic

--- src/components/Clips/ClipTable.tsx
+++ src/components/Clips/ClipTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,12 +7,18 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Checkbox from '@material-ui/core/Checkbox';
+import theme from 'theme';
 
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
+const useStyles = makeStyles(() => ({
+    selectedRow: {
+        backgroundColor: '#808080',
     },
-});
+    row: {
+        backgroundColor: '#000000',
+    },
+}));
 
 interface ClipData {
     timeStamp: string;
@@ -54,8 +60,7 @@ const tempRows = [
         'Session Title',
         ['History', 'Philosophy', 'Prop 60']
     ),
-    createTimeStamp	Clip Title	Tags/Subject	Options
-    00:40-1:53	Question title 1ata(
+    createData(
         '00:40-1:53',
         '1 min, 23 secs',
         'Question title 4',
@@ -70,7 +75,6 @@ const tempRows = [
         ['History', 'Philosophy', 'Prop 60']
     ),
 ];
-
 // function descendingComparator<T>(a: T, b: T, orderBy: keyof T){
 //     if (b[orderBy] < a[orderBy]){
 //         return -1;
@@ -83,7 +87,7 @@ const tempRows = [
 
 // type Order = 'asc' | 'desc';
 
-// interface EnhancedTableProps {
+// interface EnhancedTableProps {414
 //     classes: ReturnType<typeof useStyles>;
 //     numSelected: number;
 //     onRequestSort: (event: React.MouseEvent<unknown>, property: keyof ClipData) => void;
@@ -95,35 +99,88 @@ const tempRows = [
 
 export default function ClipTable() {
     const classes = useStyles();
+    const [selected, setSelected] = React.useState<string[]>([]);
 
-    return (
+    useEffect(() => {
+        console.log(selected);
+    }, [selected]);
+
+    const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+        // adds and pops items that are clicked/unclicked to selected list state
+        const selectedIndex = selected.indexOf(name);
+        let newSelected: string[] = [];
+
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1)
+            );
+        }
+
+        setSelected(newSelected);
+    };
+    // returns boolean if title has been selected
+    const isSelected = (title: string) => selected.indexOf(title) !== -1;
+
+    const matches = useMediaQuery('(min-width:300px)');
+
+    return !matches ? (
+        <h1>smaller than min width</h1>
+    ) : (
         <TableContainer component={Paper}>
             <Table className={classes.table} aria-label='clip table'>
                 <TableHead>
                     <TableRow>
-                        <TableCell>TimeStamp</TableCell>
-                        <TableCell>Clip Title</TableCell>
-                        <TableCell>Tags/Subject</TableCell>
-                        <TableCell>Options</TableCell>
+                        <TableCell align='center'>Clips</TableCell>
+                        <TableCell align='left'>Title</TableCell>
+                        <TableCell align='center'>Options</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {tempRows.map((row) => (
-                        <TableRow key={row.title}>
-                            <TableCell component='th' scope='row'>
-                                {row.timeStamp}
-                            </TableCell>
-                            <TableCell component='th' scope='row'>
-                                {row.title}
-                            </TableCell>
-                            <TableCell component='th' scope='row'>
-                                {!row.tags ? row.tags : row.tags[0]}
-                            </TableCell>
-                            <TableCell component='th' scope='row'>
-                                <button type='button'>options</button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
+                    {tempRows.map((row) => {
+                        const isRowSelected = isSelected(row.title);
+                        return (
+                            <TableRow
+                                hover
+                                key={row.title}
+                                onClick={(event) => {
+                                    handleClick(event, row.title);
+                                }}
+                                selected={isRowSelected}
+                            >
+                                <TableCell padding='checkbox'>
+                                    <Checkbox
+                                        checked={isRowSelected}
+                                        style={{
+                                            visibility: isRowSelected
+                                                ? 'visible'
+                                                : 'hidden',
+                                        }}
+                                    />
+                                </TableCell>
+                                <TableCell
+                                    component='th'
+                                    scope='row'
+                                    align='left'
+                                >
+                                    {row.title}
+                                </TableCell>
+                                <TableCell
+                                    component='th'
+                                    scope='row'
+                                    align='center'
+                                >
+                                    <button type='button'>options</button>
+                                </TableCell>
+                            </TableRow>
+                        );
+                    })}
                 </TableBody>
             </Table>
         </TableContainer>
