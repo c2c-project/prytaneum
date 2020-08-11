6bccea9e9ae5abec0231c8d95d8e3c17ff22bb5b
Johan Guzman <jguzm022@ucr.edu>
2020-08-11 15:34:08 -0700

    Adds include

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,5 @@
 module.exports = {
     presets: ['@babel/preset-typescript', '@babel/preset-react'],
     plugins: [['babel-plugin-typescript-to-proptypes', { comments: true }]],
+    include: ['src'],
 };
