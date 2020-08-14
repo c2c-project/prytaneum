5af3d2d360eb4ee7fd9e12df62c9936f9bbad964
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-08-13 23:09:27 -0700

    dev: optimized storybook aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,14 +9,7 @@ module.exports = {
     ],
     webpackFinal: (config) => {
         config.resolve.alias = {
-            'hooks/useEndpoint': path.resolve(
-                __dirname,
-                '../src/hooks/__mocks__/useEndpoint'
-            ),
-            'hooks/useSocketio': path.resolve(
-                __dirname,
-                '../src/hooks/__mocks__/useSocketio'
-            ),
+            hooks: path.resolve(__dirname, '../src/hooks/__mocks__/'),
         };
         return config;
     },
