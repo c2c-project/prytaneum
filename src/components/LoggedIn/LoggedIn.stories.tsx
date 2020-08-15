c46e734ab0cb18a8b7a250ebe33dabf211a3db72
Anthony Hallak <ahall012@ucr.edu>
2020-08-14 18:09:26 -0700

    This commit is to save progress, LoggedIn.stories.tsx is not working properly

--- src/components/LoggedIn/LoggedIn.stories.tsx
+++ src/components/LoggedIn/LoggedIn.stories.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core';
+import { MemoryRouter, Route } from 'react-router-dom';
 
 import Component from './LoggedIn';
+import theme from 'theme';
 
 export default {
     title: 'Components/LoggedIn',
     component: Component,
 };
 
-const children = <h1>testing if LoggedIn renders this JSX element</h1>;
+const path = '/';
+let _location: any;
 
 export function LoggedIn_JwtTrue() {
     return (
-        <Component jwt={true}>
-            {children}
-        </Component>
+        <ThemeProvider theme={theme}>
+            <MemoryRouter initialEntries={['/']}>
+                <Route path={path}>
+                    <Component jwt={true}>
+                        <h1>this should be rendered</h1>
+                    </Component>
+                </Route>
+                <Route
+                    path='*'
+                    render={({ location }) => {
+                        _location = location;
+                        return null;
+                    }}
+                />
+            </MemoryRouter>
+        </ThemeProvider>
     );
-};
+}
 
 export function LoggedIn_JwtFalse() {
     return (
-        <Component jwt={false}>
-            {children}
-        </Component>
+        <ThemeProvider theme={theme}>
+            <MemoryRouter initialEntries={['/']}>
+                <Route path={path}>
+                    <Component jwt={false}>
+                        <h1>This should not be rendered</h1>
+                    </Component>
+                </Route>
+                <Route
+                    path='*'
+                    render={({ location }) => {
+                        _location = location;
+                        return null;
+                    }}
+                />
+            </MemoryRouter>
+        </ThemeProvider>
     );
-};
+}
