4b2b0d11dfce2a978e015744f6e40427c25a929b
Johan Guzman <jguzm022@ucr.edu>
2020-08-21 09:03:54 -0700

    Comments out .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,5 @@
-module.exports = {
-    presets: ['@babel/preset-typescript', '@babel/preset-react'],
-    plugins: [['babel-plugin-typescript-to-proptypes', { comments: true }]],
-    include: ['src'],
-};
+// module.exports = {
+//     presets: ['@babel/preset-typescript', '@babel/preset-react'],
+//     plugins: [['babel-plugin-typescript-to-proptypes', { comments: true }]],
+//     include: ['src'],
+// };
