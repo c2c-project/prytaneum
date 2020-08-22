bee1f7a2e45764293d7ab13c32b53cc7a59c74fb
Carolyn Kong <carolyn_kong@outlook.com>
2020-08-21 17:21:01 -0700

    Loader and Paper tests and stories done

---
+++ src/components/Loader/Loader.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core';
+import theme from 'theme';
+
+import Component from './Loader';
+
+export default {
+    title: 'Components/Loader',
+    component: Component
+};
+
+export function Loader() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Component/>
+        </ThemeProvider>
+    );
+}
