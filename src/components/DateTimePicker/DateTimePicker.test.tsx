1199e8d0b7b6e4053395edd7e5e6caab73720054
Anthony Hallak <ahall012@ucr.edu>
2020-08-22 15:56:12 -0700

    Fixes DateTimePicker and adds a test

---
+++ src/components/DateTimePicker/DateTimePicker.test.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+import Picker from './DateTimePicker';
+import { ThemeProvider } from '@material-ui/core';
+
+import theme from 'theme';
+
+describe('DateTimePicker', function () {
+    let container: HTMLDivElement | null = null;
+
+    beforeEach(() => {
+        // setup a DOM element as a render target
+        container = document.createElement('div');
+        document.body.appendChild(container);
+    });
+
+    afterEach(() => {
+        // cleanup on exiting
+        if (container) {
+            unmountComponentAtNode(container);
+            container.remove();
+        }
+        container = null;
+        jest.restoreAllMocks();
+    });
+
+    // eslint-disable-next-line jest/expect-expect
+    it('should render', () => {
+        ReactTestUtils.act(() => {
+            render(
+                <Picker id='test' value='test' onChange={() => {}}></Picker>,
+                container
+            );
+        });
+        expect(document.getElementById('test')).toBeTruthy();
+    });
+});
