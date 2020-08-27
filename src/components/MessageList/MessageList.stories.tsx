5d0ce04b07c1b207122f14d9acc171e65d5c80b1
Anthony Hallak <ahall012@ucr.edu>
2020-08-26 20:07:54 -0700

    Fixes messagelist story and renames docs_design_templates to just docs/

--- src/components/MessageList/MessageList.stories.tsx
+++ src/components/MessageList/MessageList.stories.tsx
@@ -14,34 +14,34 @@ export function MessageList() {
     return (
         <ThemeProvider theme={theme}>
             <Component>
-                    <MessageListItem button={false} hidden={false}>
-                        <h1>item 1</h1>
-                        <h2>item 2</h2>
-                        <em>item 3</em>
-                        <i>item 4</i>
-                        <p>This is clickable but doesnt do anything</p>
-                    </MessageListItem>
-                    <MessageListItem button={true} hidden={false}>
-                        <h1>item 1</h1>
-                        <h2>item 2</h2>
-                        <em>item 3</em>
-                        <i>item 4</i>
-                        <p>This is clickable but doesnt do anything</p>
-                    </MessageListItem>
-                    <MessageListItem button={true} hidden={false}>
-                        <h1>item 1</h1>
-                        <h2>item 2</h2>
-                        <em>item 3</em>
-                        <i>item 4</i>
-                        <p>This is clickable but doesnt do anything</p>
-                    </MessageListItem>
-                    <MessageListItem button={true} hidden={false}>
-                        <h1>item 1</h1>
-                        <h2>item 2</h2>
-                        <em>item 3</em>
-                        <i>item 4</i>
-                        <p>This is clickable but doesnt do anything</p>
-                    </MessageListItem>
+                <MessageListItem button={false} hidden={false}>
+                    <h1>item 1</h1>
+                    <h2>item 2</h2>
+                    <em>item 3</em>
+                    <i>item 4</i>
+                    <p>This is clickable but doesnt do anything</p>
+                </MessageListItem>
+                <MessageListItem button={true} hidden={false}>
+                    <h1>item 1</h1>
+                    <h2>item 2</h2>
+                    <em>item 3</em>
+                    <i>item 4</i>
+                    <p>This is clickable but doesnt do anything</p>
+                </MessageListItem>
+                <MessageListItem button={true} hidden={false}>
+                    <h1>item 1</h1>
+                    <h2>item 2</h2>
+                    <em>item 3</em>
+                    <i>item 4</i>
+                    <p>This is clickable but doesnt do anything</p>
+                </MessageListItem>
+                <MessageListItem button={true} hidden={false}>
+                    <h1>item 1</h1>
+                    <h2>item 2</h2>
+                    <em>item 3</em>
+                    <i>item 4</i>
+                    <p>This is clickable but doesnt do anything</p>
+                </MessageListItem>
             </Component>
         </ThemeProvider>
     );
