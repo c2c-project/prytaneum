5c8732407c29a7ea3e7aabb407cace9079337ae9
Johan Guzman <jguzm022@ucr.edu>
2020-08-31 15:34:10 -0700

    Removes unncessary tests

--- src/domains/FeedbackPortal/ReportHistory/ReportHistory.test.tsx
+++ src/domains/FeedbackPortal/ReportHistory/ReportHistory.test.tsx
@@ -9,6 +9,7 @@ import * as API from '../api/api';
 
 jest.mock('hooks/useSnack');
 
+// TODO: Auth. Mock user Ids
 describe('CreateReportList', () => {
     let container: HTMLDivElement | null = null;
     beforeEach(() => {
@@ -32,86 +33,6 @@ describe('CreateReportList', () => {
         });
     });
 
-    it('should change report type to feedback', async () => {
-        ReactTestUtils.act(() => {
-            render(<ReportHistory />, container);
-        });
-
-        const reportSelector = document.querySelector(
-            '#reportSelector'
-        ) as HTMLSelectElement;
-
-        expect(reportSelector.value).toBe('');
-
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(reportSelector, {
-                target: ({ value: 'Feedback' } as unknown) as EventTarget,
-            });
-        });
-
-        expect(reportSelector.value).toBe('Feedback');
-    });
-
-    it('should change report type to bug', async () => {
-        ReactTestUtils.act(() => {
-            render(<ReportHistory />, container);
-        });
-
-        const reportSelector = document.querySelector(
-            '#reportSelector'
-        ) as HTMLSelectElement;
-
-        expect(reportSelector.value).toBe('');
-
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(reportSelector, {
-                target: ({ value: 'Bug' } as unknown) as EventTarget,
-            });
-        });
-
-        expect(reportSelector.value).toBe('Bug');
-    });
-
-    it('should change sorting order to ascending', async () => {
-        ReactTestUtils.act(() => {
-            render(<ReportHistory />, container);
-        });
-
-        const sortingSelector = document.querySelector(
-            '#sortingSelector'
-        ) as HTMLSelectElement;
-
-        expect(sortingSelector.value).toBe('');
-
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(sortingSelector, {
-                target: ({ value: 'true' } as unknown) as EventTarget,
-            });
-        });
-
-        expect(sortingSelector.value).toBe('true');
-    });
-
-    it('should change sorting order to descending', async () => {
-        ReactTestUtils.act(() => {
-            render(<ReportHistory />, container);
-        });
-
-        const sortingSelector = document.querySelector(
-            '#sortingSelector'
-        ) as HTMLSelectElement;
-
-        expect(sortingSelector.value).toBe('');
-
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(sortingSelector, {
-                target: ({ value: 'false' } as unknown) as EventTarget,
-            });
-        });
-
-        expect(sortingSelector.value).toBe('false');
-    });
-
     it('should submit get feedback reports request', async () => {
         const resolvedVal: AxiosResponse = {
             status: 200,
@@ -129,28 +50,6 @@ describe('CreateReportList', () => {
             render(<ReportHistory />, container);
         });
 
-        const reportSelector = document.querySelector(
-            '#reportSelector'
-        ) as HTMLSelectElement;
-        expect(reportSelector.value).toBe('');
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(reportSelector, {
-                target: ({ value: 'Feedback' } as unknown) as EventTarget,
-            });
-        });
-        expect(reportSelector.value).toBe('Feedback');
-
-        const sortingSelector = document.querySelector(
-            '#sortingSelector'
-        ) as HTMLSelectElement;
-        expect(sortingSelector.value).toBe('');
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(sortingSelector, {
-                target: ({ value: 'true' } as unknown) as EventTarget,
-            });
-        });
-        expect(sortingSelector.value).toBe('true');
-
         const submitButton = document.querySelector(
             '[type="submit"]'
         ) as HTMLButtonElement;
@@ -161,7 +60,7 @@ describe('CreateReportList', () => {
             );
         });
 
-        expect(spy).toBeCalledWith(1, 'true', '123456789');
+        expect(spy).toBeCalledWith(1, '', '123456789');
         jest.runAllTimers();
 
         await ReactTestUtils.act(async () => {
@@ -182,28 +81,6 @@ describe('CreateReportList', () => {
             render(<ReportHistory />, container);
         });
 
-        const reportSelector = document.querySelector(
-            '#reportSelector'
-        ) as HTMLSelectElement;
-        expect(reportSelector.value).toBe('');
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(reportSelector, {
-                target: ({ value: 'Feedback' } as unknown) as EventTarget,
-            });
-        });
-        expect(reportSelector.value).toBe('Feedback');
-
-        const sortingSelector = document.querySelector(
-            '#sortingSelector'
-        ) as HTMLSelectElement;
-        expect(sortingSelector.value).toBe('');
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(sortingSelector, {
-                target: ({ value: 'false' } as unknown) as EventTarget,
-            });
-        });
-        expect(sortingSelector.value).toBe('false');
-
         const submitButton = document.querySelector(
             '[type="submit"]'
         ) as HTMLButtonElement;
@@ -214,117 +91,7 @@ describe('CreateReportList', () => {
             );
         });
 
-        expect(spy).toBeCalledWith(1, 'false', '123456789');
-        jest.runAllTimers();
-
-        await ReactTestUtils.act(async () => {
-            await Promise.allSettled(spy.mock.results);
-        });
-    });
-
-    it('should submit get bug reports request', async () => {
-        const resolvedVal: AxiosResponse = {
-            status: 200,
-            data: {},
-            statusText: 'OK',
-            headers: {},
-            config: {},
-        };
-        const spy = jest
-            .spyOn(API, 'getBugReportsBySubmitter')
-            .mockResolvedValue(resolvedVal);
-        jest.useFakeTimers();
-
-        ReactTestUtils.act(() => {
-            render(<ReportHistory />, container);
-        });
-
-        const reportSelector = document.querySelector(
-            '#reportSelector'
-        ) as HTMLSelectElement;
-        expect(reportSelector.value).toBe('');
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(reportSelector, {
-                target: ({ value: 'Bug' } as unknown) as EventTarget,
-            });
-        });
-        expect(reportSelector.value).toBe('Bug');
-
-        const sortingSelector = document.querySelector(
-            '#sortingSelector'
-        ) as HTMLSelectElement;
-        expect(sortingSelector.value).toBe('');
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(sortingSelector, {
-                target: ({ value: 'false' } as unknown) as EventTarget,
-            });
-        });
-        expect(sortingSelector.value).toBe('false');
-
-        const submitButton = document.querySelector(
-            '[type="submit"]'
-        ) as HTMLButtonElement;
-
-        ReactTestUtils.act(() => {
-            submitButton.dispatchEvent(
-                new MouseEvent('click', { bubbles: true })
-            );
-        });
-
-        expect(spy).toBeCalledWith(1, 'false', '123456789');
-        jest.runAllTimers();
-
-        await ReactTestUtils.act(async () => {
-            await Promise.allSettled(spy.mock.results);
-        });
-    });
-
-    it('should attempt to submit get bug reports request and fail', async () => {
-        const rejectedVal = {
-            status: 500,
-        };
-        const spy = jest
-            .spyOn(API, 'getBugReportsBySubmitter')
-            .mockRejectedValue(rejectedVal);
-        jest.useFakeTimers();
-
-        ReactTestUtils.act(() => {
-            render(<ReportHistory />, container);
-        });
-
-        const reportSelector = document.querySelector(
-            '#reportSelector'
-        ) as HTMLSelectElement;
-        expect(reportSelector.value).toBe('');
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(reportSelector, {
-                target: ({ value: 'Bug' } as unknown) as EventTarget,
-            });
-        });
-        expect(reportSelector.value).toBe('Bug');
-
-        const sortingSelector = document.querySelector(
-            '#sortingSelector'
-        ) as HTMLSelectElement;
-        expect(sortingSelector.value).toBe('');
-        ReactTestUtils.act(() => {
-            ReactTestUtils.Simulate.change(sortingSelector, {
-                target: ({ value: 'true' } as unknown) as EventTarget,
-            });
-        });
-        expect(sortingSelector.value).toBe('true');
-
-        const submitButton = document.querySelector(
-            '[type="submit"]'
-        ) as HTMLButtonElement;
-
-        ReactTestUtils.act(() => {
-            submitButton.dispatchEvent(
-                new MouseEvent('click', { bubbles: true })
-            );
-        });
-
-        expect(spy).toBeCalledWith(1, 'true', '123456789');
+        expect(spy).toBeCalledWith(1, '', '123456789');
         jest.runAllTimers();
 
         await ReactTestUtils.act(async () => {
