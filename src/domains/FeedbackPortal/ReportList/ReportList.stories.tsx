2f624db780f37511dfca1ef79cdb474b4d30cb21
Johan Guzman <jguzm022@ucr.edu>
2020-08-31 15:35:15 -0700

    Uses report maker

--- src/domains/FeedbackPortal/ReportList/ReportList.stories.tsx
+++ src/domains/FeedbackPortal/ReportList/ReportList.stories.tsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import faker from 'faker';
 
 import Component from '.';
+import { makeFeedbackReport } from '../reportMaker';
 
 export default {
     title: 'Domains/ReportList',
     component: Component,
 };
 
-const recent = faker.date.recent();
-const future = faker.date.future();
-
-const makeReport = () => ({
-    _id: faker.random.alphaNumeric(12),
-    date: faker.date.between(recent, future).toISOString(),
-    description: faker.lorem.paragraphs(),
-    user: {
-        _id: faker.random.alphaNumeric(12),
-    },
-    type: 'feedback',
-});
-
 const makeReports = (numberOfReports: number) => {
     const list = [];
     for (let i = 0; i < numberOfReports; i += 1) {
-        list.push(makeReport());
+        list.push(makeFeedbackReport());
     }
     return list;
 };
