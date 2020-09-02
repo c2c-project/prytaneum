ab702b47989c5772b4e7e294589f0c8260752312
Kelton <kadey001@ucr.edu>
2020-09-02 13:00:08 -0700

    Refactored HandleInviteLink, no longer depends on state and useEffect

--- src/domains/Invite/HandleInviteLink/HandleInviteLink.tsx
+++ src/domains/Invite/HandleInviteLink/HandleInviteLink.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/ban-types */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import { TransitionProps } from '@material-ui/core/transitions/transition';
@@ -20,33 +22,19 @@ import { InviteTokenResult } from '../types';
 // Case 1: No account, redirect to register
 // Case 2: account exists, redirect to login page
 // Case 3: jwt invalid, display error message.
-function consumeInviteToken(
-    inviteToken: string,
-    setValidJWT: Function,
-    onError: Function
+export function consumeInviteToken(
+    inviteToken: string
 ): InviteTokenResult | undefined {
-    try {
-        const JWT_SECRET = 'secret';
-        // Decode the invite token
-        const decoded = jwt.verify(
-            inviteToken,
-            JWT_SECRET
-        ) as InviteTokenResult;
-        console.log('decoded: ', decoded);
-        const { email, townHallID } = decoded;
-        if (!email || !townHallID) {
-            // Throw error as link contains invalid data
-            errors.invalidToken(); // TODO handle this error
-            onError('Invalid Token Data');
-            return undefined;
-        }
-        setValidJWT(true);
-        return { email, townHallID };
-    } catch (e) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        onError(e.message);
+    const JWT_SECRET = 'secret';
+    // Decode the invite token
+    const decoded = jwt.verify(inviteToken, JWT_SECRET) as InviteTokenResult;
+    const { email, townHallID } = decoded;
+    if (!email || !townHallID) {
+        // Throw error as link contains invalid data
+        errors.invalidToken(); // TODO handle this error
         return undefined;
     }
+    return { email, townHallID };
 }
 
 const Transition = React.forwardRef(function Transition(
@@ -58,79 +46,63 @@ const Transition = React.forwardRef(function Transition(
     return <Slide direction='up' ref={ref} {...props} />;
 });
 
-export default function HandleInviteLink(): JSX.Element {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { token }: { token: string } = useParams();
+export function ErrorDialog({
+    errorMessage,
+}: {
+    errorMessage: string;
+}): JSX.Element {
+    const [open, setOpen] = useState(true);
     const history = useHistory();
-    const [townHallID, setTownHallID] = useState('');
-    const [validJWT, setValidJWT] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [open, setOpen] = useState(false);
-
-    const handleSuccess = () => {
-        history.push(`/townhalls/${townHallID}`); // TODO Verify correct route
-    };
-
-    const handleFailure = () => {
-        history.push('/home');
-    };
-
-    const handleOpen = () => {
-        setOpen(true);
-    };
-
     const handleClose = () => {
         setOpen(false);
         history.push('/home');
     };
-
-    const handleError = (error: string) => {
-        // JWT Error
-        setErrorMessage(error);
-        handleOpen();
-    };
-
-    const ErrorDialog = () => {
-        return (
-            <div>
-                <Dialog
-                    open={open}
-                    TransitionComponent={Transition}
-                    keepMounted
-                    onClose={handleClose}
-                    aria-labelledby='alert-dialog-slide-title'
-                    aria-describedby='alert-dialog-slide-description'
-                >
-                    <DialogContent>
-                        <InvalidInviteLink errorMessage={errorMessage} />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={handleClose} color='primary'>
-                            OK
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </div>
-        );
-    };
-
-    useEffect(() => {
-        const result = consumeInviteToken(token, setValidJWT, handleError);
-        if (result !== undefined) {
-            setTownHallID(result.townHallID);
-        }
-    }, []);
     return (
-        <div>
-            {validJWT ? (
-                <HandleInviteToken
-                    onSuccess={handleSuccess}
-                    onFailure={handleFailure}
-                    token={token}
-                />
-            ) : (
-                <ErrorDialog />
-            )}
+        <div id='error-dialog'>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-labelledby='alert-dialog-slide-title'
+                aria-describedby='alert-dialog-slide-description'
+            >
+                <DialogContent>
+                    <InvalidInviteLink errorMessage={errorMessage} />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color='primary'>
+                        OK
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
+}
+
+export default function HandleInviteLink(): JSX.Element {
+    const { token }: { token: string } = useParams();
+    const history = useHistory();
+    try {
+        const result = consumeInviteToken(token);
+        if (result === undefined) {
+            throw new Error('Undefined invite token data');
+        }
+        const handleSuccess = () => {
+            history.push(`/townhalls/${result.townHallID}`);
+        };
+
+        const handleFailure = () => {
+            history.push('/home');
+        };
+        return (
+            <HandleInviteToken
+                onSuccess={handleSuccess}
+                onFailure={handleFailure}
+                token={token}
+            />
+        );
+    } catch (e) {
+        return <ErrorDialog errorMessage={e.message} />;
+    }
 }
