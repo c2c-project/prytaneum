922aff144022843d81100c0c25e8638da5aff14e
Johan Guzman <jguzm022@ucr.edu>
2020-09-03 11:05:24 -0700

    Adds title

--- src/domains/CreditsDev/DevTeam/DevTeam.tsx
+++ src/domains/CreditsDev/DevTeam/DevTeam.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
 import Team from 'components/Team';
 import devTeam from './devTeamMembers';
 
 export default function DevTeam() {
-    return <Team teamMembers={devTeam} />;
+    return (
+        <div>
+            <Typography variant='h4' align='center' style={{ margin: 20 }}>
+                Lab Research Team
+            </Typography>
+            <Team teamMembers={devTeam} />
+        </div>
+    );
 }
