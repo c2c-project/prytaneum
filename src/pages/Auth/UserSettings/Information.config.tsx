296297262896eaefe2c80995e9b2fdee3240fc11
Carolyn Kong <carolyn_kong@outlook.com>
2020-09-03 20:19:04 -0700

    Spliting sections of User Settings into separate files

---
+++ src/pages/Auth/UserSettings/Information.config.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { ListItem } from '@material-ui/core';
+
+export const [openFeedback, setOpenFeedback] = React.useState(false);
+export const [openAboutUs, setOpenAboutUs] = React.useState(false);
+export const [openPrivacyPolicy, setOpenPrivacyPolicy] = React.useState(false);
+export const [openTOS, setOpenTOS] = React.useState(false);
+
+export function InformationState() {
+
+};
+ 
+{
+    s: [
+        'Feedback',
+        <span>hows our driving</span>,
+        openFeedback,
+        setOpenFeedback,
+    ],
+},
+{
+    s: [
+        'About Us',
+        <span>
+            <h1>This was made somehow by some people.</h1>
+        </span>,
+        openAboutUs,
+        setOpenAboutUs,
+    ],
+},
+{
+    s: [
+        'Privacy Policy',
+        <span>
+            <h1>Information is important.</h1>
+        </span>,
+        openPrivacyPolicy,
+        setOpenPrivacyPolicy,
+    ],
+},
+{
+    s: [
+        'TOS',
+        <span>plz no hurt us we no hurt u -- Carolyn</span>,
+        openTOS,
+        setOpenTOS,
+    ],
+},
+
+export const Information = {
+    title: 'About Prytaneum',
+    sectionData: [
+        {
+            title: '',
+            subtitle: (
+                <Grid
+                    component='span'
+                    container
+                    spacing={2}
+                    alignContent='center'
+                >
+                    <Grid component='span' item xs={12}>
+                        <ListItem
+                            button
+                            hidden={false}
+                            onClick={() => setOpenFeedback(true)}
+                        >
+                            Feedback
+                        </ListItem>
+                    </Grid>
+                    <Grid component='span' item xs={12}>
+                        <ListItem
+                            button
+                            hidden={false}
+                            onClick={() => setOpenAboutUs(true)}
+                        >
+                            About Us
+                        </ListItem>
+                    </Grid>
+                    <Grid component='span' item xs={12}>
+                        <ListItem
+                            button
+                            hidden={false}
+                            onClick={() => setOpenPrivacyPolicy(true)}
+                        >
+                            Privacy Policy
+                        </ListItem>
+                    </Grid>
+                    <Grid component='span' item xs={12}>
+                        <ListItem
+                            button
+                            hidden={false}
+                            onClick={() => setOpenTOS(true)}
+                        >
+                            Terms of Service
+                        </ListItem>
+                    </Grid>
+                </Grid>
+            ),
+        },
+    ],
+};
--- src/pages/Auth/UserSettings/UserSettings.tsx
+++ src/pages/Auth/UserSettings/UserSettings.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import { ListItem } from '@material-ui/core';
 
 import Paper from 'components/Paper';
 import Dialog from 'components/Dialog';
 import AppBar from 'layout/AppBar';
 import SectionList from 'components/SectionList';
+
+import UserProfile from './UserProfile.config';
+import { Options, AppearanceState } from './Options.config';
+import { AccountSettings, AccountSettingsState } from './AccountSettings.config';
+import { Information, InformationState } from './Information.config';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,35 +30,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function UserSettings() {
-    let anonymous = true; // TODO, should be part of the profile pulled from db
-    let notify = true; // TODO see above
-    // const darkmode = false; // TODO see above
-    // const colorscheme = <b>TODO</b>; // TODO see above
-    // const history = useHistory();
     const classes = useStyles();
-
-    const [form, setForm] = React.useState({
-        username: '',
-        password: '',
-    });
-    const handleChange = (
-        e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
-        id: string
-    ) => {
-        e.preventDefault();
-        const { value } = e.target;
-        setForm((state) => ({ ...state, [id]: value }));
-    };
-
-    // needed for dialogs
-    const [openAppearance, setOpenAppearance] = React.useState(false);
-    const [openLogout, setOpenLogout] = React.useState(false);
-    const [openDisable, setOpenDisable] = React.useState(false);
-    const [openDelete, setOpenDelete] = React.useState(false);
-    const [openFeedback, setOpenFeedback] = React.useState(false);
-    const [openAboutUs, setOpenAboutUs] = React.useState(false);
-    const [openPrivacyPolicy, setOpenPrivacyPolicy] = React.useState(false);
-    const [openTOS, setOpenTOS] = React.useState(false);
 
     const openStateArr: {
         s: [
@@ -66,386 +40,21 @@ export default function UserSettings() {
             React.Dispatch<React.SetStateAction<boolean>>
         ];
     }[] = [
-        {
-            s: [
-                'Appearance',
-                <>
-                    <h1>Dark mode: </h1>
-                    <h2>Color scheme: </h2>
-                </>,
-                openAppearance,
-                setOpenAppearance,
-            ],
-        },
-        {
-            s: [
-                'Logout',
-                <span>
-                    <button
-                        type='button'
-                        onClick={() => setOpenLogout(false)} // TODO: go to home page
-                    >
-                        Click here to return to the home page
-                    </button>
-                </span>,
-                openLogout,
-                setOpenLogout,
-            ],
-        },
-        {
-            s: [
-                'Disable Account',
-                <span>
-                    <h1>
-                        Disable Account?
-                        <p>
-                            You will no longer receive notifications about Town
-                            Halls and you can no longer join live Town Halls.
-                            You will still be able to log into your account.
-                            Please enter your password below twice to confirm.
-                        </p>
-                    </h1>
-                    <TextField
-                        id='Disable Account Password Entry One'
-                        required
-                        fullWidth
-                        variant='outlined'
-                        type='password'
-                        value=''
-                        onChange={() => {}}
-                        label='Please enter your password'
-                        spellCheck={false}
-                        autoComplete='off'
-                        autoCorrect='off'
-                        autoCapitalize='off'
-                    />
-                    <TextField
-                        id='Disable Account Password Entry Two'
-                        required
-                        fullWidth
-                        variant='outlined'
-                        type='password'
-                        value=''
-                        onChange={() => {}}
-                        label='Please enter your password again to DISABLE your account'
-                        spellCheck={false}
-                        autoComplete='off'
-                        autoCorrect='off'
-                        autoCapitalize='off'
-                    />
-                </span>,
-                openDisable,
-                setOpenDisable,
-            ],
-        },
-        {
-            s: [
-                'Delete Account',
-                <span>
-                    <h1>
-                        Delete Account?
-                        <p>
-                            All of your account information will be erased from
-                            Prytaneum. This action is irreversible. Please enter
-                            your password below twice to confirm.
-                        </p>
-                    </h1>
-                    <TextField
-                        id='Delete Account Password Entry One'
-                        required
-                        fullWidth
-                        variant='outlined'
-                        type='password'
-                        value=''
-                        onChange={() => {}}
-                        label='Please enter your password'
-                        spellCheck={false}
-                        autoComplete='off'
-                        autoCorrect='off'
-                        autoCapitalize='off'
-                    />
-                    <TextField
-                        id='Delete Account Password Entry Two'
-                        required
-                        fullWidth
-                        variant='outlined'
-                        type='password'
-                        value=''
-                        onChange={() => {}}
-                        label='Please enter your password again to DELETE your account'
-                        spellCheck={false}
-                        autoComplete='off'
-                        autoCorrect='off'
-                        autoCapitalize='off'
-                    />
-                </span>,
-                openDelete,
-                setOpenDelete,
-            ],
-        },
-        {
-            s: [
-                'Feedback',
-                <span>hows our driving</span>,
-                openFeedback,
-                setOpenFeedback,
-            ],
-        },
-        {
-            s: [
-                'About Us',
-                <span>
-                    <h1>This was made somehow by some people.</h1>
-                </span>,
-                openAboutUs,
-                setOpenAboutUs,
-            ],
-        },
-        {
-            s: [
-                'Privacy Policy',
-                <span>
-                    <h1>Information is important.</h1>
-                </span>,
-                openPrivacyPolicy,
-                setOpenPrivacyPolicy,
-            ],
-        },
-        {
-            s: [
-                'TOS',
-                <span>plz no hurt us we no hurt u -- Carolyn</span>,
-                openTOS,
-                setOpenTOS,
-            ],
-        },
+        {s: AppearanceState().s},
+        {s: AccountSettingsState().s1},
+        {s: AccountSettingsState().s2},
+        {s: AccountSettingsState().s3},
+        {s: InformationState()},
     ];
-
-    const userProfile = {
-        title: 'User',
-        sectionData: [
-            {
-                image: 'https://i.imgur.com/3beQH5s.jpeg', // TODO pull from db of users for pic
-                title: 'user.Fname user.Lname', // TODO pull from db of users for name
-                subtitle: (
-                    <Grid
-                        component='span'
-                        container
-                        spacing={2}
-                        alignContent='center'
-                    >
-                        <Grid component='span' item xs={12}>
-                            <TextField
-                                id='username'
-                                required
-                                fullWidth
-                                variant='outlined'
-                                type='text'
-                                value='pull from db for username'
-                                onChange={(e) => handleChange(e, 'username')}
-                                label='Username'
-                                spellCheck={false}
-                                autoComplete='off'
-                                autoCorrect='off'
-                                autoCapitalize='off'
-                            />
-                        </Grid>
-                        <Grid component='span' item xs={12}>
-                            <TextField
-                                id='email'
-                                required
-                                fullWidth
-                                variant='outlined'
-                                type='email'
-                                value='push to db for password'
-                                onChange={(e) => handleChange(e, 'email')}
-                                label='email'
-                                spellCheck={false}
-                                autoComplete='off'
-                                autoCorrect='off'
-                                autoCapitalize='off'
-                            />
-                        </Grid>
-                        <Grid component='span' item xs={12}>
-                            <TextField
-                                id='password'
-                                required
-                                fullWidth
-                                variant='outlined'
-                                type='password'
-                                value='push to db for password'
-                                onChange={(e) => handleChange(e, 'password')}
-                                label='Password'
-                                spellCheck={false}
-                                autoComplete='off'
-                                autoCorrect='off'
-                                autoCapitalize='off'
-                            />
-                        </Grid>
-                    </Grid>
-                ),
-            },
-        ],
-    };
-
-    const options = {
-        title: 'Options',
-        sectionData: [
-            {
-                title: '',
-                subtitle: (
-                    <Grid
-                        component='span'
-                        container
-                        spacing={2}
-                        alignContent='center'
-                    >
-                        <Grid component='span' item xs={12}>
-                            <Button
-                                component='span'
-                                // use state for options
-                                // flag in userProfile should have a field for anonymous
-                                // react is only re-rendered when use/set state is used
-                                // same value of variable is not kept upon refreshing
-                                onClick={() => {
-                                    anonymous = !anonymous;
-                                }}
-                            >
-                                Appear anonymous:
-                            </Button>
-                        </Grid>
-                        <Grid component='span' item xs={12}>
-                            <Button
-                                component='span'
-                                onClick={() => {
-                                    notify = !notify;
-                                }}
-                            >
-                                Notify me about upcoming Townhalls:
-                            </Button>
-                        </Grid>
-                        <Grid component='span' item xs={12}>
-                            <ListItem
-                                button
-                                hidden={false}
-                                onClick={() => setOpenAppearance(true)}
-                            >
-                                Appearance
-                            </ListItem>
-                        </Grid>
-                    </Grid>
-                ),
-            },
-        ],
-    };
-
-    const AccountSettings = {
-        title: 'Account Settings',
-        sectionData: [
-            {
-                title: '',
-                subtitle: (
-                    <Grid
-                        component='span'
-                        container
-                        spacing={2}
-                        alignContent='center'
-                    >
-                        <Grid component='span' item xs={12}>
-                            <ListItem
-                                button
-                                hidden={false}
-                                onClick={() => setOpenLogout(true)}
-                            >
-                                Logout
-                            </ListItem>
-                        </Grid>
-                        <Grid component='span' item xs={12}>
-                            <ListItem
-                                button
-                                hidden={false}
-                                onClick={() => setOpenDisable(true)}
-                            >
-                                Disable Account
-                            </ListItem>
-                        </Grid>
-                        <Grid component='span' item xs={12}>
-                            <ListItem
-                                button
-                                hidden={false}
-                                onClick={() => setOpenDelete(true)}
-                            >
-                                Delete Account
-                            </ListItem>
-                        </Grid>
-                    </Grid>
-                ),
-            },
-        ],
-    };
-
-    const Information = {
-        title: 'About Prytaneum',
-        sectionData: [
-            {
-                title: '',
-                subtitle: (
-                    <Grid
-                        component='span'
-                        container
-                        spacing={2}
-                        alignContent='center'
-                    >
-                        <Grid component='span' item xs={12}>
-                            <ListItem
-                                button
-                                hidden={false}
-                                onClick={() => setOpenFeedback(true)}
-                            >
-                                Feedback
-                            </ListItem>
-                        </Grid>
-                        <Grid component='span' item xs={12}>
-                            <ListItem
-                                button
-                                hidden={false}
-                                onClick={() => setOpenAboutUs(true)}
-                            >
-                                About Us
-                            </ListItem>
-                        </Grid>
-                        <Grid component='span' item xs={12}>
-                            <ListItem
-                                button
-                                hidden={false}
-                                onClick={() => setOpenPrivacyPolicy(true)}
-                            >
-                                Privacy Policy
-                            </ListItem>
-                        </Grid>
-                        <Grid component='span' item xs={12}>
-                            <ListItem
-                                button
-                                hidden={false}
-                                onClick={() => setOpenTOS(true)}
-                            >
-                                Terms of Service
-                            </ListItem>
-                        </Grid>
-                    </Grid>
-                ),
-            },
-        ],
-    };
-
+        
     const sections = [
         {
-            title: userProfile.title,
-            sectionData: userProfile.sectionData,
+            title: UserProfile().title,
+            sectionData: UserProfile().sectionData,
         },
         {
-            title: options.title,
-            sectionData: options.sectionData,
+            title: Options.title,
+            sectionData: Options.sectionData,
         },
         {
             title: AccountSettings.title,
