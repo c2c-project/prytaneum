7a48ee8b5ac32c6eaea5f79b48b7c1fba829ae2c
Kelton <kadey001@ucr.edu>
2020-09-10 01:25:30 -0700

    Updated InviteForm types to use ?

--- src/domains/Invite/types.ts
+++ src/domains/Invite/types.ts
@@ -1,19 +1,19 @@
 export interface InviteForm {
-	MoC: string | undefined,
-	topic: string | undefined,
-	eventDateTime: string | undefined,
-	constituentScope: 'state' | 'district',
-	region: string | undefined,
-	deliveryTime: Date | undefined,
-	townHallId: string | undefined,
+    MoC?: string;
+    topic?: string;
+    eventDateTime?: string;
+    constituentScope: 'state' | 'district';
+    region?: string;
+    deliveryTime?: Date;
+    townHallId?: string;
 }
 
 export interface InvitePreview {
-	sendPreview: boolean,
-	previewEmail: string,
+    sendPreview: boolean;
+    previewEmail: string;
 }
 
 export interface InviteTokenResult {
-	email: string,
-	townHallId: string,
+    email: string;
+    townHallId: string;
 }
