b34bf2890fb2fe34a3859c6f31835c9c4621139f
Karan <ksingh10@umd.edu>
2020-09-14 21:28:30 -0400

    reverted changes to useEndpoint and useSnack mocks

--- src/hooks/__mocks__/useSnack.ts
+++ src/hooks/__mocks__/useSnack.ts
@@ -1,29 +1,3 @@
-// import React from 'react';
-import { useSnackbar } from 'notistack';
-
-interface Options {
-    action?: JSX.Element;
-    onExited?: () => void;
-}
-
-export default function useSnack() {
-    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-    return [
-        (message: string, 
-            type: 'error' | 'success' | 'warning' | 'info' | 'default', 
-            options?: Options
-        ) => {
-            enqueueSnackbar(message, { 
-                variant: type,
-                action: options?.action,
-                onExited: options?.onExited,
-            });
-        },
-        closeSnackbar,
-    ];
-}
-
-/*
 export default function useSnack() {
     return [
         () => {
@@ -34,4 +8,3 @@ export default function useSnack() {
         },
     ];
 }
-*/
