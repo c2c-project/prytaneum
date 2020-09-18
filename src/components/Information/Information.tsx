f8a43a54ca807aff866d9c47fef4ed68df1a9d54
Anthony Hallak <ahall012@ucr.edu>
2020-09-17 20:21:59 -0700

    fixed contentFcn in information

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -8,70 +8,41 @@ export default function Information() {
     const [openFeedback, setOpenFeedback] = React.useState(false);
     const contentFeedback = () => {
         return (
-            <List>
-                <ListItem
-                    key='Feedback'
-                    button
-                    onClick={() => setContent(component)}
+            <>
+                <Grid
+                    component='span'
+                    container
+                    spacing={2}
+                    alignContent='center'
                 >
-                    <ListItemText primary={text} />
-                </ListItem>
-            </List>
+                    <Grid component='span' item xs={12}>
+                        <ListItem button hidden={false} onClick={() => {}}>
+                            Feedback
+                        </ListItem>
+                    </Grid>
+                    <Grid component='span' item xs={12}>
+                        <ListItem button hidden={false} onClick={() => {}}>
+                            About Us
+                        </ListItem>
+                    </Grid>
+                    <Grid component='span' item xs={12}>
+                        <ListItem button hidden={false} onClick={() => {}}>
+                            Privacy Policy
+                        </ListItem>
+                    </Grid>
+                    <Grid component='span' item xs={12}>
+                        <ListItem button hidden={false} onClick={() => {}}>
+                            Terms of Service
+                        </ListItem>
+                    </Grid>
+                </Grid>
+            </>
         );
     };
-    const [openAboutUs, setOpenAboutUs] = React.useState(false);
-    const [openPrivacyPolicy, setOpenPrivacyPolicy] = React.useState(false);
-    const [openTOS, setOpenTOS] = React.useState(false);
 
     return {
         title: 'About Prytaneum',
-        content: `${
-            <>
-            <Grid
-                component='span'
-                container
-                spacing={2}
-                alignContent='center'
-            >
-                <Grid component='span' item xs={12}>
-                    <ListItem
-                        button
-                        hidden={false}
-                        onClick={() => {}}
-                    >
-                        Feedback
-                    </ListItem>
-                </Grid>
-                <Grid component='span' item xs={12}>
-                    <ListItem
-                        button
-                        hidden={false}
-                        onClick={() => {}}
-                    >
-                        About Us
-                    </ListItem>
-                </Grid>
-                <Grid component='span' item xs={12}>
-                    <ListItem
-                        button
-                        hidden={false}
-                        onClick={() => {}}
-                    >
-                        Privacy Policy
-                    </ListItem>
-                </Grid>
-                <Grid component='span' item xs={12}>
-                    <ListItem
-                        button
-                        hidden={false}
-                        onClick={() => {}}
-                    >
-                        Terms of Service
-                    </ListItem>
-                </Grid>
-            </Grid>
-            </>
-        }`,
+        content: contentFeedback(),
         dialogData: [
             {
                 title: 'Feedback',
@@ -79,20 +50,24 @@ export default function Information() {
             },
             {
                 title: 'About Us',
-                component:  <span>
-                                <h1>this was made somehow by some people</h1>
-                            </span>,
+                component: (
+                    <span>
+                        <h1>this was made somehow by some people</h1>
+                    </span>
+                ),
             },
             {
                 title: 'Privacy Policy',
-                component:  <span>
-                                <h1>Information is important.</h1>
-                            </span>,
+                component: (
+                    <span>
+                        <h1>Information is important.</h1>
+                    </span>
+                ),
             },
             {
                 title: 'TOS',
                 component: <span>plz no hurt us we no hurt u</span>,
-            }
-        ]
+            },
+        ],
     };
 }
