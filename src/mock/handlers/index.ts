9e282b0bda39d9356839083224212eb52ca82caa
Stanley J. Muzhuthettu <smuzh001@ucr.edu>
2020-09-24 12:25:28 -0700

    fixed index to include clips and feedbackPortal

--- src/mock/handlers/index.ts
+++ src/mock/handlers/index.ts
@@ -3,9 +3,5 @@ import feedbackPortal from './feedbackPortal';
 import townhall from './townhall';
 import clips from './clips';
 
-<<<<<<< HEAD
 
-export default [...auth, ...townhall, ...clips];
-=======
-export default [...auth, ...townhall, ...feedbackPortal];
->>>>>>> e6a39a76cc43938333154e11659c9d0b75b894b2
+export default [...auth, ...townhall, ...clips, ...feedbackPortal];
