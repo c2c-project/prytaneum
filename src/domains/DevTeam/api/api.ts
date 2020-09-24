cc23287b0963cb457ca2224dccee760801fc7de8
Johan Guzman <jguzm022@ucr.edu>
2020-09-24 14:10:17 -0700

    Adds test. Note done yet

--- src/domains/DevTeam/api/api.ts
+++ src/domains/DevTeam/api/api.ts
@@ -6,21 +6,21 @@ export function getDevTeams() {
     return axios.get<{ devTeam: Team[] }>('/api/dev-team/get-teams');
 }
 
-export function addMemberToTeam(newMember: TeamMember, team: string) {
+export function addMemberToTeam(newMember: TeamMember, teamName: string) {
     if (
         !newMember.description ||
         !newMember.fullName ||
         !newMember.startDate ||
         !newMember.endDate ||
         !newMember.subtitle ||
-        !team
+        !teamName
     ) {
         throw errors.fieldError();
     }
 
     const body = {
-        teamMember: newMember,
-        team,
+        newMember,
+        teamName,
     };
     return axios.post<unknown>('/api/dev-team/add-member', body);
 }
--- src/mock/handlers/teams.ts
+++ src/mock/handlers/teams.ts
@@ -1,8 +1,16 @@
 import { rest } from 'msw';
-import { devTeam } from './devTeam.json';
+// Apparently fs module does not come with React, so I can't use to mack adding a new member to the json file (https://stackoverflow.com/questions/35003961/react-js-require-fs)
+// import fs from 'fs';
+// import { promisify } from 'util';
+
+// import { Team, TeamMember } from 'types';
+import devTeamFile from './devTeam.json';
+
+// const writeFilePromise = promisify(fs.writeFile);
 
 export default [
     rest.get('/api/dev-team/get-teams', (req, res, ctx) => {
+        const { devTeam } = devTeamFile;
         return res(
             ctx.status(200),
             ctx.json({
@@ -10,4 +18,35 @@ export default [
             })
         );
     }),
+    // rest.post('/api/dev-team/add-member', async (req, res, ctx) => {
+    //     const { newMember, teamName } = req.body as {
+    //         newMember: TeamMember;
+    //         teamName: string;
+    //     };
+
+    //     const { devTeam } = devTeamFile as { devTeam: Team[] };
+
+    //     if (!newMember || !teamName) {
+    //         return res(ctx.status(400));
+    //     }
+
+    //     const teamIndex = devTeam.findIndex(
+    //         (subTeam) => subTeam.name === teamName
+    //     );
+
+    //     if (teamIndex === -1) {
+    //         // teamName was not found in the .json file
+    //         return res(ctx.status(400));
+    //     }
+
+    //     try {
+    //         devTeam[teamIndex].members.push(newMember);
+    //         const newDevTeamJson = JSON.stringify(devTeam);
+    //         await writeFilePromise('./devTeam.json', newDevTeamJson, 'utf8');
+    //         return res(ctx.status(200));
+    //     } catch (error) {
+    //         console.log(error);
+    //         return res(ctx.status(400));
+    //     }
+    // }),
 ];
