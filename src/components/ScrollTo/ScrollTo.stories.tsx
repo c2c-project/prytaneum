2b460bef831657a21629da32ded2e044669ad465
Anthony Hallak <ahall012@ucr.edu>
2020-09-26 12:45:50 -0700

    Fixes ScrollTo spam button

--- src/components/ScrollTo/ScrollTo.stories.tsx
+++ src/components/ScrollTo/ScrollTo.stories.tsx
@@ -7,54 +7,21 @@ export default {
     component: Component,
 };
 
-function Spam(n: number, dir: string) {
-    // react use state
-    // generate an array of spam
-    // remove doc*
-    // click on button => add to list, similar to sectionlist
-    // see https://github.com/c2c-project/c2c-chat/blob/master/client/src/components/chat/chat.stories.jsx
-    // for inspiration
-
-    const [spam, updateSpam] = React.useState(n);
-    React.useEffect(() => {
-        const effect = document.getElementById('scrollTo');
-        const toAdd =
-            "<div style={{ height: '500px' }}><h1>SPAM #`${spam}`</h1></div>";
-        if (dir === 'top') {
-            effect?.insertAdjacentHTML('beforebegin', toAdd);
-        }
-    });
-
-    // const doc = document.getElementById('scrollTo');
-    // let toAdd = '<p><br />';
-    // const component = '<Component active >a</Component>';
-    // // needed as we cannot use n in the loop param if we modify it
-    // let x = n;
-    // while (x > 0) {
-    //     // eslint-disable-next-line
-    //     toAdd = toAdd + 'spam' + '<br />';
-    //     x -= 1;
-    // }
-    // toAdd += '</p>';
-    // if (dir === 'top') {
-    //     doc?.insertAdjacentHTML('beforebegin', toAdd);
-    //     doc?.insertAdjacentHTML('beforebegin', component);
-    // } else if (dir === 'bottom') {
-    //     doc?.insertAdjacentHTML('beforeend', toAdd);
-    //     doc?.insertAdjacentHTML('beforeend', component);
-    // } else {
-    //     doc?.insertAdjacentHTML('afterbegin', toAdd);
-    // }
-    // const temp = document.createElement('p');
-    // temp.insertAdjacentHTML('beforeend', toAdd);
-    // document.body.appendChild(temp);
-}
-
 export function ScrollToTop() {
+    const [cnt, setCount] = React.useState(0);
+    const [spam, updateSpam] = React.useState<JSX.Element[]>([<span key={cnt}><br/><br/><br/><h1>SPAM</h1><br/><br/><br/></span>]);
+    const update = () => {
+        setCount(cnt + 1);
+        spam.push(<span key={cnt}><br/><h1>SPAM</h1><br/></span>);
+        updateSpam(spam);
+    }
+    
     return (
         <div id='scrollTo'>
             <Component active direction='top'>
-                <button type='button' onClick={() => Spam(1000, 'top')}>
+                <p>{spam}</p>
+                <p>{cnt}</p>
+                <button type='button' onClick={() => update()}>
                     click to add spam
                 </button>
             </Component>
@@ -63,12 +30,22 @@ export function ScrollToTop() {
 }
 
 export function ScrollToBottom() {
+    const [cnt, setCount] = React.useState(0);
+    const [spam, updateSpam] = React.useState<JSX.Element[]>([<span key={cnt}><br/><br/><br/><h1>SPAM</h1><br/><br/><br/></span>]);
+    const update = () => {
+        setCount(cnt + 1);
+        spam.push(<span key={cnt}><br/><h1>SPAM</h1><br/></span>);
+        updateSpam(spam);
+    }
+
     return (
         <div id='scrollTo'>
             <Component active direction='bottom'>
-                <button type='button' onClick={() => Spam(1000, 'bottom')}>
+                <button type='button' onClick={() => update()}>
                     click to add spam
                 </button>
+                <p>{spam}</p>
+                <p>{cnt}</p>
             </Component>
         </div>
     );
