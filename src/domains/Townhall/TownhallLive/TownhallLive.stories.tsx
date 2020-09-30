38c4bf3f5961d732b122321274c00dc5c87f655e
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-09-29 21:21:13 -0700

    fix: story jsx -> tsx

--- src/domains/Townhall/TownhallLive/TownhallLive.stories.tsx
+++ src/domains/Townhall/TownhallLive/TownhallLive.stories.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Component from './TownhallLive';
 import TownhallProvider from '../Contexts/Townhall';
 
-export default { title: 'Domains/Townhall' };
+export default { title: 'Domains/Townhall/Townhall Live' };
 
-export function TownhallLive() {
+export function Basic() {
     return (
         <TownhallProvider townhallId='1234'>
             <Component />
