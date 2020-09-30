caf256f086249ede06277397fb5fbe53075c35c9
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-09-30 00:49:52 -0700

    fix: tests and also deleted unnecessary files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,11 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { init } from 'utils/storage';
 import App from './App';
+
+beforeAll(init);
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
