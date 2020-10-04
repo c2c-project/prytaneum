7e89be863b4040c0cb8a6e6266a5e6dbae733151
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-10-04 15:06:25 -0700

    feat: copy icon update

--- src/components/Copy/Copy.tsx
+++ src/components/Copy/Copy.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IconButton, Tooltip } from '@material-ui/core';
-import CopyIcon from '@material-ui/icons/FileCopy';
+import CopyIcon from '@material-ui/icons/ContentCopy';
 
 import useCopy from 'hooks/useCopy';
 
