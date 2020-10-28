3a56f1baa881e8f20963691dadcbecf6d8697aaf
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-10-28 15:55:22 -0700

    dev: TownhallPanes stories and modifications

--- src/domains/Townhall/TownhallPanes/TownhallPanes.tsx
+++ src/domains/Townhall/TownhallPanes/TownhallPanes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Paper, Fade } from '@material-ui/core';
+import { Fade } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 import { Pane, PaneContent, PaneNavigation } from './Panes';
 
@@ -8,14 +9,27 @@ import QuestionFeed from '../QuestionFeed';
 import Chat from '../Chat';
 import PaneSelect from '../PaneSelect';
 import AskQuestion from '../AskQuestion';
+import Information from '../Information';
 
 const useStyles = makeStyles((theme) => ({
     navigation: {
         paddingBottom: theme.spacing(2),
     },
-    paper: {
-        padding: theme.spacing(2),
+    container: {
         marginBottom: theme.spacing(2),
+    },
+    outerContainer: {
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+    },
+    innerContainer: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+    },
+    hidden: {
+        display: 'none',
     },
 }));
 
@@ -23,18 +37,15 @@ export default function TownhallPanes() {
     const panes = React.useMemo(
         () => ({
             'Question Feed': (
-                <div>
+                <>
                     <AskQuestion />
                     <QuestionFeed />
-                </div>
+                </>
             ),
-            Chat: (
-                <div>
-                    <Chat />
-                </div>
-            ),
+            Chat: <Chat />,
             Users: <div>USERS TODO</div>,
             Stats: <div>TODO</div>,
+            Information: <Information />,
         }),
         []
     );
@@ -42,10 +53,16 @@ export default function TownhallPanes() {
     const classes = useStyles();
     const [state, setState] = React.useState<PaneKey>('Chat');
 
+    function getClassName(key: string) {
+        return key === state
+            ? classes.innerContainer
+            : clsx([classes.innerContainer, classes.hidden]);
+    }
+
     return (
         <Pane>
             <PaneNavigation>
-                <Paper className={classes.paper}>
+                <div className={classes.container}>
                     <PaneSelect
                         options={Object.keys(panes)} // TODO: finish pane select
                         value={state}
@@ -55,29 +72,13 @@ export default function TownhallPanes() {
                             setState(copy.value as PaneKey);
                         }}
                     />
-                </Paper>
+                </div>
             </PaneNavigation>
             <PaneContent>
-                <div
-                    style={{
-                        position: 'relative',
-                        width: '100%',
-                        height: '100%',
-                    }}
-                >
+                <div className={classes.outerContainer}>
                     {Object.entries(panes).map(([key, value]) => (
-                        <Fade
-                            style={{
-                                position: 'absolute',
-                                width: '100%',
-                                height: '100%',
-                                display: key === state ? 'inherit' : 'none',
-                            }}
-                            in={key === state}
-                            key={key}
-                            timeout={400}
-                        >
-                            {value}
+                        <Fade in={key === state} key={key} timeout={400}>
+                            <div className={getClassName(key)}>{value}</div>
                         </Fade>
                     ))}
                 </div>
