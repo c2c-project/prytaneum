50519f0419fc8e6a8ccaa46622654192545d38ee
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-12-08 12:40:19 -0800

    temp commit

---
+++ src/domains/Questions/QuestionQueue/QuestionQueue.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import QuestionQueue from './QuestionQueue';
+
+export default { title: 'Domains/Questions/Question Queue' };
+
+export function Basic() {
+    return <QuestionQueue />;
+}
