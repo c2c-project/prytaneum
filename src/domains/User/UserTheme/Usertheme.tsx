3830029b237d2c7ccea792e4fddef1cf95131f8c
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-12-19 21:10:02 -0800

    fix: color

--- src/domains/User/UserTheme/Usertheme.tsx
+++ src/domains/User/UserTheme/Usertheme.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import InvertColorsIcon from '@material-ui/icons/InvertColors';
-import { IconButton, Tooltip } from '@material-ui/core';
+import { IconButton, Tooltip, IconButtonProps } from '@material-ui/core';
 
 import { ThemeSelector } from 'contexts/Theme';
 
-export default function UserTheme() {
+export default function UserTheme({
+    className,
+}: Pick<IconButtonProps, 'className'>) {
     const [toggle] = React.useContext(ThemeSelector);
 
     return (
         <Tooltip title='Toggle Theme'>
-            <IconButton color='inherit' edge='start' onClick={toggle}>
+            <IconButton
+                color='inherit'
+                edge='start'
+                onClick={toggle}
+                className={className}
+            >
                 <InvertColorsIcon />
             </IconButton>
         </Tooltip>
