b321e1927e3f9b9af88f3ba72b2b153269c52b88
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-12-28 20:22:50 -0800

    TextField story uses new layout components

--- src/components/TextField/TextField.stories.tsx
+++ src/components/TextField/TextField.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Page from 'layout/Page';
+import Container from 'layout/Container';
 import Component from './TextField';
 
 export default {
@@ -10,7 +10,7 @@ export default {
 
 export function TextField() {
     return (
-        <Page>
+        <Container>
             <Component
                 required
                 label='Storybook Label'
@@ -18,6 +18,6 @@ export function TextField() {
                 /* eslint-disable-next-line no-alert */
                 onChange={() => alert('onChange alert')}
             />
-        </Page>
+        </Container>
     );
 }
