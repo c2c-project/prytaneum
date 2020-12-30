675b99382f56c21b22cc30567a5b49f08d018fa5
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-12-30 01:24:44 -0800

    townhall form now displays in a dialog

--- src/pages/Townhall/TownhallList/TownhallList.tsx
+++ src/pages/Townhall/TownhallList/TownhallList.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddIcon from '@material-ui/icons/Add';
-import { Grid } from '@material-ui/core';
+import { Grid, DialogContent } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import TitleCard from 'components/TitleCard';
 import List from 'domains/Townhall/TownhallList';
+import Dialog from 'components/Dialog';
+import TownhallForm from 'domains/Townhall/TownhallForm';
 import Fab from 'components/Fab';
 import history, { makeRelativeLink } from 'utils/history';
 
@@ -19,8 +21,17 @@ const useStyles = makeStyles((theme) => ({
 
 export default function TownhallList() {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
     return (
         <Grid container className={classes.root}>
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogContent>
+                    <TownhallForm
+                        onCancel={() => setOpen(false)}
+                        onSubmit={() => setOpen(false)}
+                    />
+                </DialogContent>
+            </Dialog>
             <TitleCard title='Townhalls' stats={[['total', 1]]} />
             <Grid item xs={12}>
                 <List
@@ -29,11 +40,7 @@ export default function TownhallList() {
                     }
                 />
             </Grid>
-
-            <Fab
-                aria-label='Add Townhall'
-                onClick={() => history.push(makeRelativeLink('/create'))}
-            >
+            <Fab aria-label='Add Townhall' onClick={() => setOpen(true)}>
                 <AddIcon className={classes.fab} />
             </Fab>
         </Grid>
