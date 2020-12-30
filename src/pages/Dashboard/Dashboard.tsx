5ef8de46a4ca27c41bd90e30b8296630ffc4019d
DevelopmentByDavid <developmentbydavid@gmail.com>
2020-12-29 23:48:10 -0800

    prelim dashboard

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { motion } from 'framer-motion';
+
+import TitleCard from 'components/TitleCard';
+import { growProps } from 'components/Grow';
+
+export default function Dashboard() {
+    return (
+        <Grid container>
+            <TitleCard title='Dashboard' />
+            <Grid item xs={12}>
+                <motion.div {...growProps}>
+                    <Card
+                        style={{
+                            margin: '12px',
+                            minHeight: 500,
+                            display: 'flex',
+                        }}
+                    >
+                        <CardContent
+                            style={{
+                                display: 'flex',
+                                flex: '1 1 100%',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Grid item xs={12}>
+                                <Typography variant='h4' align='center'>
+                                    This area is still under construction!
+                                </Typography>
+                            </Grid>
+                        </CardContent>
+                    </Card>
+                </motion.div>
+            </Grid>
+        </Grid>
+    );
+}
