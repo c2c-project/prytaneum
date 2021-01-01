3aa9fef4b562750420f5d1ab40fb2ce2de1ac49e
DevelopmentByDavid <developmentbydavid@gmail.com>
2021-01-01 01:15:23 -0800

    middleware components

--- src/components/RequireLogin/RequireLogin.tsx
+++ src/components/RequireLogin/RequireLogin.tsx
@@ -5,17 +5,12 @@ import Login from 'pages/Auth/Login';
 import useStorage from 'hooks/useStorage';
 
 interface Props {
-    children: JSX.Element | JSX.Element[];
+    children: React.ReactNode | React.ReactNodeArray;
 }
 
 export default function RequireLogin({ children }: Props) {
     const [isLoggedIn] = useStorage('isLoggedIn');
     // FIXME: onLogin should do what?
-    if (!isLoggedIn)
-        return (
-            <Container>
-                <Login onLogin={() => {}} />
-            </Container>
-        );
+    if (!isLoggedIn) return <Login onLogin={() => {}} />;
     return <>{children}</>;
 }
---
+++ src/components/RequireRoles/RequireRoles.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import type { Roles } from 'prytaneum-typings';
+
+import { UserContext } from 'contexts/User';
+import Redirect from '../Redirect';
+
+interface Props {
+    requiredRoles: Roles[];
+    children: React.ReactNodeArray | React.ReactNode;
+}
+
+export default function RequireRoles({ requiredRoles, children }: Props) {
+    const user = React.useContext(UserContext);
+    const isUserAllowed = React.useMemo(
+        () => user && user.roles.some((role) => requiredRoles.includes(role)),
+        [requiredRoles, requiredRoles]
+    );
+
+    // TODO: make more robust
+    if (!user && !isUserAllowed) return <Redirect href='/logout' />;
+
+    return <>{children}</>;
+}
