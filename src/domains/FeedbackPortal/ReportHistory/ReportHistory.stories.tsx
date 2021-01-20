9402a3bf9eb3e27f90ba0826c8c48572302f20a2
Johan Guzman <jguzm022@ucr.edu>
2021-01-19 18:08:53 -0800

    adds scrolling

--- src/domains/FeedbackPortal/ReportHistory/ReportHistory.stories.tsx
+++ src/domains/FeedbackPortal/ReportHistory/ReportHistory.stories.tsx
@@ -5,5 +5,9 @@ import Component from '.';
 export default { title: 'Domains/Feedback/Report History' };
 
 export function ReportHistory() {
-    return <Component />;
+    return (
+        <div style={{ width: '100%', height: '100%', overflowY: 'scroll' }}>
+            <Component />
+        </div>
+    );
 }
