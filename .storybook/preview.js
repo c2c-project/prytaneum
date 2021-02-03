2514fde2d72ad5da4c242cbc1f969cd3e6c62e0b
DevelopmentByDavid <developmentbydavid@gmail.com>
2021-02-03 14:16:05 -0800

    minor msw improvements

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -19,7 +19,13 @@ if (typeof global.process === 'undefined' && !isChromatic()) {
     // Start the mocking when each story is loaded.
     // Repetitive calls to the `.start()` method do not register a new worker,
     // but check whether there's an existing once, reusing it, if so.
-    worker.start();
+    worker.start({
+        serviceWorker: {
+            options: {
+                scope: '/api',
+            },
+        },
+    });
 }
 
 const store = createStore(rootReducer);
