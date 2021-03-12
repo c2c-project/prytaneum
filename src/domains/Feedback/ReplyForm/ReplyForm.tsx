360aa612c9e6bdf7dc2da2f930055dbdfc1d7e29
Johan Guzman <jguzm022@ucr.edu>
2021-03-11 20:43:40 -0800

    refactors reply form

--- src/domains/Feedback/ReplyForm/ReplyForm.tsx
+++ src/domains/Feedback/ReplyForm/ReplyForm.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import useSnack from 'hooks/useSnack';
 import useEndpoint from 'hooks/useEndpoint';
 import LoadingButton from 'components/LoadingButton';
-import ReplyIcon from '@material-ui/icons/Comment';
+import type { ReportReplyForm } from 'prytaneum-typings';
 
 import Form from 'components/Form';
 import FormActions from 'components/FormActions';
 import FormContent from 'components/FormContent';
 import useForm from 'hooks/useForm';
-import Dialog from 'components/ResponsiveDialog';
 import TextField from 'components/TextField';
 import { replyToReport } from '../api';
 import { ReportTypes } from '../types';
@@ -20,10 +18,15 @@ interface Props {
     reportType: ReportTypes;
 }
 
+const ReplyFormInitialValues: ReportReplyForm = {
+    content: '',
+};
+
 export default function ReplyForm({ reportId, reportType }: Props) {
-    const [showDialog, setShowDialog] = React.useState(false);
     const [snack] = useSnack();
-    const [replyState, errors, handleSubmit, handleChange] = useForm({ content: '' });
+    const [replyState, errors, handleSubmit, handleChange, setReplyForm] = useForm<ReportReplyForm>(
+        ReplyFormInitialValues
+    );
     const replyToAPIRequest = React.useCallback(() => replyToReport(reportId, replyState.content, reportType), [
         reportId,
         replyState.content,
@@ -32,8 +35,8 @@ export default function ReplyForm({ reportId, reportType }: Props) {
 
     const [sendReplyRequest, isReplyLoading] = useEndpoint(replyToAPIRequest, {
         onSuccess: () => {
-            setShowDialog(false);
             snack('Reply successfully submitted');
+            setReplyForm(ReplyFormInitialValues);
         },
         onFailure: () => {
             snack('Reply could not be submitted. Please try again later');
@@ -41,49 +44,27 @@ export default function ReplyForm({ reportId, reportType }: Props) {
     });
 
     return (
-        <Grid item container>
-            <Grid item xs={12}>
-                <Button
-                    id='reply-button'
-                    fullWidth
-                    variant='contained'
-                    color='primary'
-                    startIcon={<ReplyIcon />}
-                    onClick={() => setShowDialog(true)}
-                >
-                    Reply
-                </Button>
-            </Grid>
-            <Dialog open={showDialog} onClose={() => setShowDialog(false)} maxWidth='md'>
-                <Form onSubmit={handleSubmit(sendReplyRequest)}>
-                    <FormContent>
-                        <TextField
-                            id='reply-content'
-                            name='content'
-                            label='Reply Content'
-                            error={Boolean(errors.content)}
-                            helperText={errors.content}
-                            required
-                            multiline
-                            value={replyState.content}
-                            onChange={handleChange('content')}
-                        />
-                    </FormContent>
-                    <FormActions>
-                        <LoadingButton loading={isReplyLoading}>
-                            <Button
-                                type='submit'
-                                id='submit-reply-button'
-                                variant='contained'
-                                fullWidth
-                                color='primary'
-                            >
-                                Submit
-                            </Button>
-                        </LoadingButton>
-                    </FormActions>
-                </Form>
-            </Dialog>
-        </Grid>
+        <Form onSubmit={handleSubmit(sendReplyRequest)}>
+            <FormContent>
+                <TextField
+                    id='reply-content'
+                    name='content'
+                    label='Reply Content'
+                    error={Boolean(errors.content)}
+                    helperText={errors.content}
+                    required
+                    multiline
+                    value={replyState.content}
+                    onChange={handleChange('content')}
+                />
+            </FormContent>
+            <FormActions>
+                <LoadingButton loading={isReplyLoading}>
+                    <Button type='submit' id='submit-reply-button' variant='contained' fullWidth color='primary'>
+                        Submit Reply
+                    </Button>
+                </LoadingButton>
+            </FormActions>
+        </Form>
     );
 }
