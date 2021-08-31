1451691c2b32ed0e42ed84b23ddb19b3a5f93a3e
rajbirjohar <github@rajbir.io>
2021-08-30 19:40:27 -0700

    feat(frontend): added footer to layout component

---
+++ app/client/src/layout/Footer/Footer.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+import { Container, Grid, Box, Link } from '@material-ui/core';
+
+export function Footer() {
+    return (
+        <footer>
+            <Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }}>
+                <Container maxWidth='lg'>
+                    <Grid container spacing={5}>
+                        <Grid item xs={12} sm={4}>
+                            <Box borderBottom={1}>Help</Box>
+                            <Box>
+                                <Link href='/'>Home</Link>
+                            </Box>
+                            <Box>
+                                <Link href='/'>Contact</Link>
+                            </Box>
+                            <Box>
+                                <Link href='/'>Support</Link>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Box borderBottom={1}>Account</Box>
+                            <Box>
+                                <Link href='/'>Login</Link>
+                            </Box>
+                            <Box>
+                                <Link href='/'>Sign Up</Link>
+                            </Box>
+                            <Box>
+                                <Link href='/'>Privacy</Link>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Box borderBottom={1}>Elsewhere</Box>
+                            <Box>
+                                <Link href='/'>Home</Link>
+                            </Box>
+                            <Box>
+                                <Link href='/'>Contact</Link>
+                            </Box>
+                            <Box>
+                                <Link href='/'>Privacy</Link>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </footer>
+    );
+}
