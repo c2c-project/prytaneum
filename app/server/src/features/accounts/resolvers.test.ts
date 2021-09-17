f27f8325a445725cc3f690407bfab3becd30907b
claynaut <jpesc002@ucr.edu>
2021-09-16 17:25:22 -0700

    test(server): initial commit for testing user account resolvers

---
+++ app/server/src/features/accounts/resolvers.test.ts
@@ -0,0 +1,1 @@
+import { resolvers } from './resolvers';
--- app/server/src/features/accounts/resolvers.ts
+++ app/server/src/features/accounts/resolvers.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { connectionFromArray, fromGlobalId } from 'graphql-relay';
-import { Resolvers, toGlobalId, runMutation } from '@local/features/utils';
+import { Resolvers, toGlobalId, runMutation } from '../utils';
 import * as User from './methods';
 
 const toUserId = toGlobalId('User');
