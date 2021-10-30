8e1420684ceea78158f8f2ac2e24add54e072e93
claynaut <claynaut@gmail.com>
2021-10-30 13:11:16 -0700

    fix(frontend): form validation for orgs

--- app/client/src/features/organizations/OrgForm.tsx
+++ app/client/src/features/organizations/OrgForm.tsx
@@ -1,35 +1,51 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { Button, TextField } from '@mui/material';
 
-import { LoadingButton } from '@local/components/LoadingButton';
-import { useForm } from '@local/core';
 import type { CreateOrganization } from '@local/graphql-types';
 import { Form } from '@local/components/Form';
 import { FormContent } from '@local/components/FormContent';
 import { FormTitle } from '@local/components/FormTitle';
 import { FormActions } from '@local/components/FormActions';
+import { makeInitialState } from '@local/utils/ts-utils';
+import { LoadingButton } from '@local/components/LoadingButton';
 
 export interface OrgFormProps {
     onSubmit: (result: TOrgFormState) => void;
 }
 
-const intialState: CreateOrganization = { name: '' };
+const initialState: CreateOrganization = { name: '' };
 
-export type TOrgFormState = typeof intialState;
+export type TOrgFormState = typeof initialState;
 
-export function OrgForm({ onSubmit }: OrgFormProps) {
-    const [state, errors, handleSubmit, handleChange] = useForm(intialState);
+type TSchema = {
+    [key in keyof TOrgFormState]: Yup.AnySchema;
+};
+const validationSchema = Yup.object().shape<TSchema>({
+    name: Yup.string().required('Please enter an organization name'),
+});
+
+export function OrgForm(props: OrgFormProps) {
+    const { onSubmit } = props;
+
+    const { handleSubmit, handleChange, values, errors } = useFormik<TOrgFormState>({
+        initialValues: makeInitialState(initialState),
+        validationSchema,
+        onSubmit,
+    });
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form onSubmit={handleSubmit}>
             <FormTitle title='Create Organization' />
             <FormContent>
                 <TextField
                     required
                     helperText={errors.name}
                     label='Organization Name'
-                    value={state.name}
+                    value={values.name}
                     fullWidth
                     onChange={handleChange('name')}
+                    error={Boolean(errors.name)}
                 />
             </FormContent>
             <FormActions disableGrow gridProps={{ justifyContent: 'flex-end' }}>
