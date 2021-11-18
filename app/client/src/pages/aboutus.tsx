e1e351e941c4d8844fc8fbf7a4e99b5809287790
claynaut <jpesc002@ucr.edu>
2021-11-17 18:37:18 -0800

    feat(frontend): implement about us page

--- app/client/src/pages/aboutus.tsx
+++ app/client/src/pages/aboutus.tsx
@@ -1,52 +1,111 @@
 import * as React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import Image from 'next/image';
+import { Grid, Typography, Hidden } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
-        height: '100%',
+        minHeight: '85vh',
+        paddingLeft: '1rem',
     },
-    paper: {
-        padding: theme.spacing(3),
+    section: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '1rem',
+        textAlign: 'center',
+    },
+    paragraph: {
+        fontSize: 'large'
     },
 }));
 
-const AboutUs = () => {
+export default function AboutUs() {
     const classes = useStyles();
+
     return (
-        <Grid className={classes.root} container direction='column' justify='center' alignItems='center' spacing={3}>
-            <Grid item>
-                <Typography variant='h1'>About Us</Typography>
-            </Grid>
-            <Grid item>
-                <Typography>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultricies pulvinar eros, in
-                    tincidunt arcu rutrum id. Pellentesque ac elit at felis efficitur ullamcorper pretium vel est.
-                    Mauris ac velit hendrerit, rutrum lectus eget, aliquet metus. Donec venenatis dolor quis arcu
-                    fermentum blandit. Maecenas finibus ac nibh et ultrices. Praesent non mattis odio, ac cursus turpis.
-                    Cras euismod quam eu leo euismod, id semper felis aliquam. Fusce vulputate pretium dui id faucibus.
-                    Mauris tristique nibh ut leo rhoncus sodales. Nam posuere lectus quis augue volutpat, at interdum
-                    orci malesuada. Aenean cursus nisi eu pharetra condimentum. Phasellus vitae urna et ex gravida
-                    eleifend.
+        <Grid container alignItems='center' className={classes.root} justify='center' spacing={2}>
+            
+            <Grid item xs={12} className={classes.section}>
+                <Grid item xs={4} sm={3} md={2}>
+                    <Image
+                        src='https://i.ibb.co/qmXt8Fw/prytaneum-logo-filled.png' 
+                        width={1107}
+                        height={1108}
+                        objectFit='contain'
+                    />
+                </Grid>
+                <Typography variant='body1' className={classes.paragraph}>
+                    Technology to Enhance Democracy
+                </Typography>
+                <Typography variant='h4'>
+                    Our Journey
                 </Typography>
             </Grid>
-            <Grid item>
-                <Typography>
-                    Fusce condimentum, orci in accumsan pulvinar, elit tellus sollicitudin eros, eu finibus dui nunc
-                    eget justo. Phasellus tincidunt nibh facilisis, tincidunt est eu, ultricies metus. Vivamus eget
-                    augue id odio condimentum aliquet ut a lacus. Nulla ultrices metus placerat magna varius volutpat.
-                    Praesent pretium nisi vel faucibus placerat. Vestibulum ante ipsum primis in faucibus orci luctus et
-                    ultrices posuere cubilia curae; Quisque at lobortis tellus. Sed aliquet tortor justo, feugiat
-                    accumsan sem aliquam at. Vestibulum ac orci odio. Vivamus placerat suscipit euismod. Sed lacinia,
-                    ante ut ornare ultricies, erat turpis finibus ligula, nec consectetur lectus turpis eu nunc. Nullam
-                    et facilisis urna. Praesent placerat lectus ac fermentum tincidunt. Nulla molestie suscipit lectus,
-                    eget maximus dolor tincidunt ut. Aenean sollicitudin varius laoreet. Fusce aliquet non nibh
-                    convallis venenatis.
+            <Grid item xs={12} sm={4} md={3} className={classes.section}>
+                <Image
+                    src='https://i.ibb.co/4YYwHx8/directors.png' 
+                    width={257}
+                    height={273}
+                    objectFit='contain'
+                />
+            </Grid>
+            <Grid item xs={12} sm={8} md={9} className={classes.section}>
+                <Typography variant='body1' className={classes.paragraph}>
+                    Built by the University of California, Riverside&apos;s Technology, 
+                    Communication and Democracy Lab (TeCD-Lab), the Prytaneum was spearheaded 
+                    by lab director Prof. <b>Kevin Esterling</b> and associate director 
+                    Prof. <b>Mariam Salloum</b>. 
+                </Typography>
+                <Hidden smDown>
+                    <Typography variant='body1' className={classes.paragraph}>
+                        In collaboration with the Congressional Management Foundation,  Prof. 
+                        Esterling and Prof. Salloum co-led the Connecting to Congress (C2C) 
+                        research team that received a grant from the National Science Foundation 
+                        (NSF) to work with the U.S. Congress on developing new technology for 
+                        constituent engagement. The C2C idea was that, in today’s society, it is 
+                        hard for individuals to feel like their voice is heard on important 
+                        policies.
+                    </Typography>
+                </Hidden>
+            </Grid>
+            <Grid item xs={12} className={classes.section}>
+                <Hidden mdUp>
+                    <Typography variant='body1' className={classes.paragraph}>
+                        In collaboration with the Congressional Management Foundation,  Prof. 
+                        Esterling and Prof. Salloum co-led the Connecting to Congress (C2C) 
+                        research team that received a grant from the National Science Foundation 
+                        (NSF) to work with the U.S. Congress on developing new technology for 
+                        constituent engagement. The C2C idea was that, in today’s society, it is 
+                        hard for individuals to feel like their voice is heard on important 
+                        policies.
+                    </Typography>
+                </Hidden>
+                <Typography variant='body1' className={classes.paragraph}>
+                    With seed funding from the Democracy Fund, the UCR TeCD-Lab developed 
+                    the <b>Prytaneum project</b>, the result of over a decade of research into 
+                    the best practices for online town halls. This new, open source online town hall 
+                    platform meets the requirements identified in the extensive C2C research, 
+                    and creates a setting for a many-participant, interactive event that lends 
+                    itself to collaborative, constructive exchanges between government officials 
+                    and constituents.
+                </Typography>
+                <Typography variant='body1' className={classes.paragraph}>
+                    The C2C research team published their findings  from the NSF study, Politics 
+                    with the People: Building a Directly Representative Democracy (Neblo, 
+                    Esterling, and Lazer 2018). In it they give recommendations for the design 
+                    features that make for a good online town hall experience. 
+                </Typography>
+                <Typography variant='body1' className={classes.paragraph}>
+                    In particular, the online platform should reduce barriers to participating 
+                    such that it is easy for everyone in the community to participate 
+                    interactively. The elected official should participate in the session 
+                    through streaming video and audio, so that the constituents can feel the 
+                    direct connection with the elected official and authenticate that it is in 
+                    fact the elected official with whom they are talking.
                 </Typography>
             </Grid>
         </Grid>
     );
-};
-
-export default AboutUs;
+}
