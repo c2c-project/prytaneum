3b595aa7919e1dd26fcf85c4125bd4fad6583c22
claynaut <hello@jspescas.io>
2021-12-15 16:29:37 -0800

    fix(frontend): implement requested changes to about us page

--- app/client/src/graphql-types.ts
+++ app/client/src/graphql-types.ts
@@ -236,6 +236,7 @@ export type EventEdge = {
   __typename?: 'EventEdge';
   cursor: Scalars['String'];
   node: Event;
+<<<<<<< HEAD
 };
 
 export type EventFeedbackMutationResponse = MutationResponse & {
@@ -243,11 +244,17 @@ export type EventFeedbackMutationResponse = MutationResponse & {
   body?: Maybe<EventLiveFeedbackEdge>;
   isError: Scalars['Boolean'];
   message: Scalars['String'];
+=======
+>>>>>>> fix(frontend): implement requested changes to about us page
 };
 
 export type EventLiveFeedback = Node & {
   __typename?: 'EventLiveFeedback';
+<<<<<<< HEAD
   createdAt?: Maybe<Scalars['Date']>;
+=======
+  createdAt?: Maybe<Scalars['String']>;
+>>>>>>> fix(frontend): implement requested changes to about us page
   createdBy?: Maybe<User>;
   event?: Maybe<Event>;
   id: Scalars['ID'];
@@ -403,12 +410,15 @@ export type EventVideoMutationResponse = MutationResponse & {
   body?: Maybe<EventVideo>;
   isError: Scalars['Boolean'];
   message: Scalars['String'];
+<<<<<<< HEAD
 };
 
 export type FeedbackOperation = {
   __typename?: 'FeedbackOperation';
   edge: EventLiveFeedbackEdge;
   operationType: Operation;
+=======
+>>>>>>> fix(frontend): implement requested changes to about us page
 };
 
 export type HideQuestion = {
--- app/client/src/pages/aboutus.tsx
+++ app/client/src/pages/aboutus.tsx
@@ -128,53 +128,76 @@ export default function AboutUs() {
                     by lab director Prof. <b>Kevin Esterling</b> and associate director 
                     Prof. <b>Mariam Salloum</b>. 
                 </Typography>
-                <Hidden smDown>
+                <Grid item>
                     <Typography variant='body1' className={classes.paragraph}>
-                        In collaboration with the Congressional Management Foundation,  Prof. 
-                        Esterling and Prof. Salloum co-led the Connecting to Congress (C2C) 
-                        research team that received a grant from the National Science Foundation 
-                        (NSF) to work with the U.S. Congress on developing new technology for 
-                        constituent engagement. The C2C idea was that, in today’s society, it is 
-                        hard for individuals to feel like their voice is heard on important 
-                        policies.
+                        Technology to Enhance Democracy
                     </Typography>
-                </Hidden>
+                </Grid>
+                <Grid item>
+                    <Typography variant='h4'>
+                        Our Journey
+                    </Typography>
+                </Grid>
             </Grid>
-            <Grid item xs={12} className={classes.section}>
-                <Hidden mdUp>
+            <Grid container item xs={12} justify='center' alignItems='center' spacing={2}>
+                <Grid item sm={5} md={3}>
+                    <Image
+                        src='/static/directors.png' 
+                        width={257}
+                        height={273}
+                        objectFit='contain'
+                    />
+                </Grid>
+                <Grid container item sm={7} md={9} spacing={2}>
+                    <Grid item>
+                        <Typography variant='body1' className={classes.paragraph}>
+                            Built by the University of California, Riverside&apos;s Technology, 
+                            Communication and Democracy Lab (TeCD-Lab), the Prytaneum was spearheaded 
+                            by lab director Prof. <b>Kevin Esterling</b> and associate director 
+                            Prof. <b>Mariam Salloum</b>. 
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant='body1' className={classes.paragraph}>
+                            In collaboration with the Congressional Management Foundation,  Prof. 
+                            Esterling and Prof. Salloum co-led the Connecting to Congress (C2C) 
+                            research team that received a grant from the National Science Foundation 
+                            (NSF) to work with the U.S. Congress on developing new technology for 
+                            constituent engagement. The C2C idea was that, in today’s society, it is 
+                            hard for individuals to feel like their voice is heard on important 
+                            policies.
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid item>
                     <Typography variant='body1' className={classes.paragraph}>
-                        In collaboration with the Congressional Management Foundation,  Prof. 
-                        Esterling and Prof. Salloum co-led the Connecting to Congress (C2C) 
-                        research team that received a grant from the National Science Foundation 
-                        (NSF) to work with the U.S. Congress on developing new technology for 
-                        constituent engagement. The C2C idea was that, in today’s society, it is 
-                        hard for individuals to feel like their voice is heard on important 
-                        policies.
+                        With seed funding from the Democracy Fund, the UCR TeCD-Lab developed 
+                        the <b>Prytaneum project</b>, the result of over a decade of research into 
+                        the best practices for online town halls. This new, open source online town hall 
+                        platform meets the requirements identified in the extensive C2C research, 
+                        and creates a setting for a many-participant, interactive event that lends 
+                        itself to collaborative, constructive exchanges between government officials 
+                        and constituents.
                     </Typography>
-                </Hidden>
-                <Typography variant='body1' className={classes.paragraph}>
-                    With seed funding from the Democracy Fund, the UCR TeCD-Lab developed 
-                    the <b>Prytaneum project</b>, the result of over a decade of research into 
-                    the best practices for online town halls. This new, open source online town hall 
-                    platform meets the requirements identified in the extensive C2C research, 
-                    and creates a setting for a many-participant, interactive event that lends 
-                    itself to collaborative, constructive exchanges between government officials 
-                    and constituents.
-                </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
-                    The C2C research team published their findings  from the NSF study, Politics 
-                    with the People: Building a Directly Representative Democracy (Neblo, 
-                    Esterling, and Lazer 2018). In it they give recommendations for the design 
-                    features that make for a good online town hall experience. 
-                </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
-                    In particular, the online platform should reduce barriers to participating 
-                    such that it is easy for everyone in the community to participate 
-                    interactively. The elected official should participate in the session 
-                    through streaming video and audio, so that the constituents can feel the 
-                    direct connection with the elected official and authenticate that it is in 
-                    fact the elected official with whom they are talking.
-                </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant='body1' className={classes.paragraph}>
+                        The C2C research team published their findings  from the NSF study, Politics 
+                        with the People: Building a Directly Representative Democracy (Neblo, 
+                        Esterling, and Lazer 2018). In it they give recommendations for the design 
+                        features that make for a good online town hall experience. 
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant='body1' className={classes.paragraph}>
+                        In particular, the online platform should reduce barriers to participating 
+                        such that it is easy for everyone in the community to participate 
+                        interactively. The elected official should participate in the session 
+                        through streaming video and audio, so that the constituents can feel the 
+                        direct connection with the elected official and authenticate that it is in 
+                        fact the elected official with whom they are talking.
+                    </Typography>
+                </Grid>
             </Grid>
         </Grid>
     );
