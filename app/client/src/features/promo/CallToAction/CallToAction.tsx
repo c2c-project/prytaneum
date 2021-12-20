c64aef0ea955aae422a28b14df268abfa37b7e91
claynaut <hello@jspescas.io>
2021-12-20 11:01:10 -0800

    feat(frontend): break up landing into components

--- app/client/src/features/promo/CallToAction/CallToAction.tsx
+++ app/client/src/features/promo/CallToAction/CallToAction.tsx
@@ -6,14 +6,41 @@ import { useRouter } from 'next/router';
 import { useUser } from '@local/features/accounts';
 
 const useStyles = makeStyles((theme) => ({
+<<<<<<< HEAD
     header: {
         marginBottom: theme.spacing(2),
+=======
+    landing: {
+        width: '100%',
+        minHeight: '100vh',
+        margin: 0,
+        [theme.breakpoints.down('sm')]: {
+            minHeight: '90vh',
+        },
+    },
+    header: {
+        marginBottom: 20,
+>>>>>>> feat(frontend): break up landing into components
         display: 'flex',
         flexDirection: 'column',
         [theme.breakpoints.down('sm')]: {
             alignItems: 'center'
         },
     },
+<<<<<<< HEAD
+=======
+    title: {
+        [theme.breakpoints.down('md')]: {
+            marginTop: 50,
+        },
+        [theme.breakpoints.down('sm')]: {
+            marginTop: 35,
+        },
+        [theme.breakpoints.down('xs')]: {
+            marginTop: 20,
+        },
+    },
+>>>>>>> feat(frontend): break up landing into components
     subtitle: {
         textAlign: 'right',
         color: '#272C6C',
@@ -25,7 +52,11 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     button: {
+<<<<<<< HEAD
         marginTop: theme.spacing(2),
+=======
+        marginTop: 20,
+>>>>>>> feat(frontend): break up landing into components
         minWidth: 300,
         alignSelf: 'flex-start',
         fontSize: 24,
@@ -38,6 +69,20 @@ const useStyles = makeStyles((theme) => ({
             fontSize: 20,
         },
     },
+<<<<<<< HEAD
+=======
+    arrowsection: {
+        display: 'flex',
+        justifyContent: 'center',
+        [theme.breakpoints.down('sm')]: {
+            display: 'none',
+        },
+    },
+    downarrow: {
+        fontSize: '4rem',
+        transform: 'rotate(-90deg)'
+    }
+>>>>>>> feat(frontend): break up landing into components
 }));
 
 export function CallToAction() {
@@ -48,12 +93,23 @@ export function CallToAction() {
     return (
         <>
             <Grid item xs={12} md={6} className={classes.header}>
+<<<<<<< HEAD
                 <Image
                     src='/static/prytaneum_logo2.svg' 
                     width={3483}
                     height={665}
                     objectFit='contain'
                 />
+=======
+                <div className={classes.title}>
+                    <Image
+                        src='/static/prytaneum_logo2.svg' 
+                        width={3483}
+                        height={665}
+                        objectFit='contain'
+                    />
+                </div>
+>>>>>>> feat(frontend): break up landing into components
                 <Typography variant='h5' className={classes.subtitle}>
                     A crucial tool for a better democracy.
                 </Typography>
