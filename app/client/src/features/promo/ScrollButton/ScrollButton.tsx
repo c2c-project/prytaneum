7c2f77f8c23af5b971d9955267f4cc4a6ed8106a
claynaut <hello@jspescas.io>
2021-12-21 10:44:14 -0800

    feat(frontend): implement working scrollbutton

---
+++ app/client/src/features/promo/ScrollButton/ScrollButton.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Grid, IconButton } from '@material-ui/core';
+import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
+import { makeStyles } from '@material-ui/core/styles';
+import useScrollTo from '@local/features/core/useScrollTo'
+
+const useStyles = makeStyles((theme) => ({
+    arrowsection: {
+        display: 'flex',
+        justifyContent: 'center',
+        [theme.breakpoints.down('sm')]: {
+            display: 'none',
+        },
+    },
+    downarrow: {
+        fontSize: '4rem',
+        transform: 'rotate(-90deg)'
+    },
+}));
+
+interface Props {
+    sentinelRef: React.RefObject<HTMLElement>;
+}
+
+export function ScrollButton({sentinelRef}: Props) {
+    const classes = useStyles();
+    const [scrollToAnchor, isAnchorInView] = useScrollTo(sentinelRef);
+
+    return (
+        <Grid item xs={12} className={classes.arrowsection}>
+            <IconButton onClick={() => scrollToAnchor('smooth')}>
+                <ArrowBackIosRoundedIcon  className={classes.downarrow}/>
+            </IconButton>
+        </Grid>
+    )
+}
