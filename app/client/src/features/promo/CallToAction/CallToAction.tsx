3499dabe117137b0326943e1e3987e76c92a78ef
claynaut <hello@jspescas.io>
2021-12-21 10:51:07 -0800

    chore(frontend): remove unnecessary CSS properties, use theme spacing

--- app/client/src/features/promo/CallToAction/CallToAction.tsx
+++ app/client/src/features/promo/CallToAction/CallToAction.tsx
@@ -8,29 +8,15 @@ import { useUser } from '@local/features/accounts';
 const useStyles = makeStyles((theme) => ({
     landing: {
         width: '100%',
-        minHeight: '100vh',
+        height: '100%',
         margin: 0,
-        [theme.breakpoints.down('sm')]: {
-            minHeight: '90vh',
-        },
     },
     header: {
-        marginBottom: 20,
+        marginBottom: theme.spacing(2),
         display: 'flex',
         flexDirection: 'column',
         [theme.breakpoints.down('sm')]: {
             alignItems: 'center'
-        },
-    },
-    title: {
-        [theme.breakpoints.down('md')]: {
-            marginTop: 50,
-        },
-        [theme.breakpoints.down('sm')]: {
-            marginTop: 35,
-        },
-        [theme.breakpoints.down('xs')]: {
-            marginTop: 20,
         },
     },
     subtitle: {
@@ -44,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     button: {
-        marginTop: 20,
+        marginTop: theme.spacing(2),
         minWidth: 300,
         alignSelf: 'flex-start',
         fontSize: 24,
@@ -57,17 +43,6 @@ const useStyles = makeStyles((theme) => ({
             fontSize: 20,
         },
     },
-    arrowsection: {
-        display: 'flex',
-        justifyContent: 'center',
-        [theme.breakpoints.down('sm')]: {
-            display: 'none',
-        },
-    },
-    downarrow: {
-        fontSize: '4rem',
-        transform: 'rotate(-90deg)'
-    }
 }));
 
 export function CallToAction() {
@@ -78,14 +53,12 @@ export function CallToAction() {
     return (
         <>
             <Grid item xs={12} md={6} className={classes.header}>
-                <div className={classes.title}>
-                    <Image
-                        src='/static/prytaneum_logo2.svg' 
-                        width={3483}
-                        height={665}
-                        objectFit='contain'
-                    />
-                </div>
+                <Image
+                    src='/static/prytaneum_logo2.svg' 
+                    width={3483}
+                    height={665}
+                    objectFit='contain'
+                />
                 <Typography variant='h5' className={classes.subtitle}>
                     A crucial tool for a better democracy.
                 </Typography>
