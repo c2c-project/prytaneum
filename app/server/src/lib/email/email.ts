0867c4f09fdaee774d9b23f233476a662e1ae5e6
kadey001 <kadey001@ucr.edu>
2022-01-13 16:52:38 -0800

    refactor(mailgun): simplified sendEmail function to make it more generic and reuseable

--- app/server/src/lib/email/email.ts
+++ app/server/src/lib/email/email.ts
@@ -1,30 +1,22 @@
 import mg from './client';
-
-export interface Email {
-    to: string | Array<string>,
-    subject: string,
-    text: string,
-    deliveryTime: Date,
-    recipientVariables: string,
+export interface EmailConfig {
+    to: string | Array<string>;
+    subject: string;
+    text?: string;
+    'o:deliverytime'?: string;
+    'recipient-variables'?: string;
+    template?: 'prytaneum-invite';
+    'h:X-Mailgun-Variables'?: string;
 }
+
 
 /**
  * @description internal function to use mg api to send email
- * @param {string} to email adress being sent to
- * @param {string} subject subject of the email
- * @param {string} text body of the email
- * @param {Date} deliveryTime Time that the email should be sent out (If the date is in the past it will be sent immediately)
- * @param {string} recipientVariables stringified recipient variables for email template
- * @param {string} customAPIKey? optional custom API key for mailgun
- * @param {string} customDomain? optional custom domain for mailgun
+ * @param {EmailConfig} mailgun email configuration
  * @returns {Promise<string>}
  */
 export async function sendEmail(
-    to: string | Array<string>,
-    subject: string,
-    text: string,
-    deliveryTime: Date,
-    recipientVariables: string
+    config: EmailConfig
 ) {
     if (!process.env.MAILGUN_DOMAIN) throw new Error('MAILGUN_DOMAIN not configured');
     
@@ -33,21 +25,13 @@ export async function sendEmail(
     }
     if (process.env.NODE_ENV === 'development') {
         return mg.messages.create(process.env.MAILGUN_DOMAIN, {
-            to,
+            ...config,
             from: `Prytaneum <${process.env.MAILGUN_FROM_EMAIL}>`,
-            subject,
-            text,
-            'recipient-variables': recipientVariables,
-            'o:deliverytime': deliveryTime.toUTCString(),
             'o:testmode': 'true',
         });
     }
     return mg.messages.create(process.env.MAILGUN_DOMAIN, {
-        to,
+        ...config,
         from: `Prytaneum <${process.env.MAILGUN_FROM_EMAIL}>`,
-        subject,
-        text,
-        'recipient-variables': recipientVariables,
-        'o:deliverytime': deliveryTime.toUTCString(),
     });
 }
