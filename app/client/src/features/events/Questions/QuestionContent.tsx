e3f299d1fa49621247b995aa0adfd2719dea37ba
claynaut <hello@jspescas.io>
2022-01-28 11:09:09 -0800

    feat(frontend): restyle question content

--- app/client/src/features/events/Questions/QuestionContent.tsx
+++ app/client/src/features/events/Questions/QuestionContent.tsx
@@ -1,5 +1,6 @@
 import { graphql, useFragment } from 'react-relay';
 import { CardContent, CardContentProps, Typography, TypographyProps } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import type { QuestionContentFragment$key } from '@local/__generated__/QuestionContentFragment.graphql';
 
@@ -14,11 +15,18 @@ export const QUESTION_CONTENT_FRAGMENT = graphql`
     }
 `;
 
+const useStyles = makeStyles((theme) => ({
+    content: {
+        margin: '-0.5rem 0'
+    },
+}));
+
 export function QuestionContent({ fragmentRef, typographyProps = {}, ...props }: QuestionContentProps) {
     const questionContentData = useFragment(QUESTION_CONTENT_FRAGMENT, fragmentRef);
+    const classes = useStyles();
     return (
-        <CardContent {...props}>
-            <Typography style={{ wordBreak: 'break-word' }} {...typographyProps}>{questionContentData.question}</Typography>
+        <CardContent {...props} className={classes.content}>
+            <Typography variant='inherit' style={{ wordBreak: 'break-word' }} {...typographyProps}>{questionContentData.question}</Typography>
         </CardContent>
     );
 }
