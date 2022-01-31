9232928891c8c266e69cd1ce461d649bd106419d
claynaut <hello@jspescas.io>
2022-01-30 16:41:33 -0800

    fix(frontend): clarify comments/variable names

--- app/client/src/components/Tabs/Tabs.tsx
+++ app/client/src/components/Tabs/Tabs.tsx
@@ -16,21 +16,22 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 interface Props {
-    value: number
+    tabIndex: number
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     onChange: (e: React.ChangeEvent<any>, newTabIndex: number) => void
     tabs: string[]
 }
 
-export const Tabs = ({ value, onChange, tabs }: Props) => {
+export const Tabs = ({ tabIndex, onChange, tabs }: Props) => {
     const classes = useStyles();
     return (
         <Grid className={classes.root}>
             { tabs.map((tabTitle: string, idx: number) =>
                 <Button
                     key={tabTitle}
-                    color={idx === value ? 'primary' : 'default'}
-                    variant={idx === value ? 'contained' : 'text'}
+                    // conditionally styles the color/variant selected tab
+                    color={idx === tabIndex ? 'primary' : 'default'}
+                    variant={idx === tabIndex ? 'contained' : 'text'}
                     onClick={(e) => onChange(e, idx)}
                     className={classes.button}
                 >
