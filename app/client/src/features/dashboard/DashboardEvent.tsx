7ee2511c6b16bee3b0c8a831de3488682d11611c
michael07w <michaelwessels207@gmail.com>
2022-02-24 19:34:37 -0800

    feat(frontend): Updated event layout to match the format seen in 'My Organizations' page :skip :skip

--- app/client/src/features/dashboard/DashboardEvent.tsx
+++ app/client/src/features/dashboard/DashboardEvent.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import * as React from 'react';
-import { Grid, Typography, Button, Link } from '@material-ui/core';
+import { ListItemText } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 // import TitleCard from '@local/components/TitleCard';
 // import FadeThrough from '@local/animations/FadeThrough';
 // import RequireRoles from '@local/domains/Logical/RequireRoles';
 // import RoleInvite from '@local//Admin/RoleInvite';
+
+import { formatDate } from '@local/utils/format';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -31,49 +33,18 @@ interface DashboardEventProps {
     id: string,
     title: string,
     description: string,
-    live: boolean,
+    startDateTime: Date,
 }
 
 export function DashboardEvent(props: DashboardEventProps) {
-    // const event_data = useFragment(DASHBOARD_EVENT_FRAGMENT, fragmentRef);
     const classes = useStyles();
-
-    if (props.live) {
-        return (
-            <>
-                <Grid item xs={12} sm={8} className={classes.text}>
-                    <Typography variant='subtitle2'>
-                        {props.title}
-                    </Typography>
-                    <Typography variant='body2' className={classes.secondaryText}>
-                        {props.description}
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <Link href={props.id}>
-                        <Button
-                            aria-label='view live feed of current event'
-                            variant='contained'
-                            color='primary'
-                        >
-                            Live Feed
-                        </Button>
-                    </Link>
-                </Grid>
-            </>
-        );
-    }
 
     return (
         <>
-            <Grid item xs={12} sm={8} className={classes.text}>
-                <Typography variant='subtitle2'>
-                    {props.title}
-                </Typography>
-                <Typography variant='body2' className={classes.secondaryText}>
-                    {props.description}
-                </Typography>
-            </Grid>
+            <ListItemText
+                primary={props.title}
+                secondary={props.startDateTime && formatDate(props.startDateTime)}
+            />
         </>
     );
 }
