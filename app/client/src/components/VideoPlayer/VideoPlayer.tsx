facc076d2233dda0fd2518e944188d4b187aeacf
kadey001 <kadey001@ucr.edu>
2022-03-29 20:56:27 -0700

    style(video text): updated to new phrase Prytaneum: A Crucial Tool for Democracy :skip

--- app/client/src/components/VideoPlayer/VideoPlayer.tsx
+++ app/client/src/components/VideoPlayer/VideoPlayer.tsx
@@ -51,7 +51,7 @@ export function VideoPlayer({ url, rest }: ReactPlayerProps) {
                     />
                 ) : (
                     <Grid container justifyContent='center' alignContent='center' className={classes.placeholder}>
-                        <Typography>At the start time of the event refresh your browser to see the video</Typography>
+                        <Typography>Prytaneum: A Crucial Tool for Democracy</Typography>
                     </Grid>
                 )}
             </div>
