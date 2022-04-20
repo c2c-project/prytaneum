58cbbe8bc818cb78aa540e8a1a146629465797ab
kadey001 <kadey001@ucr.edu>
2022-04-20 15:52:00 -0700

    chore(eslint): updated dev dep to include .spec files

--- app/client/e2e/landing.spec.ts
+++ app/client/e2e/landing.spec.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { test, expect } from '@playwright/test';
 
 test('landing page should have register button', async ({ page }) => {
