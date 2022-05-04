bbfb0b8604429ce53e2d5bb2af3de4a784128402
michael07w <michaelwessels207@gmail.com>
2022-05-03 21:06:45 -0700

    fix(frontend): more updates to material-ui imports for transition to v5

--- app/client/src/features/dashboard/DashboardEvent.tsx
+++ app/client/src/features/dashboard/DashboardEvent.tsx
@@ -1,26 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import * as React from 'react';
 import { ListItemText } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import { formatDate } from '@local/utils/format';
-
-const useStyles = makeStyles((theme) => ({
-    item: {
-        margin: theme.spacing(0, 0, 4, 0),
-    },
-    card: {
-        padding: theme.spacing(1, 1, 1, 1),
-    },
-    title: {
-        marginBottom: theme.spacing(1),
-    },
-    text: {
-        marginLeft: theme.spacing(1),
-    },
-    secondaryText: {
-        color: theme.palette.text.secondary,
-    },
-}));
 
 interface DashboardEventProps {
     id: string,
