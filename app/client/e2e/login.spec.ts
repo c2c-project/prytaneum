c42ea5d8a4da78b3c4d60881decc82759e9334a7
Minsoo Kim <minsooerickim@gmail.com>
2022-05-06 12:56:15 -0700

    test(e2e): added register page testing & fixed login page testing

--- app/client/e2e/login.spec.ts
+++ app/client/e2e/login.spec.ts
@@ -1,19 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('login page should have email form', async ({ page }) => {
-    await page.goto('/login');
-    await expect(page.locator('[data-test-id=login-email-text]')).toContainText('Email');
+test.describe('page redirects', () => {
+    test.describe.configure({ mode: 'parallel' });
+    test('login page should have email form', async ({ page }) => {
+        await page.goto('/login');
+        await expect(page.locator('[data-test-id=login-email-text]')).toContainText('Email');
+    })
+    test('login page should have password form', async ({ page }) => {
+        await page.goto('/login');
+        await expect(page.locator('[data-test-id=login-password-text]')).toContainText('Password');
+    });
 })
-test('login page should have password form', async ({ page }) => {
-    await page.goto('/login');
-    await expect(page.locator('[data-test-id=login-password-text]')).toContainText('Password');
-});
-test('An error displays when logging in with incorrect credentials', async ({ page }) => {
-    await page.goto('/login');
-    await page.locator('input[type="email"]').click();
-    await page.locator('input[type="email"]').fill('test@test.com');
-    await page.locator('input[type="email"]').press('Tab');
-    await page.locator('input[type="password"]').fill('testPassword');
-    await page.locator('text=LoginOr, register an account >> button').click();
-    await page.locator('text=Login failed; Invalid email or password.')
+test.describe('errors', () => {
+    test('An error displays when logging in with incorrect credentials', async ({ page }) => {
+        await page.goto('/login');
+        await page.locator('input[type="email"]').click();
+        await page.locator('input[type="email"]').fill('test@test.com');
+        await page.locator('input[type="email"]').press('Tab');
+        await page.locator('input[type="password"]').fill('testPassword');
+        await page.locator('text=LoginOr, register an account >> button').click();
+        await expect(page.locator('text=Login failed; Invalid email or password.Dismiss')).toBeVisible(); 
+    })
 })
---
+++ app/client/e2e/register.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('page redirects', () => {
+    test.describe.configure({ mode: 'parallel' });
+    test('register page should have a first name field', async ({ page }) => {
+        await page.goto('/register');
+        await expect(page.locator('label:has-text("First Name *")')).toBeVisible();
+    });
+    test('register page should have a last name field', async ({ page }) => {
+        await page.goto('/register');
+        await expect(page.locator('label:has-text("Last Name *")')).toBeVisible();
+    });
+    test('register page should have a email field', async ({ page }) => {
+        await page.goto('/register');
+        await expect(page.locator('label:has-text("Email *")')).toBeVisible();
+    });
+    test('register page should have a password field', async ({ page }) => {
+        await page.goto('/register');
+        await expect(page.locator('#register-password-label')).toBeVisible();
+    });
+    test('register page should have a confirm password field', async ({ page }) => {
+        await page.goto('/register');
+        await expect(page.locator('label:has-text("Confirm Password *")')).toBeVisible();
+    });
+})
+test.describe('errors', () => {
+    test('An error displays when registering with different passwords', async ({ page }) => {
+        await page.goto('/register');
+        await page.locator('text=First Name *First Name * >> input[type="text"]').click();
+        await page.locator('text=First Name *First Name * >> input[type="text"]').fill('John');
+        await page.locator('text=Last Name *Last Name * >> input[type="text"]').click();
+        await page.locator('text=Last Name *Last Name * >> input[type="text"]').fill('Doe');
+        await page.locator('input[type="email"]').click();
+        await page.locator('input[type="email"]').fill('JohnDoe@gmail.com');
+        await page.locator('text=Password *Password *Passwords must be at least 8 characters >> input[type="password"]').click();
+        await page.locator('text=Password *Password *Passwords must be at least 8 characters >> input[type="password"]').fill('JohnDoePwd');
+        await page.locator('text=Confirm Password *Confirm Password * >> input[type="password"]').click();
+        await page.locator('text=Confirm Password *Confirm Password * >> input[type="password"]').fill('diffPwd');
+        await page.locator('text=RegisterAlready have an account? >> button').click();
+        await expect(page.locator('text=Passwords must matchDismiss')).toBeVisible(); //times out for some reason currently
+    })
+})
