7fda6db83e53bb8ab4d02d667678e7809e1d8797
michael07w <michaelwessels207@gmail.com>
2022-06-24 09:41:19 -0700

    refactor(frontend): Updated function to get ongoing and upcoming events. Incorporated useMemo to memoize ongoing and upcoming events. Updated date comparison logic. Formatted code with prettier.

--- app/client/src/features/dashboard/Dashboard.tsx
+++ app/client/src/features/dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { graphql, PreloadedQuery, usePreloadedQuery } from 'react-relay';
 import { useRouter } from 'next/router';
+import { isBefore, isAfter } from 'date-fns';
 
 import { Button, Card, CardContent, Grid, Link, List, ListItem, Typography, IconButton } from '@mui/material';
 import { makeStyles } from '@mui/styles';
@@ -59,40 +60,37 @@ export function Dashboard({ queryRef }: Props) {
     const listOfEvents = React.useMemo(() => data.me?.events?.edges ?? [], [data.me]);
     const classes = useStyles();
     const router = useRouter();
-    const [user,,isLoading] = useUser();
+    const [user, , isLoading] = useUser();
     const handleNav = (path: string) => () => router.push(path);
 
     // Verify user is logged in
     React.useEffect(() => {
         if (!isLoading && !user) router.push('/');
     }, [user, router, isLoading]);
-    
-    if(!data) return <Loader />;
 
-    // Get current date to categorize events as current or upcoming
-    const currentDateTime = new Date();
-
-    // Store current events
-    const currentEvents = listOfEvents.filter(({ node: event }) => {
-        if (event.startDateTime && event.endDateTime) {
-            const eventStartDateTime = new Date(event.startDateTime);
-            const eventEndDateTime = new Date(event.endDateTime);
-            if (eventStartDateTime.getTime() < currentDateTime.getTime() && 
-                eventEndDateTime.getTime() > currentDateTime.getTime()) {
-                return true;
-            }
-        }
-    });
+    // Store ongoing events
+    const ongoingEvents = React.useMemo(
+        () =>
+            listOfEvents.filter(({ node: event }) => {
+                if (!event.startDateTime || !event.endDateTime) return false;
+                const now = new Date();
+                return isBefore(new Date(event.startDateTime), now) && isAfter(new Date(event.endDateTime), now);
+            }),
+        [listOfEvents]
+    );
 
     // Store upcoming events
-    const upcomingEvents = listOfEvents.filter(({ node: event }) => {
-        if (event.startDateTime) {
-            const eventStartDateTime = new Date(event.startDateTime);
-            if (currentDateTime < eventStartDateTime) {
-                return true;
-            }
-        }
-    });
+    const upcomingEvents = React.useMemo(
+        () =>
+            listOfEvents.filter(({ node: event }) => {
+                if (!event.startDateTime) return false;
+                const now = new Date();
+                return isAfter(new Date(event.startDateTime), now);
+            }),
+        [listOfEvents]
+    );
+
+    if (!data) return <Loader />;
 
     return (
         <Grid container>
@@ -102,17 +100,23 @@ export function Dashboard({ queryRef }: Props) {
                         <Typography variant='h6' className={classes.title}>
                             Current Events
                         </Typography>
-                        {currentEvents.length > 0 ? (
+                        {ongoingEvents.length > 0 ? (
                             <Grid container item direction='column'>
                                 <Grid item xs={12}>
                                     <List>
-                                        {currentEvents.map(({ node: event }, idx) => {
-                                            if (event?.id && event?.description && event?.title && event?.startDateTime && event?.organization?.name) {
+                                        {ongoingEvents.map(({ node: event }, idx) => {
+                                            if (
+                                                event?.id &&
+                                                event?.description &&
+                                                event?.title &&
+                                                event?.startDateTime &&
+                                                event?.organization?.name
+                                            ) {
                                                 return (
                                                     <ListItem
                                                         button
                                                         key={event?.id}
-                                                        divider={idx !== currentEvents.length - 1}
+                                                        divider={idx !== ongoingEvents.length - 1}
                                                         onClick={handleNav(`/events/${event?.id}/settings`)}
                                                     >
                                                         <DashboardEvent
@@ -142,9 +146,7 @@ export function Dashboard({ queryRef }: Props) {
                         ) : (
                             <Grid container justifyContent='space-between' alignItems='center' spacing={1}>
                                 <Grid item xs={12} sm={8} className={classes.text}>
-                                    <Typography variant='subtitle2'>
-                                        No Events To Display
-                                    </Typography>
+                                    <Typography variant='subtitle2'>No Events To Display</Typography>
                                 </Grid>
                             </Grid>
                         )}
@@ -162,7 +164,13 @@ export function Dashboard({ queryRef }: Props) {
                                 <Grid item xs={12}>
                                     <List>
                                         {upcomingEvents.map(({ node: event }, idx) => {
-                                            if (event?.id && event?.description && event?.title && event?.startDateTime && event?.organization?.name)
+                                            if (
+                                                event?.id &&
+                                                event?.description &&
+                                                event?.title &&
+                                                event?.startDateTime &&
+                                                event?.organization?.name
+                                            )
                                                 return (
                                                     <ListItem
                                                         button
@@ -179,18 +187,15 @@ export function Dashboard({ queryRef }: Props) {
                                                             organization={event.organization.name}
                                                         />
                                                     </ListItem>
-                                                )
-                                            }
-                                        )}
+                                                );
+                                        })}
                                     </List>
                                 </Grid>
                             </Grid>
                         ) : (
                             <Grid container justifyContent='space-between' alignItems='center' spacing={1}>
                                 <Grid item className={classes.text}>
-                                    <Typography variant='subtitle2'>
-                                        No Events To Display
-                                    </Typography>
+                                    <Typography variant='subtitle2'>No Events To Display</Typography>
                                 </Grid>
                             </Grid>
                         )}
@@ -199,11 +204,8 @@ export function Dashboard({ queryRef }: Props) {
             </Grid>
             <Grid item>
                 <Card>
-                    <CardContent style={{ display: 'flex', justifyContent: 'center', padding: 12, }}>
-                        <IconButton
-                            aria-label='view future event'
-                            onClick={handleNav('/organizations/me')}
-                        >
+                    <CardContent style={{ display: 'flex', justifyContent: 'center', padding: 12 }}>
+                        <IconButton aria-label='view future event' onClick={handleNav('/organizations/me')}>
                             <Add style={{ fontSize: 32, color: 'black' }} />
                         </IconButton>
                     </CardContent>
