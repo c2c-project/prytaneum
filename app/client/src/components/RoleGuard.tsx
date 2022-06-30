87118a64d2724ec3142364968d1610ea119fe6a9
kadey001 <kadey001@ucr.edu>
2022-06-30 15:30:13 -0700

    fix(roleguard): fixed rolegaurd check and action

--- app/client/src/components/RoleGuard.tsx
+++ app/client/src/components/RoleGuard.tsx
@@ -5,9 +5,9 @@ export interface RoleGuardProps {
 }
 
 // By default this component will not render any children unless the user is an organizer
-export function RoleGuard({ organizer, children }: RoleGuardProps){
+export function RoleGuard({ organizer, children }: RoleGuardProps) {
     const [user] = useUser();
-    if (!user) return <></> // if user is not logged in
-    if (user.isOrganizer && organizer) return <>{children}</> // if user is organizer
-    return <></> // if the user is logged in but not an organizer // bug: isOrganizer is not returning correctly
+    if (!user) return <></>; // if user is not logged in
+    if (user.isOrganizer && organizer) return <>{children}</>; // if user is organizer
+    return <></>; // if the user is logged in but not an organizer // bug: isOrganizer is not returning correctly
 }
