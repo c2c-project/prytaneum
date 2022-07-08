d07a465b0093241ddb581a4bd9accd50375ec6cc
kadey001 <kadey001@ucr.edu>
2022-07-07 19:09:56 -0700

    chore(prisma): updated seed to ensure clean db before testing

--- app/server/prisma/seed.ts
+++ app/server/prisma/seed.ts
@@ -1,25 +1,11 @@
-import bcrypt from 'bcryptjs';
 import { PrismaClient } from '../src/__generated__/prisma';
 const prisma = new PrismaClient();
 
 async function main() {
-    const encryptedPassword = await bcrypt.hash('password', 10);
-
-    const testUser = await prisma.user.upsert({
-        where: { email: 'test@prytaneum.io' },
-        update: {},
-        create: {
-            email: 'test@prytaneum.io',
-            firstName: 'Test',
-            lastName: 'User',
-            fullName: 'Test User',
-            password: encryptedPassword,
-            preferredLang: 'EN',
-        },
-    });
-
-    console.log('DB Seeded.');
-    console.log({ testUser });
+    // Ensure database tables are empty
+    await prisma.event.deleteMany();
+    await prisma.organization.deleteMany();
+    await prisma.user.deleteMany();
 }
 
 main()
