d3692a510dbe55da550b13bd5cdd318f580f01af
kadey001 <kadey001@ucr.edu>
2022-07-09 02:25:27 -0700

    chore(prisma): added default and organizer account to seed

--- app/server/prisma/seed.ts
+++ app/server/prisma/seed.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import { PrismaClient } from '../src/__generated__/prisma';
 const prisma = new PrismaClient();
 
@@ -6,6 +7,29 @@ async function main() {
     await prisma.event.deleteMany();
     await prisma.organization.deleteMany();
     await prisma.user.deleteMany();
+    // Default User
+    await prisma.user.create({
+        data: {
+            email: 'test@example.com',
+            firstName: 'Test',
+            lastName: 'User',
+            fullName: 'Test User',
+            password: await bcrypt.hash('Password1!', 10),
+            preferredLang: 'EN',
+        },
+    });
+    // Organizer
+    await prisma.user.create({
+        data: {
+            email: 'organizer@example.com',
+            firstName: 'Test',
+            lastName: 'Organizer',
+            fullName: 'Test Organizer',
+            password: await bcrypt.hash('Password1!', 10),
+            preferredLang: 'EN',
+            canMakeOrgs: true,
+        },
+    });
 }
 
 main()
