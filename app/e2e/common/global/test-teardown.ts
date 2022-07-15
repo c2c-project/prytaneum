051fcd4bd105c9913a27a4966120e43f704ccc6c
kadey001 <kadey001@ucr.edu>
2022-07-14 22:02:16 -0700

    fix(e2e): tests use context based on device now, ensuring isolation

--- app/e2e/common/global/test-teardown.ts
+++ app/e2e/common/global/test-teardown.ts
@@ -1,37 +1,82 @@
 import { chromium } from '@playwright/test';
-import { PlaywrightLandingPage } from '@local/common/pages/playwright-landing-page';
+import { PlaywrightLandingPage } from '@local/common/pages';
+import { getOrganizersByDeviceAmmount, getUsersByDeviceAmmount } from '../utils/accounts';
 
 async function globalTeardown() {
-    // Setup Context
-    const browser = await chromium.launch();
-    const context = await browser.newContext({ baseURL: process.env.BASE_URL || 'http://localhost:8080' });
-    // Open new page
-    const page = await context.newPage();
-    const landing = new PlaywrightLandingPage(page);
-    await landing.goto();
-    await landing.clickOnLogin();
-    await landing.fillInEmail('test@example.com');
-    await landing.fillInPassword('Password1!');
-    await landing.submitLoginForm();
-    // Delete Account
-    // TODO update once methods exist for these actions
-    await page.locator('[data-test-id="appbar-user-menu"]').click();
-    await Promise.all([
-        page.waitForNavigation(/*{ url: '/settings' }*/),
-        page.locator('li[role="menuitem"]:has-text("Settings")').click(),
-    ]);
-    await page.locator('[aria-label="Enter your password"]').click();
-    await page.locator('[aria-label="Enter your password"]').fill('Password1!');
-    await page.locator('[aria-label="Enter your password again"]').click();
-    await page.locator('[aria-label="Enter your password again"]').fill('Password1!');
-    await Promise.all([
-        page.waitForNavigation(/*{ url: '/organizations/me' }*/),
-        page.locator('button:has-text("Delete account")').click(),
-    ]);
-    await page.goto('http://localhost:8080/');
-    // Cleanup
-    await context.close();
-    await browser.close();
+    console.log('Running Global Teardown...');
+    // const browser = await chromium.launch();
+
+    // const users = getUsersByDeviceAmmount();
+    // const organizers = getOrganizersByDeviceAmmount();
+
+    // // Delete all users
+    // for (let i = 0; i < users.length; i++) {
+    //     // Setup Context
+    //     const context = await browser.newContext({ baseURL: process.env.BASE_URL || 'http://localhost:8080' });
+    //     // Open new page
+    //     const page = await context.newPage();
+    //     const landing = new PlaywrightLandingPage(page);
+    //     await landing.goto();
+    //     await landing.clickOnLogin();
+    //     await landing.fillInEmail(`${users[i]}@example.com`);
+    //     await landing.fillInPassword('Password1!');
+    //     await landing.submitLoginForm();
+    //     // Delete Account
+    //     // TODO update once methods exist for these actions
+    //     await page.locator('[data-test-id="appbar-user-menu"]').click();
+    //     await Promise.all([
+    //         page.waitForNavigation(/*{ url: '/settings' }*/),
+    //         page.locator('li[role="menuitem"]:has-text("Settings")').click(),
+    //     ]);
+    //     await page.locator('[aria-label="Enter your password"]').click();
+    //     await page.locator('[aria-label="Enter your password"]').fill('Password1!');
+    //     await page.locator('[aria-label="Enter your password again"]').click();
+    //     await page.locator('[aria-label="Enter your password again"]').fill('Password1!');
+    //     await Promise.all([
+    //         page.waitForNavigation(/*{ url: '/organizations/me' }*/),
+    //         page.locator('button:has-text("Delete account")').click(),
+    //     ]);
+    //     await page.goto('http://localhost:8080/');
+    //     // Cleanup
+    //     await page.close();
+    //     await context.close();
+    // }
+
+    // // Delete all organizers
+    // for (let i = 0; i < users.length; i++) {
+    //     // Setup Context
+    //     const context = await browser.newContext({ baseURL: process.env.BASE_URL || 'http://localhost:8080' });
+    //     // Open new page
+    //     const page = await context.newPage();
+    //     const landing = new PlaywrightLandingPage(page);
+    //     await landing.goto();
+    //     await landing.clickOnLogin();
+    //     await landing.fillInEmail(`${organizers[i]}@example.com`);
+    //     await landing.fillInPassword('Password1!');
+    //     await landing.submitLoginForm();
+    //     // Delete Account
+    //     // TODO update once methods exist for these actions
+    //     await page.locator('[data-test-id="appbar-user-menu"]').click();
+    //     await Promise.all([
+    //         page.waitForNavigation(/*{ url: '/settings' }*/),
+    //         page.locator('li[role="menuitem"]:has-text("Settings")').click(),
+    //     ]);
+    //     await page.locator('[aria-label="Enter your password"]').click();
+    //     await page.locator('[aria-label="Enter your password"]').fill('Password1!');
+    //     await page.locator('[aria-label="Enter your password again"]').click();
+    //     await page.locator('[aria-label="Enter your password again"]').fill('Password1!');
+    //     await Promise.all([
+    //         page.waitForNavigation(/*{ url: '/organizations/me' }*/),
+    //         page.locator('button:has-text("Delete account")').click(),
+    //     ]);
+    //     await page.goto('http://localhost:8080/');
+    //     // Cleanup
+    //     await page.close();
+    //     await context.close();
+    // }
+
+    // await browser.close();
+    console.log('Global Teardown Complete');
 }
 
 export default globalTeardown;
--- app/e2e/common/pages/playwright-organizations-page.ts
+++ app/e2e/common/pages/playwright-organizations-page.ts
@@ -1,25 +1,38 @@
 // playwright-organizations-page.ts
-import { Browser, expect, Locator, Page } from '@playwright/test';
+import { Browser, expect, Locator, Page, TestInfo } from '@playwright/test';
+import { Device, getIndexByDevice } from '@local/common/utils';
 
 export class PlaywrightOrganizationsPage {
     readonly page: Page;
+    readonly device: Device;
 
-    readonly CreateOrganizationButton: Locator;
+    readonly createOrganizationButton: Locator;
+    readonly createOrganizationInput: Locator;
+    readonly createOrganizationSubmitButton: Locator;
 
-    constructor(page: Page) {
+    constructor(page: Page, device: Device) {
         this.page = page;
-        this.CreateOrganizationButton = page.locator('[data-test-id="create-organization-button"]');
+        this.device = device;
+
+        this.createOrganizationButton = page.locator('[data-test-id="create-organization-button"]');
+        this.createOrganizationInput = page.locator('input[type="text"]');
+        this.createOrganizationSubmitButton = page.locator('button:has-text("Create")');
     }
 
-    // Allows for the reuse of authentication for the organization page tests
-    // From playwright docs https://playwright.dev/docs/test-auth#testing-multiple-roles-with-pom-fixtures
+    /**
+     * Allows for the reuse of authentication for the organization page tests
+     * From playwright docs https://playwright.dev/docs/test-auth#testing-multiple-roles-with-pom-fixtures
+     * The Parallel Index is used to isolate context between workers
+     * You can find more info about the Parallel Index here: https://playwright.dev/docs/api/class-testinfo#test-info-parallel-index
+     */
     async create(browser: Browser) {
+        const deviceIndex = getIndexByDevice(this.device);
         const context = await browser.newContext({
             baseURL: process.env.BASE_URL || 'http://localhost:8080',
-            storageState: './common/state/organizerStorageState.json',
+            storageState: `./common/state/organizer${deviceIndex}StorageState.json`,
         });
         const page = await context.newPage();
-        return new PlaywrightOrganizationsPage(page);
+        return new PlaywrightOrganizationsPage(page, this.device);
     }
 
     async goto() {
@@ -27,14 +40,29 @@ export class PlaywrightOrganizationsPage {
     }
 
     async clickOnCreateOrganization() {
-        await this.CreateOrganizationButton.click();
+        await this.createOrganizationButton.click();
     }
 
-    async fillInOrganizationName(organizationName: string) {}
+    async fillInOrganizationName(organizationName: string) {
+        await this.createOrganizationInput.fill(organizationName);
+    }
 
-    async submitOrganizationForm() {}
+    async submitOrganizationForm() {
+        await this.createOrganizationSubmitButton.click();
+    }
 
-    async clickOnOrganization(organizationName: string) {}
+    async clickOnOrganization(organizationName: string) {
+        await Promise.all([
+            await this.page.locator(`div[role="button"]:has-text("${organizationName}")`).first().click(),
+            await this.page.waitForNavigation(),
+        ]);
+    }
 
-    async clickDeleteOrganizationButton(organizationName: string) {}
+    async clickDeleteOrganizationButton() {
+        await this.page.locator('[aria-label="delete organization"]').first().click();
+    }
+
+    async clickConfirmDeleteOrganizationButton() {
+        await this.page.locator('text=Confirm').click();
+    }
 }
