1b9aa1aa84e0fbb9e35ff42d1e300cb2d1a325a3
kadey001 <kadey001@ucr.edu>
2022-07-14 22:03:44 -0700

    chore(e2e): removed unessisary use statement + updated some to run parallel tests

--- app/e2e/tests/landing/landing-login.spec.ts
+++ app/e2e/tests/landing/landing-login.spec.ts
@@ -1,25 +1,23 @@
 import { test } from '@playwright/test';
 import { PlaywrightLandingPage } from '@local/common/pages/playwright-landing-page';
 
-test.describe('landing page login button', () => {
-    test.use({ storageState: undefined });
+test.describe.configure({ mode: 'parallel' });
 
-    test('I can close the login modal if I accidentally open it', async ({ page }) => {
-        const landing = new PlaywrightLandingPage(page);
-        await landing.goto();
-        await landing.clickOnLogin();
-        await landing.see(landing.loginFormModal);
-        await landing.presEscape();
-        await landing.doNotSee(landing.loginFormModal);
-    });
+test('I can close the login modal if I accidentally open it', async ({ page }) => {
+    const landing = new PlaywrightLandingPage(page);
+    await landing.goto();
+    await landing.clickOnLogin();
+    await landing.see(landing.loginFormModal);
+    await landing.presEscape();
+    await landing.doNotSee(landing.loginFormModal);
+});
 
-    test('I can login from the app bar', async ({ page }) => {
-        const landing = new PlaywrightLandingPage(page);
-        await landing.goto();
-        await landing.clickOnLogin();
-        await landing.fillInEmail('test@example.com');
-        await landing.fillInPassword('Password1!');
-        await landing.submitLoginForm();
-        await landing.amLoggedIn();
-    });
+test('I can login from the app bar', async ({ page }) => {
+    const landing = new PlaywrightLandingPage(page);
+    await landing.goto();
+    await landing.clickOnLogin();
+    await landing.fillInEmail('user1@example.com');
+    await landing.fillInPassword('Password1!');
+    await landing.submitLoginForm();
+    await landing.amLoggedIn();
 });
