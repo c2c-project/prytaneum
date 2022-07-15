cac68593234975fd9d6bd71ad9bd3ce50081d22d
kadey001 <kadey001@ucr.edu>
2022-07-14 22:02:16 -0700

    fix(e2e): tests use context based on device now, ensuring isolation

--- app/e2e/playwright.config.ts
+++ app/e2e/playwright.config.ts
@@ -28,21 +28,23 @@ const config: PlaywrightTestConfig = {
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 2 : undefined,
+    /* Amount of workers based on number of browsers being tested */
+    workers: process.env.CI ? 2 : 6,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: process.env.CI ? 'github' : [['list'], ['experimental-allure-playwright']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-        /* Used for loading login state. */
-        storageState: 'storageState.json',
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
         actionTimeout: 0,
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: 'http://localhost:8080',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
+        trace: {
+            mode: 'retain-on-failure',
+            screenshots: true,
+            snapshots: true,
+        },
     },
 
     /* Configure projects for major browsers */
