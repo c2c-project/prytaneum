5fbdfbf24bdcbafe5096b155dbc4193d5eb5f65b
DevelopmentByDavid <developmentbydavid@gmail.com>
2022-07-16 11:53:01 -0700

    feat(server): pretty print graphql strings in pino

--- app/server/src/core/server.ts
+++ app/server/src/core/server.ts
@@ -35,7 +35,7 @@ const makeDevelopmentServer = () =>
         logger: {
             level: process.env.LOG_LEVEL ?? 'debug',
             prettyPrint: {
-                translateTime: 'HH:MM:ss Z',
+                translateTime: 'SYS:HH:MM:ss Z',
                 ignore: 'pid,hostname,res,reqId,req',
             },
         },
