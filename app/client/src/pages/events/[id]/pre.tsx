4204632a091dc74a4b5f503d9bca8b87f0720219
Minsoo Kim <minsooerickim@gmail.com>
2022-10-13 18:31:47 -0700

    use EVENT_LIVE_QUERY with an added field

--- app/client/src/pages/events/[id]/pre.tsx
+++ app/client/src/pages/events/[id]/pre.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { PreloadedQuery, usePreloadedQuery, useQueryLoader } from 'react-relay';
 import { useRouter } from 'next/router';
 import type { liveQuery } from '@local/__generated__/liveQuery.graphql'
-import { EventLiveLoader } from '@local/features/events';
+import { EventLiveLoader, EVENT_LIVE_QUERY } from '@local/features/events';
 import { ConditionalRender } from '@local/components';
 import { LIVE_QUERY } from './live';
+import { EventLiveQuery } from '@local/__generated__/EventLiveQuery.graphql';
 export interface PreloadedEventLiveProps {
-    queryReference: PreloadedQuery<liveQuery>;
+    eventLiveQueryRef: PreloadedQuery<EventLiveQuery>;
 }
 
 
@@ -20,10 +21,10 @@ export async function getServerSideProps() {
     return { props: baseProps };
 }
 
-export function PrePage({queryReference}: PreloadedEventLiveProps) {
+export function PrePage({eventLiveQueryRef}: PreloadedEventLiveProps) {
     const router = useRouter()
-    const data = usePreloadedQuery<liveQuery>(LIVE_QUERY, queryReference);
-    var isActive = data.findSingleEvent?.isActive 
+    const { node } = usePreloadedQuery(EVENT_LIVE_QUERY, eventLiveQueryRef);
+    var isActive = node?.isActive
 
     console.log(isActive)
 
@@ -41,29 +42,27 @@ export function PrePage({queryReference}: PreloadedEventLiveProps) {
     )
 }
 
-export default function Pre(initialQueryRef: PreloadedQuery<liveQuery>) {
+export default function Pre() {
     const router = useRouter();
-    const id = router.query.id
+    const eventId = router.query.id
 
-    const [queryReference, loadQuery] = useQueryLoader(
-        LIVE_QUERY, initialQueryRef
-    );
+    const [eventLiveQueryRef, loadEventQuery] = useQueryLoader<EventLiveQuery>(EVENT_LIVE_QUERY);
 
     
     React.useEffect(() => {
-        loadQuery({eventId: id})
+        if (!eventLiveQueryRef) loadEventQuery({ eventId });
         const interval = setInterval(() => {
-            loadQuery({eventId: id}, {fetchPolicy: 'network-only'})
+            loadEventQuery({ eventId }, {fetchPolicy: 'network-only'})
         }, 2000);
         return () => clearInterval(interval)
-    }, [])
+    }, [eventLiveQueryRef, loadEventQuery, eventId]);
     
     return (
         <div>
             <ConditionalRender client>
                 <React.Suspense fallback={<EventLiveLoader />}>
-                    {queryReference != null && queryReference !== undefined && id !== undefined
-                        ? <PrePage queryReference={queryReference} />
+                    {eventLiveQueryRef != null && eventLiveQueryRef !== undefined && eventId !== undefined
+                        ? <PrePage eventLiveQueryRef={eventLiveQueryRef} />
                         : <h1>uh</h1>
                     }
                 </React.Suspense>
