b9f3e803d68121ebc783362dae489c32423b0caf
kadey001 <kadey001@ucr.edu>
2022-11-28 20:43:27 -0800

    fix(bigint): updated type parser and added more logging

--- app/server/src/features/events/moderation/methods.ts
+++ app/server/src/features/events/moderation/methods.ts
@@ -71,7 +71,7 @@ export async function updateQuestionPosition(userId: string, prisma: PrismaClien
             id: questionId,
         },
         data: {
-            position: BigInt(position),
+            position,
         },
     });
 }
