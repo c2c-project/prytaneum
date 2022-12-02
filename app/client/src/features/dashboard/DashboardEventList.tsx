9dd0b237b317853fedd3fa0eaa6e7962d54e13d8
Kelton <kadey001@ucr.edu>
2022-12-02 02:40:14 -0800

    Revert "Dashboard tweaks (#355)" (#378)
    This reverts commit 28b5239395eb886faa4b3cca43201fd6b7ba1f48.

--- app/client/src/features/dashboard/DashboardEventList.tsx
+++ app/client/src/features/dashboard/DashboardEventList.tsx
@@ -1,51 +1,49 @@
-import type { ReactNode } from 'react';
 import { useRouter } from 'next/router';
 import { Button, Card, CardContent, Grid, List, ListItemSecondaryAction, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
 import { DashboardEventListItem } from './DashboardEventListItem';
 import { Event } from './Dashboard';
 
-interface LiveFeedButtonProps {
-    ongoing: boolean;
-    onClick: () => void;
-}
-
-function LiveFeedButton({ ongoing, onClick }: LiveFeedButtonProps) {
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault();
-        onClick();
-    };
-
-    if (ongoing)
-        return (
-            <ListItemSecondaryAction>
-                <Button
-                    aria-label='view live feed of current event'
-                    variant='contained'
-                    color='primary'
-                    onClick={handleClick}
-                >
-                    Live Feed
-                </Button>
-            </ListItemSecondaryAction>
-        );
-    return <></>;
-}
+const useStyles = makeStyles((theme) => ({
+    item: {
+        marginBottom: theme.spacing(4),
+    },
+    card: {
+        padding: theme.spacing(1),
+    },
+    title: {
+        marginBottom: theme.spacing(1),
+    },
+    text: {
+        marginLeft: theme.spacing(1),
+    },
+}));
 
 interface DashboardEventListProps {
     eventList: Event[];
-    sectionTitle: ReactNode;
     ongoing: boolean;
 }
 
-export function DashboardEventList({ eventList, sectionTitle, ongoing }: DashboardEventListProps) {
+export function DashboardEventList({ eventList, ongoing }: DashboardEventListProps) {
+    const classes = useStyles();
     const router = useRouter();
+    const handleNav = (path: string) => () => router.push(path);
 
     return (
-        <Grid item xs={12} style={{ marginBottom: '2rem' }}>
-            <Card style={{ padding: '.5rem' }}>
+        <Grid item xs={12} className={classes.item}>
+            <Card className={classes.card}>
                 <CardContent>
-                    {sectionTitle}
+                    {ongoing ? (
+                        <Typography variant='h6' className={classes.title}>
+                            Current Events
+                        </Typography>
+                    ) : (
+                        <Typography variant='h6' className={classes.title}>
+                            Upcoming Events
+                        </Typography>
+                    )}
+
                     {eventList.length > 0 ? (
                         <List>
                             {eventList.map(({ node: event }, idx) => {
@@ -55,16 +53,28 @@ export function DashboardEventList({ eventList, sectionTitle, ongoing }: Dashboa
                                         event={event}
                                         divider={idx !== eventList.length - 1}
                                     >
-                                        <LiveFeedButton
-                                            ongoing={ongoing}
-                                            onClick={() => router.push(`/events/${event.id}/live`)}
-                                        />
+                                        {ongoing && (
+                                            <ListItemSecondaryAction>
+                                                <Button
+                                                    aria-label='view live feed of current event'
+                                                    variant='contained'
+                                                    color='primary'
+                                                    onClick={handleNav(`/events/${event.id}/live`)}
+                                                >
+                                                    Live Feed
+                                                </Button>
+                                            </ListItemSecondaryAction>
+                                        )}
+                                        {/* event.isViewerModerator && (
+                                                 // Render a badge here to indicate the user is a moderator
+                                             )
+                                        */}
                                     </DashboardEventListItem>
                                 );
                             })}
                         </List>
                     ) : (
-                        <Typography style={{ marginLeft: '.5rem' }} variant='subtitle2'>
+                        <Typography className={classes.text} variant='subtitle2'>
                             No Events To Display
                         </Typography>
                     )}
