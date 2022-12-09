43aee47e70a0d1cd22e2c9384629dfc0dd275a23
kadey001 <kadey001@ucr.edu>
2022-12-08 18:31:50 -0800

    fix(tests): updated integration test setup

--- app/server/src/features/accounts/resolver.integration.test.ts
+++ app/server/src/features/accounts/resolver.integration.test.ts
@@ -6,10 +6,6 @@ import * as plugins from '@local/core/plugins';
 import * as jwt from '@local/lib/jwt';
 import { toGlobalId } from '../utils';
 
-const server = getOrCreateServer();
-const testClient = createMercuriusTestClient(server);
-const prisma = getPrismaClient(server.log);
-
 const toUserId = toGlobalId('User');
 
 const userData = {
@@ -24,10 +20,11 @@ const userData = {
 };
 
 beforeAll(async () => {
-    // Attack relevant plugins
-    plugins.attachCookieTo(server);
+    const server = getOrCreateServer();
+    // Attach relevant plugins
     plugins.attachMercuriusTo(server);
-
+    plugins.attachCookieTo(server);
+    const prisma = getPrismaClient(server.log);
     // hash password as they are never stored in plaintext on the DB
     const encryptedPassword = await bcrypt.hash(userData.password, 10);
 
@@ -38,13 +35,20 @@ beforeAll(async () => {
             password: encryptedPassword,
         },
     });
+    await server.ready();
 });
 
 afterAll(async () => {
+    const server = getOrCreateServer();
+    const prisma = getPrismaClient(server.log);
     await prisma.user.deleteMany();
     await prisma.$disconnect();
     await server.close();
 });
+
+const server = getOrCreateServer();
+const testClient = createMercuriusTestClient(server);
+const prisma = getPrismaClient(server.log);
 
 describe('account resolvers', () => {
     describe('Query', () => {
@@ -116,7 +120,6 @@ describe('account resolvers', () => {
                 afterAll(async () => {
                     await prisma.organization.deleteMany();
                 });
-
                 test('should return list of edges with organization node', async () => {
                     // Arrange
                     const query = 'query { me { organizations { edges { node { name } } } } }';
