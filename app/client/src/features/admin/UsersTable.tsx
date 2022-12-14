fc6d2755f94b1965b3fad18af575daebaf18885d
kadey001 <kadey001@ucr.edu>
2022-12-13 19:12:14 -0800

    fix(users-table): updated filter field type and table now jus shows empty when no results found

--- app/client/src/features/admin/UsersTable.tsx
+++ app/client/src/features/admin/UsersTable.tsx
@@ -64,13 +64,7 @@ function SearchBar({ handleSearchFilter }: SearchBarProps) {
                     />
                 </Grid>
                 <Grid item>
-                    <TextField
-                        type='email'
-                        label='Email'
-                        aria-label='Email'
-                        value={form.email}
-                        onChange={handleChange('email')}
-                    />
+                    <TextField label='Email' aria-label='Email' value={form.email} onChange={handleChange('email')} />
                 </Grid>
                 <Grid item>
                     <IconButton
@@ -151,100 +145,94 @@ export function UsersTable({ fragmentRef }: UsersTableProps) {
             <Grid container justifyContent='center'>
                 <SearchBar handleSearchFilter={handleSearchFilter} />
             </Grid>
-            {usersListLength > 0 ? (
-                <TableContainer style={{ paddingLeft: '2rem', paddingRight: '2rem' }}>
-                    <Table sx={{ minWidth: 650 }} aria-label='dashboard-table'>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell style={{ width: 250 }}>
-                                    <Typography fontWeight='bold'>Name</Typography>
+            <TableContainer style={{ paddingLeft: '2rem', paddingRight: '2rem' }}>
+                <Table sx={{ minWidth: 650 }} aria-label='dashboard-table'>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell style={{ width: 250 }}>
+                                <Typography fontWeight='bold'>Name</Typography>
+                            </TableCell>
+                            <TableCell style={{ width: 250 }}>
+                                <Typography fontWeight='bold'>Email</Typography>
+                            </TableCell>
+                            <TableCell style={{ width: 150 }}>
+                                <Typography fontWeight='bold'>User Type</Typography>
+                            </TableCell>
+                            <TableCell style={{ width: 200 }}>
+                                <Typography fontWeight='bold'>Last Login Date</Typography>
+                            </TableCell>
+                            <TableCell />
+                            <TableCell />
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {(rowsPerPage > 0
+                            ? users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : users
+                        ).map((user) => (
+                            <TableRow key={user.id}>
+                                <TableCell>
+                                    <Typography>{user.firstName + ' ' + user.lastName}</Typography>
                                 </TableCell>
-                                <TableCell style={{ width: 250 }}>
-                                    <Typography fontWeight='bold'>Email</Typography>
+                                <TableCell>
+                                    <Typography>{user.email}</Typography>
                                 </TableCell>
-                                <TableCell style={{ width: 150 }}>
-                                    <Typography fontWeight='bold'>User Type</Typography>
+                                <TableCell>
+                                    <Typography>{user.canMakeOrgs ? 'Organizer' : 'Participant'}</Typography>
                                 </TableCell>
-                                <TableCell style={{ width: 200 }}>
-                                    <Typography fontWeight='bold'>Last Login Date</Typography>
+                                <TableCell>
+                                    {/* TODO: Get this from the backend */}
+                                    <Typography>
+                                        {date.toLocaleDateString() +
+                                            ' ' +
+                                            date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                                    </Typography>
                                 </TableCell>
-                                <TableCell />
-                                <TableCell />
+                                <TableCell>
+                                    <Button variant='contained' onClick={() => handleEdit(user.id)}>
+                                        Edit
+                                    </Button>
+                                </TableCell>
+                                <TableCell>
+                                    <Button variant='contained' color='error' onClick={() => handleDelete(user.id)}>
+                                        Delete
+                                    </Button>
+                                </TableCell>
                             </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {(rowsPerPage > 0
-                                ? users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                : users
-                            ).map((user) => (
-                                <TableRow key={user.id}>
-                                    <TableCell>
-                                        <Typography>{user.firstName + ' ' + user.lastName}</Typography>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Typography>{user.email}</Typography>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Typography>{user.canMakeOrgs ? 'Organizer' : 'Participant'}</Typography>
-                                    </TableCell>
-                                    <TableCell>
-                                        {/* TODO: Get this from the backend */}
-                                        <Typography>
-                                            {date.toLocaleDateString() +
-                                                ' ' +
-                                                date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-                                        </Typography>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Button variant='contained' onClick={() => handleEdit(user.id)}>
-                                            Edit
-                                        </Button>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Button variant='contained' color='error' onClick={() => handleDelete(user.id)}>
-                                            Delete
-                                        </Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                            {emptyRows > 0 && (
-                                <TableRow style={{ height: 70 * emptyRows }}>
-                                    <TableCell colSpan={6} />
-                                </TableRow>
-                            )}
-                        </TableBody>
-                        <TableFooter>
-                            <TableRow>
-                                <TablePagination
-                                    count={usersListLength}
-                                    rowsPerPage={rowsPerPage}
-                                    page={page}
-                                    onPageChange={handleChangePage}
-                                    onRowsPerPageChange={handleChangeRowsPerPage}
-                                    rowsPerPageOptions={[5, 10, 25]}
-                                    labelRowsPerPage={<span>Rows per page:</span>}
-                                    labelDisplayedRows={({ page: _page }) => {
-                                        return `Page: ${_page + 1}`;
-                                    }}
-                                    SelectProps={{
-                                        inputProps: {
-                                            'aria-label': 'page number',
-                                        },
-                                        style: { width: '4rem' },
-                                    }}
-                                    showFirstButton={true}
-                                    showLastButton={true}
-                                    ActionsComponent={TablePaginationActions}
-                                />
+                        ))}
+                        {emptyRows > 0 && (
+                            <TableRow style={{ height: 70 * emptyRows }}>
+                                <TableCell colSpan={6} />
                             </TableRow>
-                        </TableFooter>
-                    </Table>
-                </TableContainer>
-            ) : (
-                <Grid item container justifyContent='center' paddingY='2rem'>
-                    <Typography>No users found</Typography>
-                </Grid>
-            )}
+                        )}
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TablePagination
+                                count={usersListLength}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                onPageChange={handleChangePage}
+                                onRowsPerPageChange={handleChangeRowsPerPage}
+                                rowsPerPageOptions={[5, 10, 25]}
+                                labelRowsPerPage={<span>Rows per page:</span>}
+                                labelDisplayedRows={({ page: _page }) => {
+                                    return `Page: ${_page + 1}`;
+                                }}
+                                SelectProps={{
+                                    inputProps: {
+                                        'aria-label': 'page number',
+                                    },
+                                    style: { width: '4rem' },
+                                }}
+                                showFirstButton={true}
+                                showLastButton={true}
+                                ActionsComponent={TablePaginationActions}
+                            />
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+            </TableContainer>
         </React.Fragment>
     );
 }
