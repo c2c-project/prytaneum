909b19c1a643b78eca36436bd5975f75ec730753
Minsoo Kim <minsooerickim@gmail.com>
2022-12-27 16:00:46 +0900

    style(generated): fix spacing

--- app/client/src/graphql-types.ts
+++ app/client/src/graphql-types.ts
@@ -688,6 +688,7 @@ export type Mutation = {
      * returns false if an account with the provided email cannot be found
      */
     resetPasswordRequest: ResetPasswordRequestMutationResponse;
+    shareFeedbackPromptResults: EventFeedbackPromptMutationResponse;
     /** Start the event so that it is "live" */
     startEvent: EventMutationResponse;
     updateEmail: UserMutationResponse;
@@ -960,6 +961,7 @@ export type Query = {
     myFeedback?: Maybe<Array<Maybe<EventLiveFeedback>>>;
     node?: Maybe<Node>;
     prompt?: Maybe<EventLiveFeedbackPrompt>;
+    promptResponseVotes: Votes;
     promptResponses?: Maybe<Array<EventLiveFeedbackPromptResponse>>;
     prompts?: Maybe<Array<EventLiveFeedbackPrompt>>;
     questionsByEventId?: Maybe<Array<EventQuestion>>;
@@ -1060,6 +1062,7 @@ export type Subscription = {
     eventLiveFeedbackCreated: EventLiveFeedback;
     eventUpdates: Event;
     feedbackCRUD: FeedbackOperation;
+    feedbackPromptResultsShared: EventLiveFeedbackPrompt;
     feedbackPrompted: EventLiveFeedbackPrompt;
     /** subscription for whenever a new org is added */
     orgUpdated: OrganizationSubscription;
