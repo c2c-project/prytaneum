78159f0117916b594100bdd7c20ade841001e543
kadey001 <kadey001@ucr.edu>
2023-02-09 20:47:00 -0800

    fix(csv download): added check + err msg for empty lists

--- app/client/src/features/events/EventSettings/EventData.tsx
+++ app/client/src/features/events/EventSettings/EventData.tsx
@@ -2,6 +2,7 @@ import { Button } from '@mui/material';
 import { useFragment, graphql } from 'react-relay';
 
 import { EventDataFragment$key } from '@local/__generated__/EventDataFragment.graphql';
+import { useSnack } from '@local/core/useSnack';
 
 export interface EventDataProps {
     fragmentRef: EventDataFragment$key;
@@ -38,6 +39,7 @@ export const EVENT_DATA_FRAGMENT = graphql`
 
 // Converts array of objects into a CSV-formatted string.
 function toCSV(data: Object[]) {
+    if (data.length === 0) return '';
     let content = 'data:text/csv;charset=utf-8';
     const keys = Object.keys(data[0]);
 
@@ -46,11 +48,11 @@ function toCSV(data: Object[]) {
     content += '\r\n';
 
     // Load each entry
-    data.forEach(function(rowObj) {
+    data.forEach(function (rowObj) {
         let row = '';
         for (let i = 0; i < keys.length; i++) row += `${rowObj[keys[i] as keyof typeof rowObj]},`;
         content += row + '\r\n';
-    })
+    });
 
     return content;
 }
@@ -69,35 +71,51 @@ export function EventData({ fragmentRef }: EventDataProps) {
     const data = useFragment(EVENT_DATA_FRAGMENT, fragmentRef);
     const feedback = data.liveFeedback?.edges ?? [];
     const questions = data.questions?.edges ?? [];
+    const { displaySnack } = useSnack();
 
     const feedbackArray = feedback.map(({ node }) => ({
-        FirstName: node.createdBy?.firstName,
-        LastName: node.createdBy?.lastName,
-        Email: node.createdBy?.email,
-        Message: node.message,
+        FirstName: node.createdBy?.firstName || '',
+        LastName: node.createdBy?.lastName || '',
+        Email: node.createdBy?.email || '',
+        Message: node.message || '',
     }));
 
     const questionArray = questions.map(({ node }) => ({
-        FirstName: node.createdBy?.firstName,
-        LastName: node.createdBy?.lastName,
-        Email: node.createdBy?.email,
-        Question: node.question,
+        FirstName: node.createdBy?.firstName || '',
+        LastName: node.createdBy?.lastName || '',
+        Email: node.createdBy?.email || '',
+        Question: node.question || '',
     }));
 
     // Load feedback and questions into csv format
     const feedbackContent = toCSV(feedbackArray);
     const questionContent = toCSV(questionArray);
 
+    const csvDownload = (content: string, filename: string) => {
+        if (content === '') {
+            displaySnack('No data to download', { variant: 'error' });
+            return;
+        }
+        downloadCSV(content, filename);
+    };
+
     return (
         <div>
-            <Button onClick={() => { downloadCSV(feedbackContent, 'feedback.csv') }}>
+            <Button
+                onClick={() => {
+                    csvDownload(feedbackContent, 'feedback.csv');
+                }}
+            >
                 Download Event Feedback
             </Button>
             <br />
-            <Button onClick={() => { downloadCSV(questionContent, 'questions.csv') }}>
+            <Button
+                onClick={() => {
+                    csvDownload(questionContent, 'questions.csv');
+                }}
+            >
                 Download Event Questions
             </Button>
         </div>
-        
     );
 }
