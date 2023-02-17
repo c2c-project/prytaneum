b703ddcd53a17e426473d9361a586f4fec978962
michael07w <michaelwessels207@gmail.com>
2023-02-17 15:22:26 -0800

    style(frontend): Completed Participant and Moderator View section Will refactor after entire page is complete.

--- app/client/src/features/landing/Landing.tsx
+++ app/client/src/features/landing/Landing.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ReactPlayer from 'react-player';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { Box, Button, Card, CardActions, CardContent, Divider, Grid, Stack, Typography } from '@mui/material';
+import { Button, Divider, Grid, Stack, Typography } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import makeStyles from '@mui/styles/makeStyles';
 import { CallToAction } from '@local/features/landing/CallToAction'
@@ -62,18 +62,12 @@ export default function Landing() {
         <Grid container overflow='hidden' alignItems='center' justifyContent='center' spacing={2} className={classes.landing} marginTop={-10}>
             <CallToAction />
         </Grid>
-        {/* <div ref={sentinelRef} className={classes.sentinel}> */}
+        
         <Grid container alignItems='center' justifyContent='center' spacing={2} className={classes.root} style={{ backgroundColor: '#8EAFFF42' }}>
             <Blurb
                 title='Enable constructive, virtual dialogue'
-                color='#282D6E'
+                titleColor='#282D6E'
             />
-            {/*
-                <ParticipantDemo
-                    shadow='10px 10px 0 0 #f5c64f'
-                    scale='scale(0.95)'
-                />
-            */}
             <Grid item xs={12}>
                 <div className={classes.video}>
                     <ReactPlayer url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' height='480px' width='854px' />
@@ -97,219 +91,88 @@ export default function Landing() {
                 </Button>
             </Grid>
         </Grid>
-       
-        {/* This is the only section of the page that doesn't need the style prop detailed above. */}
+        
         <Grid container alignItems='center' justifyContent='center' spacing={2} className={classes.root}>
             <Blurb
                 title='A better solution for remote public engagement.'
-                color='#F5C64F'
+                paragraphs={['See how Prytaneum enables interaction through our town hall platform:']}
+                titleColor='#F5C64F'
+                paragraphsColor='#272C6C'
             />
-            <Blurb paragraphs={['See how Prytaneum enables interaction through our town hall platform:']} color='#272C6C' />
             {/*<Carousel cards={views} />*/}
+            <Grid container alignItems='center' justifyContent='center' wrap='nowrap' marginTop={5}>
+                <Grid item>
+                    <Grid container wrap='nowrap'>
+                        <PersonIcon fontSize='large' style={{ color: '#F5C64F', marginTop: 11 }} />
+                        <Typography variant='h4' color='#F5C64F' fontSize='40px'>
+                            Participant View
+                        </Typography>
+                    </Grid>
+                    <Typography variant='body1' color='#272C6C' fontSize='18px' align='center' width='80%'>
+                        Sit in on Prytaneum events as a participant and contribute to event discussion
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Image
+                        data-test-id='participant-view'
+                        alt='Participant View Screenshot'
+                        src='/static/participant_view.svg'
+                        width={1114}
+                        height={576}
+                        objectFit='contain'
+                    />
+                </Grid>
+            </Grid>
+            <Grid container alignItems='center' justifyContent='center' wrap='nowrap' marginTop={10}>
+                <Grid item>
+                    <Image
+                        data-test-id='moderator-view'
+                        alt='Moderator View Screenshot'
+                        src='/static/moderator_view.svg'
+                        width={1114}
+                        height={576}
+                        objectFit='contain'
+                    />
+                </Grid>
+                <Grid item marginRight={-10} marginLeft={10}>
+                    <Grid container wrap='nowrap'>
+                        <Image 
+                            alt='Moderator Icon'
+                            src='/static/moderator_icon.svg' 
+                            width={36} 
+                            height={26}
+                            objectFit='contain' 
+                        />
+                        <Typography variant='h4' color='#F5C64F' fontSize='40px'>
+                            Moderator View
+                        </Typography>
+                    </Grid>
+                    <Typography variant='body1' color='#272C6C' fontSize='18px' align='center' width='80%' marginLeft={-2}>
+                        or take charge of your own Prytaneum event as a moderator and administrate event discussion
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Grid>
+
+        <Grid container alignItems='center' justifyContent='center' spacing={2} className={classes.root}>
+            <Blurb
+                paragraphs={
+                    [
+                        'Just like any town hall, Prytaneum offers roles to fit the needs of any attendee: organizer, speaker, moderator, moderator assistant, and participant. Prytaneum complements these roles by promoting constructive engagement through the user interface and “pro-social” algorithm.',
+                    ]
+                }
+            />
+            <Carousel cards={roles} />
+        </Grid>
+
+        <Grid container alignItems='center' justifyContent='center' spacing={3} className={classes.root}>
             <Grid item>
-                <PersonIcon fontSize='large' />
                 <Image
-                    data-test-id='participant-view'
-                    alt='Participant View Screenshot'
-                    src='/static/participant_view.svg'
-                    width={557}
-                    height={288}
+                    src='/static/democracy_fund_logo.svg'
+                    width={280}
+                    height={200}
                     objectFit='contain'
-                />
-            </Grid>
-        </Grid>
-
-        <Grid container alignItems='center' justifyContent='center' spacing={2} className={classes.root} style={{ backgroundColor: '#F5C64F42' }}>
-            <Grid item width='100%' marginTop={8}>
-                <Blurb title='Participate in town hall events using roles' />
-            </Grid>
-
-            <Grid item width='25%' zIndex={500} marginTop={5} marginBottom={20}>
-                <Card sx={{ height: 520, backgroundColor: '#F5C64F', borderRadius: 0 }}>
-                    <CardContent>
-                        <Grid container display='flex' alignItems='center' justifyContent='center'>
-                            <Image 
-                                alt='Speaker Icon'
-                                src='/static/speaker_icon.svg' 
-                                width={82} 
-                                height={80}
-                                objectFit='contain'
-                            />
-                        </Grid>
-                        <Blurb 
-                            title='Speaker' 
-                            titleColor='#F9F9F9' 
-                            paragraphs={['Officials who answer the discussion given by moderators.']}
-                        />
-                        <Typography sx={{ fontSize: 18, marginTop: 10}}>
-                            <Box
-                                component='span'
-                                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-                            >
-                                •
-                            </Box>
-                            Same experience as a Zoom call
-                        </Typography>
-                        <Typography sx={{ fontSize: 18, marginBottom: 15 }}>
-                            <Box
-                                component='span'
-                                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-                            >
-                                •
-                            </Box>
-                            One on one with moderators
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Grid>
-
-            <Grid item width='25%' zIndex={500} marginTop={5} marginBottom={20}>
-                <Card sx={{ height: 520, backgroundColor: '#F5C64F', borderRadius: 0, display: 'flex', flexDirection: 'column' }}>
-                    <CardContent>
-                        <Grid container display='flex' alignItems='center' justifyContent='center'>
-                            <Image 
-                                alt='Participant Icon'
-                                src='/static/participant_icon.svg' 
-                                width={82} 
-                                height={80}
-                                objectFit='contain'
-                            />
-                        </Grid>
-                        <Blurb 
-                            title='Participant' 
-                            titleColor='#F9F9F9' 
-                            paragraphs={['Residents who engage in discussion on a policy topic.']}
-                        />
-                        <Typography sx={{ fontSize: 18, marginTop: 10}}>
-                            <Box
-                                component='span'
-                                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-                            >
-                                •
-                            </Box>
-                            Submit questions
-                        </Typography>
-                        <Typography sx={{ fontSize: 18 }}>
-                            <Box
-                                component='span'
-                                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-                            >
-                                •
-                            </Box>
-                            Like and quote other participant questions
-                        </Typography>
-                        <Typography sx={{ fontSize: 18, marginBottom: 0 }}>
-                            <Box
-                                component='span'
-                                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-                            >
-                                •
-                            </Box>
-                            Provide feedback to moderators
-                        </Typography>
-                    </CardContent>
-                    <CardActions sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 'auto', marginBottom: 2 }}>
-                        <Button size='medium' style={{ color: 'white' }}>Participant Guide</Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-
-            <Grid item width='25%' zIndex={500} marginTop={5} marginBottom={20}>
-                <Card sx={{ height: 520, backgroundColor: '#F5C64F', borderRadius: 0, display: 'flex', flexDirection: 'column' }}>
-                    <CardContent>
-                        <Grid container display='flex' alignItems='center' justifyContent='center'>
-                            <Image 
-                                alt='Moderator Icon'
-                                src='/static/moderator_icon.svg' 
-                                width={82}
-                                height={80}
-                                objectFit='contain'
-                            />
-                        </Grid>
-                        <Blurb 
-                            title='Moderator' 
-                            titleColor='#F9F9F9' 
-                            paragraphs={['Officials that oversee event discussion with the speaker.']}
-                        />
-                        <Typography sx={{ fontSize: 18, marginTop: 10}}>
-                            <Box
-                                component='span'
-                                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-                            >
-                                •
-                            </Box>
-                            Manage the question queue by adding, removing and reordering participant questions
-                        </Typography>
-                        <Typography sx={{ fontSize: 18  }}>
-                            <Box
-                                component='span'
-                                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-                            >
-                                •
-                            </Box>
-                            Respond to participant feedback
-                        </Typography>
-                    </CardContent>
-                    <CardActions sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 'auto', marginBottom: 2 }}>
-                        <Button size='medium' style={{ color: 'white' }}>Moderator Guide</Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-
-            <Grid item width='25%' zIndex={500} marginTop={5} marginBottom={20}>
-                <Card sx={{ height: 520, backgroundColor: '#F5C64F', borderRadius: 0, display: 'flex', flexDirection: 'column' }}>
-                    <CardContent>
-                        <Grid container display='flex' alignItems='center' justifyContent='center'>
-                            <Image 
-                                alt='Organizer Icon'
-                                src='/static/organizer_icon.svg' 
-                                width={82}
-                                height={80}
-                                objectFit='contain'
-                            />
-                        </Grid>
-                        <Blurb 
-                            title='Organizer' 
-                            titleColor='#F9F9F9' 
-                            paragraphs={['Officials that create and set up events on Prytaneum.']}
-                        />
-                        <Typography sx={{ fontSize: 18, marginTop: 10}}>
-                            <Box
-                                component='span'
-                                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-                            >
-                                •
-                            </Box>
-                            Create events for an organization
-                        </Typography>
-                        <Typography sx={{ fontSize: 18 }}>
-                            <Box
-                                component='span'
-                                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-                            >
-                                •
-                            </Box>
-                            Configure event details
-                        </Typography>
-                    </CardContent>
-                    <CardActions sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 'auto', marginBottom: 2 }}>
-                        <Button size='medium' style={{ color: 'white' }}>Organizer Guide</Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-
-            <Grid item height={40} width='80%' marginTop={-110} zIndex={1000} display='flex' alignItems='center' justifyContent='center' style={{ backgroundColor: '#D2A942' }}>
-                <Typography variant='h4' color='white' fontSize={20} marginBottom={2}>Features</Typography>
-            </Grid>
-        </Grid>
-
-        <Grid container display='flex' alignItems='center' justifyContent='center' spacing={2} className={classes.root}>
-            <Grid item marginTop={2} marginBottom={1}>
-                <Blurb 
-                    title='Read more on how Prytaneum came to be,'
-                    paragraphs={['the team behind the online town hall platform, and our motivations.']}
-                    titleColor='#F5C64F'
-                    paragraphsColor='#272C6C'
+                    alt='democracy fund logo'
                 />
             </Grid>
             <Grid item>
