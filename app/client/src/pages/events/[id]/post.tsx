952533fa5fae85a1a87fd22ba1bd89c7f1e4e642
Minsoo Kim <minsooerickim@gmail.com>
2023-02-18 22:38:53 -0800

    style(frontend) add card

--- app/client/src/pages/events/[id]/post.tsx
+++ app/client/src/pages/events/[id]/post.tsx
@@ -1,5 +1,31 @@
+import { Card, CardContent, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+    item: {
+        marginBottom: theme.spacing(4),
+    },
+    card: {
+        padding: theme.spacing(1),
+    },
+    title: {
+        marginBottom: theme.spacing(1),
+    },
+    text: {
+        marginLeft: theme.spacing(1),
+    },
+}));
+
 export default function Pre() {
+    const classes = useStyles();
     return (
-        <h1>post-event page</h1>
-    )
+        <Card className={classes.card}>
+            <CardContent>
+                <Typography variant='h6' className={classes.title}>
+                    Event Ended
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 }
