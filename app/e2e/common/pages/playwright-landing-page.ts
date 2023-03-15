ba0a732ff4001d3ac042d30c178a917820d0a60e
kadey001 <kadey001@ucr.edu>
2023-03-14 17:17:52 -0700

    fix(e2e): changed waitForNavigation to waitForTimeout due to firefox bug

--- app/e2e/common/pages/playwright-landing-page.ts
+++ app/e2e/common/pages/playwright-landing-page.ts
@@ -105,20 +105,20 @@ export class PlaywrightLandingPage {
         const loginButton = this.page.getByRole('button', { name: 'Login' });
         await expect(loginButton).toBeVisible();
         await loginButton.click();
-        await this.page.waitForNavigation();
+        await this.page.waitForTimeout(5000); // Using this over waitForNavigation because it was not working with firefox.
     }
 
     async submitRegisterForm() {
         const registerButton = this.page.getByRole('button', { name: 'Register' });
         await expect(registerButton).toBeVisible();
         await registerButton.click();
-        await this.page.waitForNavigation();
+        await this.page.waitForTimeout(5000); // Using this over waitForNavigation because it was not working with firefox.
     }
 
     async clickOnLargeRegisterButton() {
         await expect(this.largeRegisterButton).toBeVisible();
         await this.largeRegisterButton.click();
-        await this.page.waitForNavigation();
+        await this.page.waitForTimeout(5000); // Using this over waitForNavigation because it was not working with firefox.
     }
     // Route Checks
     async amLoggedIn() {
