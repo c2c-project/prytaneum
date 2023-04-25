641b2b1fe6ac407f0910b52abb95459e568ad211
Kelton <kadey001@ucr.edu>
2023-04-25 11:41:20 -0700

    React-Player Fix (#423)
    * fix(react-player): updated package so live youtube urls to work
    
    * chore(react-player): set to muted by default to allow autoplay on chrome

--- app/client/src/components/VideoPlayer/VideoPlayer.tsx
+++ app/client/src/components/VideoPlayer/VideoPlayer.tsx
@@ -42,6 +42,7 @@ export function VideoPlayer({ url, rest }: ReactPlayerProps) {
                     <ReactPlayer
                         url={url}
                         playing={process.env.NODE_ENV === 'production'}
+                        muted
                         width='100%'
                         height='100%'
                         playsinline
