3d51f95e8e4f8d2e1cf1242c8d13ba6860f8835e
kadey001 <kadey001@ucr.edu>
2023-05-09 18:16:39 -0700

    fix(countdown): updated formatting and labeling

--- app/client/src/components/Countdown.tsx
+++ app/client/src/components/Countdown.tsx
@@ -1,36 +1,37 @@
-import Countdown from 'react-countdown'
+import Countdown from 'react-countdown';
+import { Typography } from '@mui/material';
 
 const styles = {
-  spaceBetween: {
-    display: 'flex',
-    justifyContent:'space-between'
-  },
-  inline: {
-    display: 'inline-block',
-  },
-  textCenter: {
-    textAlign: 'center' as 'center'
-  }
-}
+    spaceEvenly: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+    },
+    inline: {
+        display: 'inline-block',
+    },
+    textCenter: {
+        textAlign: 'center' as 'center',
+    },
+};
 
 interface TimeBlockProps {
-  /** Decides when to show a particular time block (e.g. if 0 days are left, time blocks for days are hidden). */
-  condition: boolean
-  /** Number of seconds, minutes, hours, or days. */
-  num: string
-  /** Label for the time blocks (e.g. 'days', 'seconds'). */
-  label: string
-  /** Decides when to show a separator after a pair of time blocks. */
-  separator?: boolean
+    /** Decides when to show a particular time block (e.g. if 0 days are left, time blocks for days are hidden). */
+    condition: boolean;
+    /** Number of seconds, minutes, hours, or days. */
+    num: string;
+    /** Label for the time blocks (e.g. 'days', 'seconds'). */
+    label: string;
+    /** Decides when to show a separator after a pair of time blocks. */
+    separator?: boolean;
 }
 
 export interface rendererProps {
-  /** Start date & time of the event */
-  days: Date | string | number;
-  hours: Date | string | number;
-  minutes: Date | string | number;
-  seconds: Date | string | number;
-  completed: boolean;
+    /** Start date & time of the event */
+    days: Date | string | number;
+    hours: Date | string | number;
+    minutes: Date | string | number;
+    seconds: Date | string | number;
+    completed: boolean;
 }
 
 export interface CountdownWrapperProps {
@@ -39,80 +40,60 @@ export interface CountdownWrapperProps {
 
 /** Blocks representing a unit of time in XX format. */
 function TimeBlock({ condition, num, label, separator }: TimeBlockProps) {
-  return (
-    <>
-      {condition &&
-      <>
-        <div>
-          <div>
-            { Array.from(num).map((n, idx) =>
-              <div key={label+String(idx)} style={styles.inline}>{n}</div>
+    return (
+        <>
+            {condition && (
+                <>
+                    <div>
+                        <Typography variant='subtitle2'>{label}</Typography>
+                        <div>
+                            {Array.from(num).map((n, idx) => (
+                                <Typography variant='h3' key={label + String(idx)} style={styles.inline}>
+                                    {n}
+                                </Typography>
+                            ))}
+                        </div>
+                    </div>
+                    <div style={{ marginTop: '20px' }}>{separator && <Typography variant='h3'> : </Typography>}</div>
+                </>
             )}
-          </div>
-          <p>{label}</p>
-        </div>
-        { separator && 
-          <div> : </div> }
-      </>}
-    </>
-  )
+        </>
+    );
 }
 
 const CompletedComponent = () => (
-  <div>
-    <h2>Please wait for the organizers to start the event.</h2>
-  </div>
-)
+    <div>
+        <Typography variant='h5'>Please wait for the organizers to start the event.</Typography>
+    </div>
+);
 
 const renderer = ({ days, hours, minutes, seconds, completed }: rendererProps) => {
-  if (completed) {
-    return <CompletedComponent />
-  } 
-  else {
-    const numDays = days < 10 ? String(`0${days}`) : String(days)
-    const numHours = hours < 10 ? String(`0${hours}`) : String(hours)
-    const numMinutes = minutes < 10 ? String(`0${minutes}`) : String(minutes)
-    const numSeconds = seconds < 10 ? String(`0${seconds}`) : String(seconds)
+    if (completed) {
+        return <CompletedComponent />;
+    } else {
+        const numDays = days < 10 ? String(`0${days}`) : String(days);
+        const numHours = hours < 10 ? String(`0${hours}`) : String(hours);
+        const numMinutes = minutes < 10 ? String(`0${minutes}`) : String(minutes);
+        const numSeconds = seconds < 10 ? String(`0${seconds}`) : String(seconds);
 
-    return (
-      <div style={styles.textCenter}>
-        <h3>
-          Event will start in
-        </h3>
-        <div style={styles.spaceBetween}>
-          <TimeBlock
-            condition={Boolean(days > 0)}
-            num={numDays}
-            label='days'
-            separator
-          />
-          <TimeBlock
-            condition={Boolean(days > 0 || hours > 0)}
-            num={numHours}
-            label='hours'
-            separator
-          />
-          <TimeBlock
-            condition={Boolean(days > 0 || hours > 0 || minutes > 0)}
-            num={numMinutes}
-            label='mins'
-            separator
-          />
-          <TimeBlock
-            condition={Boolean(days > 0 || hours > 0 || minutes > 0 || seconds > 0)}
-            num={numSeconds}
-            label='secs'
-          />
-        </div>
-      </div>
-    )
-  }
-}
+        return (
+            <div style={styles.textCenter}>
+                <Typography variant='h6'>Event Countdown</Typography>
+                <div style={styles.spaceEvenly}>
+                    <TimeBlock condition={true} num={numDays} label='days' separator />
+                    <TimeBlock condition={true} num={numHours} label='hours' separator />
+                    <TimeBlock condition={true} num={numMinutes} label='mins' separator />
+                    <TimeBlock condition={true} num={numSeconds} label='secs' />
+                </div>
+            </div>
+        );
+    }
+};
 
 export function CountdownWrapper({ date }: CountdownWrapperProps) {
-  return (
-    <h2>
-      <Countdown date={date} renderer={renderer} />
-    </h2>
-  )
+    return (
+        <h2>
+            <Countdown date={date} renderer={renderer} />
+        </h2>
+    );
 }
