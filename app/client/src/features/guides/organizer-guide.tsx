4654c810f5c9015a00d41a51b73e99bd088412e5
kadey001 <kadey001@ucr.edu>
2023-06-06 16:45:20 -0700

    chore(styles): fixed guide img size bug, removed makeStyles from guides+aboutus

--- app/client/src/features/guides/organizer-guide.tsx
+++ app/client/src/features/guides/organizer-guide.tsx
@@ -1,236 +1,161 @@
 import * as React from 'react';
-import { Grid, Link, Typography } from '@mui/material';
+import { Grid, Link, Typography, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import BookOutlinedIcon from '@mui/icons-material/BookOutlined';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import makeStyles from '@mui/styles/makeStyles';
+
 import { useUser } from '../accounts';
 import { Loader } from '@local/components';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        minHeight: '85vh',
-        [theme.breakpoints.down('lg')]: {
-            paddingLeft: 0,
-        },
-        [theme.breakpoints.up('lg')]: {
-            paddingLeft: 250,
-        },
-        scrollPaddingTop: '-10rem',
-    },
-    rootLoggedOut: {
-        width: '100%',
-        [theme.breakpoints.down('lg')]: {
-            paddingLeft: 0,
-            paddingRight: 0,
-        },
-        [theme.breakpoints.up('lg')]: {
-            paddingLeft: '10%',
-            paddingRight: '10%',
-        },
-        minHeight: '85vh',
-        scrollPaddingTop: '-10rem',
-    },
-    heading: {
-        paddingTop: '80px',
-    },
-    paragraph: {
-        fontSize: '18px',
-    },
-    section: {
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '0.5rem',
-    },
-    centeredSection: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: '0.5rem',
-    },
-    formWidth: {
-        maxWidth: '560',
-    },
-    icon: {
-        fontSize: '2.25rem',
-    },
-    paper: {
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '1rem',
-        width: '100%',
-        maxWidth: 425,
-        padding: theme.spacing(2),
-    },
-    contain: {
-        objectFit: 'contain',
-        maxWidth: '250px',
-    },
-}));
-
 export default function OrganizerGuide() {
-    const classes = useStyles();
+    const theme = useTheme();
+    const lgUpBreakpoint = useMediaQuery(theme.breakpoints.up('lg'));
     const { user, isLoading } = useUser();
 
     if (isLoading) return <Loader />;
 
-    // const [open, setOpen] = React.useState(false);
-    // const [infoIndex, setInfoIndex] = React.useState(0);
-
-    // const info = [
-    //     {
-    //         title: 'Meeting Window',
-    //         text: 'The window where you will see the speaker(s) and moderator(s).',
-    //     },
-    // ];
-
-    // const handleClickOpen = (index: number) => {
-    //     setInfoIndex(index - 1);
-    //     setOpen(true);
-    // };
-
-    // const handleClose = () => {
-    //     setOpen(false);
-    // };
-
     return (
         <Grid
             container
+            width='100%'
+            height='100%'
             alignItems='center'
-            className={user ? classes.root : classes.rootLoggedOut}
             justifyContent='center'
+            paddingLeft={user ? (lgUpBreakpoint ? '250px' : 0) : lgUpBreakpoint ? '5%' : 0}
+            paddingRight={user ? (lgUpBreakpoint ? '5%' : 0) : 0}
             spacing={4}
+            sx={{ scrollPaddingTop: '-10rem' }}
         >
             <Grid item xs={4} sm={3}>
                 <img
-                    className={classes.contain}
+                    style={{ objectFit: 'contain' }}
                     src='/static/prytaneum_logo.svg'
                     width='100%'
                     height='100%'
                     alt='Prytaneum Logo'
                 />
             </Grid>
-            <Grid item xs={12} sm={9} className={classes.section}>
+            <Grid item xs={12} sm={9} display='flex' flexDirection='column' gap='0.5rem'>
                 <Typography variant='h3'>
-                    <BookOutlinedIcon className={classes.icon} /> <b>Guide to Prytaneum:</b> Organizer
+                    <BookOutlinedIcon style={{ fontSize: '2.25rem' }} /> <b>Guide to Prytaneum:</b> Organizer
                 </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
+                <Typography variant='body1' fontSize='large'>
                     As an organizer, you maintain and set up events for your organizations.
                 </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
+                <Typography variant='body1' fontSize='large'>
                     This article covers:
                 </Typography>
                 <Link href='#organizations-page'>Organization&#39;s Page</Link>
                 <Link href='#managing-organizations'>Managing Organizations</Link>
                 <Link href='#managing-events'>Managing Events/Event Settings</Link>
             </Grid>
-            <Grid item xs={12} className={classes.section}>
-                <Typography variant='h4' id='organizations-page' className={classes.heading}>
+            <Grid item xs={12} display='flex' flexDirection='column' gap='0.5rem'>
+                <Typography variant='h4' id='organizations-page' paddingTop='80px'>
                     Organization&#39;s Page
                 </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
+                <Typography variant='body1' fontSize='large'>
                     <b>My Organizations</b> is a page where you can find and view your organizations. You are also able
                     to view and manage your organization&#39;s events and members, as well as create and delete
                     organizations. To delete an organization, corresponding X to the right of the organization&#39;s
                     name.
                 </Typography>
             </Grid>
-            <Grid item xs={12} className={classes.centeredSection}>
+            <Grid item xs={12} display='flex' flexDirection='column' alignItems='center' gap='0.5rem'>
                 <img
                     src='https://storage.googleapis.com/prytaneum.io/static/guide/org-list-page.png'
                     width='100%'
-                    className={classes.contain}
+                    style={{ objectFit: 'contain' }}
                     alt='Org List Page'
                 />
             </Grid>
-            <Grid item xs={12} className={classes.section}>
-                <Typography variant='body1' className={classes.paragraph}>
+            <Grid item xs={12} display='flex' flexDirection='column' gap='0.5rem'>
+                <Typography variant='body1' fontSize='large'>
                     If you don&#39;t have an organization yet, you can create one by clicking the{' '}
-                    <img src='/static/fab.svg' width={27} className={classes.contain} alt='Fab Icon' /> in the bottom
-                    right corner of the page. Selecting an organization will show you the organization&#39;s events and
-                    members. Here, you can create new events and invite new members.
+                    <img src='/static/fab.svg' width={27} style={{ objectFit: 'contain' }} alt='Fab Icon' /> in the
+                    bottom right corner of the page. Selecting an organization will show you the organization&#39;s
+                    events and members. Here, you can create new events and invite new members.
                 </Typography>
             </Grid>
-            <Grid item xs={12} className={classes.centeredSection}>
+            <Grid item xs={12} display='flex' flexDirection='column' alignItems='center' gap='0.5rem'>
                 <img
                     src='https://storage.googleapis.com/prytaneum.io/static/guide/event-list-page.png'
                     width='100%'
-                    className={classes.contain}
+                    style={{ objectFit: 'contain' }}
                     alt='Event List Page'
                 />
             </Grid>
-            <Grid item xs={12} className={classes.section}>
-                <Typography variant='h4' id='managing-organizations' className={classes.heading}>
+            <Grid item xs={12} display='flex' flexDirection='column' gap='0.5rem'>
+                <Typography variant='h4' id='managing-organizations' paddingTop='80px'>
                     Managing Organizations
                 </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
+                <Typography variant='body1' fontSize='large'>
                     Clicking <b>+NEW EVENT</b> in the Events card will bring up the ceate event menu. Enter your event
                     information and finalize by clicking <b>CREATE</b>. Once you&#39;ve created your event, you can
                     access and modify event information by clicking on your event in the Events card. Learn more about{' '}
                     <Link href='#managing-events'>Event Settings</Link> and controls for moderators and participants.
                 </Typography>
             </Grid>
-            <Grid item xs={12} className={classes.centeredSection}>
-                <div className={classes.formWidth}>
+            <Grid item xs={12} display='flex' flexDirection='column' alignItems='center' gap='0.5rem'>
+                <div style={{ maxWidth: '560px' }}>
                     <img
                         src='https://storage.googleapis.com/prytaneum.io/static/guide/create-event-form.png'
                         width='100%'
-                        className={classes.contain}
+                        style={{ objectFit: 'contain' }}
                         alt='Create Event Form'
                     />
                 </div>
             </Grid>
-            <Grid item xs={12} className={classes.section}>
-                <Typography variant='body1' className={classes.paragraph}>
+            <Grid item xs={12} display='flex' flexDirection='column' gap='0.5rem'>
+                <Typography variant='body1' fontSize='large'>
                     Clicking <b>+NEW MEMBER</b> in the Members card will bring up the member form menu. Enter the email
                     of the new member you want to invite and finalize by clicking <b>INVITE</b> add members to the
                     organization via email.
                 </Typography>
             </Grid>
-            <Grid item xs={12} className={classes.centeredSection}>
-                <div className={classes.formWidth}>
+            <Grid item xs={12} display='flex' flexDirection='column' alignItems='center' gap='0.5rem'>
+                <div style={{ maxWidth: '560px' }}>
                     <img
+                        style={{ objectFit: 'contain' }}
                         src='https://storage.googleapis.com/prytaneum.io/static/guide/org-add-member-form.png'
                         width='100%'
-                        className={classes.contain}
                         alt='Org Add Member Form'
                     />
                 </div>
             </Grid>
-            <Grid item xs={12} className={classes.section}>
-                <Typography variant='h4' id='managing-events' className={classes.heading}>
+            <Grid item xs={12} display='flex' flexDirection='column' gap='0.5rem'>
+                <Typography variant='h4' id='managing-events' paddingTop='80px'>
                     Managing Events/Event Settings
                 </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
+                <Typography variant='body1' fontSize='large'>
                     Clicking on one of your organization&#39;s events will bring you to the event&#39;s settings page.
                     This is where you can modify event details, general settings, add speakers and moderators, create
                     invitations, modify the video stream, and delete the event.
                 </Typography>
             </Grid>
-            <Grid item xs={12} className={classes.centeredSection}>
+            <Grid item xs={12} display='flex' flexDirection='column' alignItems='center' gap='0.5rem'>
                 <img
+                    style={{ objectFit: 'contain' }}
                     src='https://storage.googleapis.com/prytaneum.io/static/guide/event-settings.png'
                     width='100%'
-                    className={classes.contain}
                     alt='Event Settings'
                 />
             </Grid>
-            <Grid item xs={12} className={classes.section}>
-                <Typography variant='body1' className={classes.paragraph}>
+            <Grid item xs={12} display='flex' flexDirection='column' gap='0.5rem'>
+                <Typography variant='body1' fontSize='large'>
                     General settings include question feed visibility, user ratings, event forums, and event
                     privatization.
                 </Typography>
             </Grid>
-            <Grid item xs={12} className={classes.centeredSection}>
+            <Grid item xs={12} display='flex' flexDirection='column' alignItems='center' gap='0.5rem'>
                 <img
+                    style={{ objectFit: 'contain' }}
                     src='https://storage.googleapis.com/prytaneum.io/static/guide/event-settings-1.png'
                     width='100%'
-                    className={classes.contain}
                     alt='Event Settings'
                 />
             </Grid>
-            <Grid item xs={12} className={classes.section}>
-                <Typography variant='body1' className={classes.paragraph}>
+            <Grid item xs={12} display='flex' flexDirection='column' gap='0.5rem'>
+                <Typography variant='body1' fontSize='large'>
                     Click on the respective buttons to add video streams, speakers, and moderators. Video streams
                     require a language and video link. To add a speaker, you will need their name, title, email, a
                     description ofthe speaker, and a link to a picture of the speaker. To add a moderator, you will need
@@ -238,25 +163,25 @@ export default function OrganizerGuide() {
                     video stream, speaker, or moderator by clicking the associated <MoreVertIcon />.
                 </Typography>
             </Grid>
-            <Grid item xs={12} className={classes.centeredSection}>
+            <Grid item xs={12} display='flex' flexDirection='column' alignItems='center' gap='0.5rem'>
                 <img
+                    style={{ objectFit: 'contain' }}
                     src='https://storage.googleapis.com/prytaneum.io/static/guide/event-settings-2.png'
                     width='100%'
-                    className={classes.contain}
                     alt='Event Settings'
                 />
             </Grid>
-            <Grid item xs={12} className={classes.section}>
-                <Typography variant='body1' className={classes.paragraph}>
+            <Grid item xs={12} display='flex' flexDirection='column' gap='0.5rem'>
+                <Typography variant='body1' fontSize='large'>
                     Invite participants through email or by creating an invite link. Delete your event by entering the
                     event name and confirming the deletion.
                 </Typography>
             </Grid>
-            <Grid item xs={12} className={classes.centeredSection}>
+            <Grid item xs={12} display='flex' flexDirection='column' alignItems='center' gap='0.5rem'>
                 <img
+                    style={{ objectFit: 'contain' }}
                     src='https://storage.googleapis.com/prytaneum.io/static/guide/event-settings-3.png'
                     width='100%'
-                    className={classes.contain}
                     alt='Event Settings'
                 />
             </Grid>
--- app/client/src/pages/aboutus.tsx
+++ app/client/src/pages/aboutus.tsx
@@ -1,62 +1,33 @@
 import * as React from 'react';
-import { Grid, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { Grid, Typography, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
 import { useUser } from '@local/features/accounts';
 import { Loader } from '@local/components';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        height: '100%',
-        [theme.breakpoints.down('lg')]: {
-            paddingLeft: 0,
-        },
-        [theme.breakpoints.up('lg')]: {
-            paddingLeft: 250,
-        },
-        textAlign: 'center',
-        alignContent: 'flex-start',
-    },
-    rootLoggedOut: {
-        width: '100%',
-        [theme.breakpoints.down('lg')]: {
-            paddingLeft: 0,
-            paddingRight: 0,
-        },
-        [theme.breakpoints.up('lg')]: {
-            paddingLeft: '5%',
-            paddingRight: '5%',
-        },
-        minHeight: '85vh',
-        scrollPaddingTop: '-10rem',
-    },
-    section: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: '1rem',
-        textAlign: 'center',
-    },
-    paragraph: {
-        fontSize: 'large',
-    },
-    contain: {
-        objectFit: 'contain',
-    },
-}));
-
 export default function AboutUs() {
-    const classes = useStyles();
+    const theme = useTheme();
+    const lgUpBreakpoint = useMediaQuery(theme.breakpoints.up('lg'));
     const { user, isLoading } = useUser();
 
     if (isLoading) return <Loader />;
 
     return (
-        <Grid container className={user ? classes.root : classes.rootLoggedOut} alignItems='center' spacing={2}>
+        <Grid
+            container
+            width='100%'
+            height='100%'
+            textAlign='center'
+            alignItems='center'
+            alignContent='flex-start'
+            paddingLeft={user ? (lgUpBreakpoint ? '250px' : 0) : lgUpBreakpoint ? '5%' : 0}
+            paddingRight={user ? 0 : lgUpBreakpoint ? '5%' : 0}
+            spacing={2}
+        >
             <Grid container item xs={12} direction='column' alignItems='center' spacing={1}>
                 <Grid item xs={4} sm={3} md={2}>
                     <img
-                        className={classes.contain}
+                        css={{ objectFit: 'contain' }}
                         src='/static/prytaneum_logo.svg'
                         width='250px'
                         height='250px'
@@ -64,7 +35,7 @@ export default function AboutUs() {
                     />
                 </Grid>
                 <Grid item>
-                    <Typography variant='body1' className={classes.paragraph}>
+                    <Typography variant='body1' fontSize='large'>
                         Technology to Enhance Democracy
                     </Typography>
                 </Grid>
@@ -75,7 +46,7 @@ export default function AboutUs() {
             <Grid container item xs={12} alignItems='center' justifyContent='center' spacing={2}>
                 <Grid item sm={5} md={3}>
                     <img
-                        className={classes.contain}
+                        css={{ objectFit: 'contain' }}
                         src='/static/directors.png'
                         width='250px'
                         height='250px'
@@ -84,14 +55,14 @@ export default function AboutUs() {
                 </Grid>
                 <Grid container item sm={7} md={9} spacing={2}>
                     <Grid item>
-                        <Typography variant='body1' className={classes.paragraph}>
+                        <Typography variant='body1' fontSize='large'>
                             Built by the University of California, Riverside&apos;s Technology, Communication and
                             Democracy Lab (TeCD-Lab), the Prytaneum was spearheaded by lab director Prof.{' '}
                             <b>Kevin Esterling</b> and associate director Prof. <b>Mariam Salloum</b>.
                         </Typography>
                     </Grid>
                     <Grid item>
-                        <Typography variant='body1' className={classes.paragraph}>
+                        <Typography variant='body1' fontSize='large'>
                             In collaboration with the Congressional Management Foundation, Prof. Esterling and Prof.
                             Salloum co-led the Connecting to Congress (C2C) research team that received a grant from the
                             National Science Foundation (NSF) to work with the U.S. Congress on developing new
@@ -101,7 +72,7 @@ export default function AboutUs() {
                     </Grid>
                 </Grid>
                 <Grid item>
-                    <Typography variant='body1' className={classes.paragraph}>
+                    <Typography variant='body1' fontSize='large'>
                         With seed funding from the Democracy Fund, the UCR TeCD-Lab developed the{' '}
                         <b>Prytaneum project</b>, the result of over a decade of research into the best practices for
                         online town halls. This new, open source online town hall platform meets the requirements
@@ -111,14 +82,14 @@ export default function AboutUs() {
                     </Typography>
                 </Grid>
                 <Grid item>
-                    <Typography variant='body1' className={classes.paragraph}>
+                    <Typography variant='body1' fontSize='large'>
                         The C2C research team published their findings from the NSF study, Politics with the People:
                         Building a Directly Representative Democracy (Neblo, Esterling, and Lazer 2018). In it they give
                         recommendations for the design features that make for a good online town hall experience.
                     </Typography>
                 </Grid>
                 <Grid item>
-                    <Typography variant='body1' className={classes.paragraph}>
+                    <Typography variant='body1' fontSize='large'>
                         In particular, the online platform should reduce barriers to participating such that it is easy
                         for everyone in the community to participate interactively. The elected official should
                         participate in the session through streaming video and audio, so that the constituents can feel
