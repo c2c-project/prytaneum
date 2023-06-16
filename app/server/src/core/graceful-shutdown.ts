80af79b3fa4e74283705677f6744420f2d6705de
kadey001 <kadey001@ucr.edu>
2023-06-15 19:51:11 -0700

    chore(redis): updated to ioredis

--- app/server/src/core/graceful-shutdown.ts
+++ app/server/src/core/graceful-shutdown.ts
@@ -4,15 +4,14 @@ import { getRedisClient } from './utils';
 // TODO: make this much more robust.
 export function initGracefulShutdown(logger: FastifyLoggerInstance) {
     const cleanup = () => {
-        getRedisClient(logger).then((redis) => {
-            try {
-                redis.quit().then(() => {
-                    logger.info('Redis client closed.');
-                });
-            } catch (err) {
-                logger.error(err);
-            }
-        });
+        const redis = getRedisClient(logger);
+        try {
+            redis.quit().then(() => {
+                logger.info('Redis client closed.');
+            });
+        } catch (err) {
+            logger.error(err);
+        }
     };
 
     process.on('exit', cleanup);
