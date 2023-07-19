ef2db33c8dfab1ec5a6478b10de111ef4461e13b
kadey001 <kadey001@ucr.edu>
2023-07-18 18:42:17 -0700

    chore(providers): added providers + mui default theme

---
+++ app/cc2c/src/app/theme/themes.ts
@@ -0,0 +1,14 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+export const lightTheme = createTheme({
+    palette: {
+        mode: 'light',
+    },
+});
