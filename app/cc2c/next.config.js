996fbe6b8f0c47ae1f6b3a60e1b4746b11ea1ddd
kadey001 <kadey001@ucr.edu>
2023-07-18 19:45:11 -0700

    feat(auth): implemented next-auth login/logout

--- app/cc2c/next.config.js
+++ app/cc2c/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     experimental: {
         serverActions: true,
     },
+    env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
 };
 
 module.exports = nextConfig;
