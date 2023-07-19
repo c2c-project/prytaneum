74d411d5ba8415c217b07d707c93af9427f77e52
kadey001 <kadey001@ucr.edu>
2023-07-18 18:42:17 -0700

    chore(providers): added providers + mui default theme

---
+++ app/cc2c/src/components/Providers.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+import { darkTheme, lightTheme } from '@local/app/theme';
+import { ThemeProvider } from '@mui/material';
+import { SessionProvider } from 'next-auth/react';
+
+interface Props {
+    children: ReactNode;
+}
+
+export function Providers({ children }: Props) {
+    return (
+        <SessionProvider>
+            <ThemeProvider theme={darkTheme}>{children}</ThemeProvider>
+        </SessionProvider>
+    );
+}
+
+export default Providers;
