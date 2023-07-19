6d4211f70998d80de8decfcb560c16f811598941
kadey001 <kadey001@ucr.edu>
2023-07-18 19:45:11 -0700

    feat(auth): implemented next-auth login/logout

--- app/cc2c/src/app/page.tsx
+++ app/cc2c/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { AppBar } from '@local/components';
 import { Fragment } from 'react';
 import Landing from './landing';
 
 export default async function LandingPage() {
     return (
         <Fragment>
+            <AppBar />
             <Landing />
         </Fragment>
     );
