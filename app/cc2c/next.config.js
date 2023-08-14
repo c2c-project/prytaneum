1a66e9e545476cc89f20ddc0d1455a21f1806626
kadey001 <kadey001@ucr.edu>
2023-08-14 16:01:29 -0700

    chore(next config): added watch options

--- app/cc2c/next.config.js
+++ app/cc2c/next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
     env: {
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     },
+    webpack: (config) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+        return config;
+    },
 };
 
 module.exports = nextConfig;
