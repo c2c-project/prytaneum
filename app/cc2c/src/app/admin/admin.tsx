f6e95dcb408454b5be16516f8c2fd39ca5b815c5
kadey001 <kadey001@ucr.edu>
2023-08-16 16:28:16 -0700

    wip(admin): added admin page

---
+++ app/cc2c/src/app/admin/admin.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import { Grid } from '@mui/material';
+
+interface Props {}
+
+export function Admin({}: Props) {
+    return <Grid container>Admin</Grid>;
+}
