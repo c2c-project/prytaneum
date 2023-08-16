a287276a12306f7de7b6d77f4905b30aaf9b3de7
kadey001 <kadey001@ucr.edu>
2023-08-16 16:25:15 -0700

    feat(dashboard): added page + route protection

---
+++ app/cc2c/src/app/dashboard/dashboard.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+import { Grid } from '@mui/material';
+import { useProtectedRoute } from '@local/lib/useProtectedRoute';
+
+interface Props {}
+
+export function Dashboard({}: Props) {
+    return <Grid container>Dashboard</Grid>;
+}
