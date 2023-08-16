10c56d36ccf549e284ae668a446abf1034739b93
kadey001 <kadey001@ucr.edu>
2023-08-16 16:21:16 -0700

    feat(components): added buttons and updated appbar

--- app/cc2c/src/components/index.ts
+++ app/cc2c/src/components/index.ts
@@ -1,3 +1,6 @@
 export * from './AppBar';
 export * from './Providers';
 export * from './SignInButton';
+export * from './SignOutButton';
+export * from './DashboardButton';
+export * from './PersistentDrawer';
