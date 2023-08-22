38a488138f4f99515d1de5f3578953e33c669ad6
Kelton <kadey001@ucr.edu>
2023-08-22 03:22:28 -0700

    Revert "Connecting Classrooms to Congress Setup (#482)" (#483)
    This reverts commit 666ba0e3e55e4910ad90db9f4d7a0865419b76bf.

--- app/gcp-service/src/core/meta-routes.ts
+++
@@ -1,9 +0,0 @@
-import type { FastifyInstance } from 'fastify';
-
-export function setupMetaRoutes(server: FastifyInstance) {
-    server.log.debug('Setting up meta routes.');
-
-    // Routes for kubernetes health checks.
-    server.get('/', async () => ({ status: 'Healthy' }));
-    server.get('/healthz', async () => ({ status: 'Healthy' }));
-}
