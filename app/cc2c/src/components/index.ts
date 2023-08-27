3af886e5f14604854b3ed469fb06e6cbabc0cd34
kadey001 <kadey001@ucr.edu>
2023-08-27 00:04:56 -0700

    chore(components): removed generic upload file

--- app/cc2c/src/components/index.ts
+++ app/cc2c/src/components/index.ts
@@ -9,7 +9,6 @@ export * from './TemporaryDrawer';
 export * from './Providers';
 export * from './TablePaginationActions';
 export * from './PageLoading';
-export * from './UploadFile';
 export * from './upload/UploadPostWriting';
 export * from './upload/UploadPreWriting';
 export * from './upload/UploadRoster';
