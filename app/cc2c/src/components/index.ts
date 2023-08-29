cf172061d239e643416bd9e2f0ef847ceda9d3d0
kadey001 <kadey001@ucr.edu>
2023-08-28 17:27:33 -0700

    feat(components): added students list

--- app/cc2c/src/components/index.ts
+++ app/cc2c/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './admin/UsersTable';
 export * from './admin/ClassesTable';
+export * from './admin/AddTeacherFormModal';
 export * from './auth/SignInButton';
 export * from './auth/SignOutButton';
 export * from './auth/RegisterButton';
@@ -8,6 +9,7 @@ export * from './CreateClassForm';
 export * from './DashboardButton';
 export * from './TemporaryDrawer';
 export * from './Providers';
+export * from './StudentsList';
 export * from './TablePaginationActions';
 export * from './PageLoading';
 export * from './upload/UploadPostWriting';
