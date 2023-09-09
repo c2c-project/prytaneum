e2f5261640b4d711d04104620190a151b43ac08c
kadey001 <kadey001@ucr.edu>
2023-09-08 17:38:30 -0700

    chore(providers): added snackbar provider

--- app/cc2c/src/components/Providers.tsx
+++ app/cc2c/src/components/Providers.tsx
@@ -2,17 +2,38 @@
 
 import React, { ReactNode } from 'react';
 import { darkTheme, lightTheme } from '@local/app/theme';
-import { ThemeProvider } from '@mui/material';
+import { ThemeProvider, Button } from '@mui/material';
 import { SessionProvider } from 'next-auth/react';
+import { SnackbarProvider, ProviderContext } from 'notistack';
 
 interface Props {
     children: ReactNode;
 }
 
 export function Providers({ children }: Props) {
+    const notistackRef = React.useRef<ProviderContext | null>(null);
+
+    const onClickDismiss = (key: string | number) => () => {
+        notistackRef?.current?.closeSnackbar(key);
+    };
+
     return (
         <SessionProvider>
-            <ThemeProvider theme={darkTheme}>{children}</ThemeProvider>
+            <ThemeProvider theme={darkTheme}>
+                <SnackbarProvider
+                    ref={(ref) => {
+                        notistackRef.current = ref;
+                    }}
+                    action={(key) => (
+                        <Button color='inherit' onClick={onClickDismiss(key)}>
+                            Dismiss
+                        </Button>
+                    )}
+                    maxSnack={3}
+                >
+                    {children}
+                </SnackbarProvider>
+            </ThemeProvider>
         </SessionProvider>
     );
 }
