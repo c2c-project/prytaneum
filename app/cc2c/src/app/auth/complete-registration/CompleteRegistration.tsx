42401a5ab85fcfbaab47c809a7f8b12d1414e912
kadey001 <kadey001@ucr.edu>
2023-09-11 15:08:02 -0700

    chore(complete reg): updated routing and added text+padding

--- app/cc2c/src/app/auth/complete-registration/CompleteRegistration.tsx
+++ app/cc2c/src/app/auth/complete-registration/CompleteRegistration.tsx
@@ -18,19 +18,28 @@ export function CompleteRegistration({ token }: { token: string }) {
         const { isError, message } = await updatePasswordWithToken(formData);
 
         if (isError) setError(message);
+        else router.replace('/auth/signin');
         setLoading(false);
-        router.replace('/auth/signin');
     };
 
     return (
         <Grid container direction='column' alignItems='center' paddingTop='10rem'>
+            <Typography variant='h3'>Welcome to the Connecting Classrooms to Congress project website!</Typography>
+            <Typography variant='h5'>
+                Please set your password here to finish creating your account. Your teacher will tell you when to use
+                the cc2c website, so once you set your password you can close your browser.
+            </Typography>
             <form action={handleCompleteRegistration}>
-                <input type='hidden' name='token' value={token} />
-                <TextField name='password' label='Password' type='password' required />
-                <TextField name='confirmPassword' label='Confirm Password' type='password' required />
-                <Button disabled={loading} type='submit'>
-                    Set Password
-                </Button>
+                <Grid container direction='column' alignItems='center' paddingTop='2rem'>
+                    <input type='hidden' name='token' value={token} />
+                    <TextField name='password' label='Password' type='password' required />
+                    <Grid item paddingTop='1rem' />
+                    <TextField name='confirmPassword' label='Confirm Password' type='password' required />
+                    <Grid item paddingTop='1rem' />
+                    <Button disabled={loading} type='submit'>
+                        Set Password
+                    </Button>
+                </Grid>
             </form>
             {error !== '' && <Typography color='error'>{error}</Typography>}
         </Grid>
