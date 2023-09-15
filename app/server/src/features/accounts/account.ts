c43ee96fbfc7f96ad7f98418821a6785da446b4e
kadey001 <kadey001@ucr.edu>
2023-09-14 19:31:40 -0700

    feat(prytaneum server): added generate invite token api route

--- app/server/src/features/accounts/account.ts
+++ app/server/src/features/accounts/account.ts
@@ -1,6 +1,8 @@
+import { fromGlobalId } from 'graphql-relay';
 import { getOrCreateServer } from '@local/core/server';
 import { getPrismaClient } from '@local/core/utils';
 import { register } from './methods';
+import { sign } from '@local/lib/jwt';
 
 const server = getOrCreateServer();
 
@@ -26,6 +28,35 @@ server.route({
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Methods', 'POST');
             res.status(500).send('Error creating account');
+        }
+    },
+});
+
+server.route({
+    method: 'POST',
+    url: '/api/generate-invite-token',
+    handler: async (req, res) => {
+        interface ExpectedBody {
+            email: string;
+            eventId: string;
+        }
+        const { email, eventId } = req.body as ExpectedBody;
+        const prisma = getPrismaClient(server.log);
+        try {
+            const user = await prisma.user.findFirst({ where: { email } });
+            if (!user) throw new Error('User not found');
+            const { id: globalEventId } = fromGlobalId(eventId);
+            const event = await prisma.event.findUnique({ where: { id: globalEventId } });
+            if (!event) throw new Error('Event not found');
+            const token = await sign({ email, eventId });
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'POST');
+            res.status(200).send(JSON.stringify({ token }));
+        } catch (error) {
+            server.log.error(error);
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'POST');
+            res.status(500).send('Error generating token');
         }
     },
 });
