f4fe089730443ba283732f69f6dcad027628917f
Kelton <kadey001@ucr.edu>
2023-09-20 15:01:10 -0700

    chore(redis): restored ioredis with password removed (#530)

--- app/server/src/core/graceful-shutdown.ts
+++ app/server/src/core/graceful-shutdown.ts
@@ -1,19 +1,19 @@
-// import type { FastifyLoggerInstance } from 'fastify';
-// import { getRedisClient } from './utils';
+import type { FastifyLoggerInstance } from 'fastify';
+import { getRedisClient } from './utils';
 
-// // TODO: make this much more robust.
-// export function initGracefulShutdown(logger: FastifyLoggerInstance) {
-//     const cleanup = () => {
-//         const redis = getRedisClient(logger);
-//         try {
-//             redis.quit().then(() => {
-//                 logger.info('Redis client closed.');
-//             });
-//         } catch (err) {
-//             logger.error(err);
-//         }
-//     };
+// TODO: make this much more robust.
+export function initGracefulShutdown(logger: FastifyLoggerInstance) {
+    const cleanup = () => {
+        const redis = getRedisClient(logger);
+        try {
+            redis.quit().then(() => {
+                logger.info('Redis client closed.');
+            });
+        } catch (err) {
+            logger.error(err);
+        }
+    };
 
-//     process.on('exit', cleanup);
-//     process.on('uncaughtException', cleanup);
-// }
+    process.on('exit', cleanup);
+    process.on('uncaughtException', cleanup);
+}
--- app/server/src/core/utils/index.ts
+++ app/server/src/core/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './load-schema';
 export * from './prisma';
+export * from './redis';
 export * from './jwt';
