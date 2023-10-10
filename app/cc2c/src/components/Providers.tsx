970a52ba328c090cbb6de79b3871262123d69338
kadey001 <kadey001@ucr.edu>
2023-10-10 16:43:27 -0700

    feat(theme switch): added theme switch for dark/light modes

--- app/cc2c/src/components/Providers.tsx
+++ app/cc2c/src/components/Providers.tsx
@@ -2,6 +2,7 @@
 
 import React, { ReactNode } from 'react';
 import { darkTheme, lightTheme } from '@local/app/theme';
+import type { Theme } from '@mui/material/styles';
 import { ThemeProvider, Button } from '@mui/material';
 import { SessionProvider } from 'next-auth/react';
 import { SnackbarProvider, ProviderContext } from 'notistack';
@@ -10,8 +11,28 @@ interface Props {
     children: ReactNode;
 }
 
+export function useThemeMode() {
+    const [theme, setTheme] = React.useState<Theme>(localStorage.getItem('theme') === 'dark' ? darkTheme : lightTheme);
+    const [darkMode, setDarkMode] = React.useState<boolean>(localStorage.getItem('theme') === 'dark');
+
+    const toggleTheme = () => {
+        if (darkMode) {
+            localStorage.setItem('theme', 'light');
+            setDarkMode(false);
+            setTheme(lightTheme);
+        } else {
+            localStorage.setItem('theme', 'dark');
+            setDarkMode(true);
+            setTheme(darkTheme);
+        }
+    };
+
+    return { darkMode, toggleTheme, theme };
+}
+
 export function Providers({ children }: Props) {
     const notistackRef = React.useRef<ProviderContext | null>(null);
+    const { theme } = useThemeMode();
 
     const onClickDismiss = (key: string | number) => () => {
         notistackRef?.current?.closeSnackbar(key);
@@ -19,7 +40,7 @@ export function Providers({ children }: Props) {
 
     return (
         <SessionProvider>
-            <ThemeProvider theme={darkTheme}>
+            <ThemeProvider theme={theme}>
                 <SnackbarProvider
                     ref={(ref) => {
                         notistackRef.current = ref;
