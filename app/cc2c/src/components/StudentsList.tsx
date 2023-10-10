9b471eb9084ef6e0ef869787be1b048724257eff
kadey001 <kadey001@ucr.edu>
2023-10-09 20:46:49 -0700

    feat(students writings): added download writings buttons

--- app/cc2c/src/components/StudentsList.tsx
+++ app/cc2c/src/components/StudentsList.tsx
@@ -11,30 +11,34 @@ import {
     Grid,
     DialogContent,
     Button,
+    IconButton,
     FormGroup,
     FormControlLabel,
     Checkbox,
 } from '@mui/material';
 import DoneIcon from '@mui/icons-material/Done';
+import DownloadIcon from '@mui/icons-material/Download';
 import PendingIcon from '@mui/icons-material/Pending';
 import { ResponsiveDialog, useResponsiveDialog } from './ResponsiveDialog';
 import { updateStudentData } from '@local/server';
 import { useSnack } from '@local/lib';
 
+type Student = {
+    userId: string;
+    preWriting: string;
+    postWriting: string;
+    user: {
+        studentId: string;
+        email: string;
+        firstName: string;
+        lastName: string;
+        researchProjectConsent: boolean;
+        shadowAccount: boolean;
+    };
+};
+
 export interface StudentsListProps {
-    students: {
-        userId: string;
-        preWriting: string;
-        postWriting: string;
-        user: {
-            studentId: string;
-            email: string;
-            firstName: string;
-            lastName: string;
-            researchProjectConsent: boolean;
-            shadowAccount: boolean;
-        };
-    }[];
+    students: Student[];
     classId: string;
 }
 
@@ -81,6 +85,51 @@ export function StudentsList({ students, classId }: StudentsListProps) {
         close();
     };
 
+    const handleDownloadWritings = (student: Student) => () => {
+        setIsLoading(true);
+        // const student = students.find((student) => student.userId === selectedStudent);
+        if (!student) {
+            displaySnack('Student not found', { variant: 'error' });
+            setIsLoading(false);
+            return;
+        }
+        if (student.preWriting === '' && student.postWriting === '') {
+            displaySnack('No writings to download', { variant: 'warning' });
+            setIsLoading(false);
+            return;
+        }
+        const csvContent =
+            'data:text/csv;charset=utf-8,' +
+            'Email,preWriting,postWriting\n' +
+            `${student?.user.email},${student?.preWriting},${student?.postWriting}`;
+
+        const encodedUri = encodeURI(csvContent);
+        const link = document.createElement('a');
+        link.setAttribute('href', encodedUri);
+        link.setAttribute('download', `${student.user.email}_writings.csv`);
+        document.body.appendChild(link);
+        link.click();
+        setIsLoading(false);
+    };
+
+    const handleDownloadAllWritings = () => {
+        setIsLoading(true);
+        const csvContent =
+            'data:text/csv;charset=utf-8,' +
+            'Email,preWriting,postWriting\n' +
+            students
+                .map((student) => `${student?.user.email},${student?.preWriting},${student?.postWriting}`)
+                .join('\n');
+
+        const encodedUri = encodeURI(csvContent);
+        const link = document.createElement('a');
+        link.setAttribute('href', encodedUri);
+        link.setAttribute('download', `students_writings.csv`);
+        document.body.appendChild(link);
+        link.click();
+        setIsLoading(false);
+    };
+
     return (
         <React.Fragment>
             <List>
@@ -101,9 +150,17 @@ export function StudentsList({ students, classId }: StudentsListProps) {
                             </Typography>
                             <ListItemText primary={student.postWriting === '' ? <PendingIcon /> : <DoneIcon />} />
                         </ListItemButton>
+                        <IconButton onClick={handleDownloadWritings(student)}>
+                            <DownloadIcon />
+                        </IconButton>
                     </ListItem>
                 ))}
             </List>
+            <Grid container justifyContent='center' paddingBottom={2}>
+                <Button variant='outlined' onClick={handleDownloadAllWritings}>
+                    Download All Writings
+                </Button>
+            </Grid>
             <ResponsiveDialog open={isOpen} onClose={handleDialogClose} fullWidth>
                 <DialogContent>
                     <form action={handleEditStudent}>
