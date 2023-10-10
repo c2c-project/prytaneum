ef80ee66ae391a2f240afd00a309bc872e8096c0
kadey001 <kadey001@ucr.edu>
2023-10-09 20:13:41 -0700

    feat(students list): added edit option and icons for writing status

--- app/cc2c/src/components/StudentsList.tsx
+++ app/cc2c/src/components/StudentsList.tsx
@@ -1,7 +1,25 @@
 'use client';
 
 import React from 'react';
-import { List, ListItem, ListItemText, ListItemButton, Avatar } from '@mui/material';
+import {
+    List,
+    ListItem,
+    ListItemText,
+    ListItemButton,
+    Avatar,
+    Typography,
+    Grid,
+    DialogContent,
+    Button,
+    FormGroup,
+    FormControlLabel,
+    Checkbox,
+} from '@mui/material';
+import DoneIcon from '@mui/icons-material/Done';
+import PendingIcon from '@mui/icons-material/Pending';
+import { ResponsiveDialog, useResponsiveDialog } from './ResponsiveDialog';
+import { updateStudentData } from '@local/server';
+import { useSnack } from '@local/lib';
 
 export interface StudentsListProps {
     students: {
@@ -17,11 +35,50 @@ export interface StudentsListProps {
             shadowAccount: boolean;
         };
     }[];
+    classId: string;
 }
 
-export function StudentsList({ students }: StudentsListProps) {
+export function StudentsList({ students, classId }: StudentsListProps) {
+    const [selectedStudent, setSelectedStudent] = React.useState('');
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [isOpen, open, close] = useResponsiveDialog();
+    const [resetPreWritingChecked, setResetPreWritingChecked] = React.useState(false);
+    const [resetPostWritingChecked, setResetPostWritingChecked] = React.useState(false);
+    const { displaySnack } = useSnack();
+
+    const handleResetPreWritingCheckedChange = () => {
+        setResetPreWritingChecked(!resetPreWritingChecked);
+    };
+
+    const handleResetPostWritingCheckedChange = () => {
+        setResetPostWritingChecked(!resetPostWritingChecked);
+    };
+
     const handleSelectStudent = (studentId: string) => () => {
-        console.log('handleSelectStudent', studentId);
+        setSelectedStudent(studentId);
+        open();
+    };
+
+    const handleEditStudent = async (formData: FormData) => {
+        setIsLoading(true);
+        const { isError, message } = await updateStudentData(formData, resetPreWritingChecked, resetPostWritingChecked);
+        if (isError) {
+            displaySnack(message, { variant: 'error' });
+        } else {
+            displaySnack('Student Updated', { variant: 'success' });
+        }
+        setResetPostWritingChecked(false);
+        setResetPreWritingChecked(false);
+        setSelectedStudent('');
+        close();
+        setIsLoading(false);
+    };
+
+    const handleDialogClose = () => {
+        setSelectedStudent('');
+        setResetPreWritingChecked(false);
+        setResetPostWritingChecked(false);
+        close();
     };
 
     return (
@@ -34,13 +91,60 @@ export function StudentsList({ students }: StudentsListProps) {
                             <div style={{ width: '1rem' }} />
                             <ListItemText primary={student.user.firstName + ' ' + student.user.lastName} />
                             <div style={{ width: '1rem' }} />
-                            <ListItemText primary={student.preWriting === '' ? 'PRE: X' : 'PRE: O'} />
+                            <Typography variant='body1' fontWeight='bold' paddingRight='1rem'>
+                                Pre:
+                            </Typography>
+                            <ListItemText primary={student.preWriting === '' ? <PendingIcon /> : <DoneIcon />} />
                             <div style={{ width: '1rem' }} />
-                            <ListItemText primary={student.postWriting === '' ? 'POST: X' : 'POST: O'} />
+                            <Typography variant='body1' fontWeight='bold' paddingRight='1rem'>
+                                Post:
+                            </Typography>
+                            <ListItemText primary={student.postWriting === '' ? <PendingIcon /> : <DoneIcon />} />
                         </ListItemButton>
                     </ListItem>
                 ))}
             </List>
+            <ResponsiveDialog open={isOpen} onClose={handleDialogClose} fullWidth>
+                <DialogContent>
+                    <form action={handleEditStudent}>
+                        <Typography variant='h4' align='center'>
+                            Edit Student
+                        </Typography>
+                        <input type='hidden' name='studentId' value={selectedStudent} />
+                        <input type='hidden' name='classId' value={classId} />
+                        <Grid container width='100%' direction='column' alignItems='center' spacing={2}>
+                            <FormGroup>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={resetPreWritingChecked}
+                                            onChange={handleResetPreWritingCheckedChange}
+                                        />
+                                    }
+                                    label='Reset Pre-Writing'
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={resetPostWritingChecked}
+                                            onChange={handleResetPostWritingCheckedChange}
+                                        />
+                                    }
+                                    label='Reset Post-Writing'
+                                />
+                            </FormGroup>
+                        </Grid>
+                        <Grid container paddingTop='1rem' justifyContent='flex-end'>
+                            <Button color='secondary' onClick={close}>
+                                Cancel
+                            </Button>
+                            <Button disabled={isLoading} type='submit' variant='contained' color='primary'>
+                                Update
+                            </Button>
+                        </Grid>
+                    </form>
+                </DialogContent>
+            </ResponsiveDialog>
         </React.Fragment>
     );
 }
