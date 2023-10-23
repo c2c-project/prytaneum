76b1d9e9cf93c2f75e44ed712d8d6396ff450005
kadey001 <kadey001@ucr.edu>
2023-10-23 00:11:41 -0700

    feat(teacher dashboard): display list of classes teacher is assigned to

--- app/cc2c/src/app/teacher/dashboard/page.tsx
+++ app/cc2c/src/app/teacher/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation';
 
 import { AppBar } from '@local/components';
 import { TeacherDashboard } from './TeacherDashboard';
-import { getClassByTeacherId } from './actions';
+import { getClassesByTeacherId } from './actions';
 import type { UserWithoutPass } from '@local/app/api/auth/types';
 import { authOptions } from '@local/app/api/auth/[...nextauth]/route';
 
@@ -12,13 +12,13 @@ export default async function DashboardPage() {
     const session = await getServerSession(authOptions);
     const user = session?.user as UserWithoutPass | undefined;
     if (!user) redirect('/');
-    const classId = await getClassByTeacherId(user.id);
+    const classes = await getClassesByTeacherId(user.id);
 
     return (
         <React.Fragment>
             <AppBar />
             {/* @ts-ignore - Server Component */}
-            <TeacherDashboard classId={classId} />
+            <TeacherDashboard classes={classes} />
         </React.Fragment>
     );
 }
