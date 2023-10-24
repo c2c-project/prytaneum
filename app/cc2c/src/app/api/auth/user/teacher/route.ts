52db97535aba3ce0fb0fc0d482cdd908fee55b40
kadey001 <kadey001@ucr.edu>
2023-10-24 04:30:30 -0700

    feat(teacher signup): added prytaneum account creation

--- app/cc2c/src/app/api/auth/user/teacher/route.ts
+++ app/cc2c/src/app/api/auth/user/teacher/route.ts
@@ -12,17 +12,31 @@ export async function POST(req: Request) {
             throw new ProtectedError({ userMessage: 'Invalid authorization code' });
         }
 
+        const lowerCaseEmail = body.email.toLowerCase().replace(/ /g, ''); // Remove spaces and make lowercase
+
         // If authorization code is valid, create teacher account
         const newUser = await prisma.user.create({
             data: {
                 firstName: body.firstName,
                 lastName: body.lastName,
-                email: body.email.toLowerCase(),
+                email: lowerCaseEmail,
                 password: await bcrypt.hash(body.password, 10),
                 shadowAccount: false,
                 role: 'TEACHER',
             },
         });
+
+        // Create prytaneum account for user as well
+        const response = await fetch(`${process.env.PRYTANEUM_URL}/api/create-account`, {
+            method: 'POST',
+            body: JSON.stringify({ email: lowerCaseEmail, firstName: body.firstName, lastName: body.lastName }),
+        });
+
+        if (response.ok) console.log(`Successfully created Prytaneum account: ${lowerCaseEmail}`);
+        else {
+            console.error(response);
+            console.error(`Error creating Prytaneum account: ${lowerCaseEmail}`);
+        }
 
         const { password, ...userWithoutPass } = newUser;
         return userWithoutPass;
