2d109581e53d7bf75206ace0e048083218851151
kadey001 <kadey001@ucr.edu>
2023-10-25 16:15:43 -0700

    chore(dashboard): added aditional check in case auth token has stale role

--- app/cc2c/src/app/dashboard/page.tsx
+++ app/cc2c/src/app/dashboard/page.tsx
@@ -1,9 +1,19 @@
 import { Fragment } from 'react';
+import { getServerSession } from 'next-auth';
 
 import { AppBar } from '@local/components';
 import { StudentDashboard } from './StudentDashboard';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { UserWithoutPass } from '../api/auth/types';
+import { redirect } from 'next/navigation';
 
 export default async function DashboardPage() {
+    const session = await getServerSession(authOptions);
+    const user = session?.user as UserWithoutPass | undefined;
+    if (!user) redirect('/');
+    if (user.role === 'TEACHER') redirect(`/teacher/dashboard`);
+    if (user.role === 'ADMIN') redirect(`/admin/dashboard`);
+
     return (
         <Fragment>
             <AppBar />
