40a1e1fc0c5bb9d9cb8d48e13aa70dee732cf6eb
kadey001 <kadey001@ucr.edu>
2024-02-02 18:51:18 -0800

    feat(feedback prompt form): added multiple choice option

--- app/client/src/features/events/LiveFeedbackPrompts/LiveFeedbackPrompt/LiveFeedbackPromptForm.tsx
+++ app/client/src/features/events/LiveFeedbackPrompts/LiveFeedbackPrompt/LiveFeedbackPromptForm.tsx
@@ -7,9 +7,13 @@ import { FormContent } from '@local/components/FormContent';
 import { FormActions } from '@local/components/FormActions';
 import { useForm } from '@local/core';
 import Grid from '@mui/material/Grid';
-import { FEEDBACK_PROMPT_MAX_LENGTH } from '@local/utils/rules';
+import { FEEDBACK_PROMPT_MAX_LENGTH, CHOICE_MAX_LENGTH } from '@local/utils/rules';
 
-export type TLiveFeedbackPromptFormState = { prompt: string; feedbackType: string };
+export type TLiveFeedbackPromptFormState = {
+    prompt: string;
+    feedbackType: string;
+    choices: string[];
+};
 
 export interface LiveFeedbackPromptFormProps {
     onSubmit?: (state: TLiveFeedbackPromptFormState) => void;
@@ -17,13 +21,30 @@ export interface LiveFeedbackPromptFormProps {
 }
 
 export function LiveFeedbackPromptForm({ onSubmit, onCancel }: LiveFeedbackPromptFormProps) {
+    const [choices, setChoices] = React.useState<string[]>(new Array(3).fill('', 0, 3));
     // form related hooks
     const [form, errors, handleSubmit, handleChange] = useForm({
         prompt: '',
         feedbackType: 'open-ended',
+        choices: choices,
     });
 
+    const handleMultipleChoiceChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newChoices = [...choices];
+        newChoices[index] = event.target.value;
+        setChoices(newChoices);
+        form.choices = newChoices;
+    };
+
     const isFeedbackPromptValid = React.useMemo(() => form.prompt.trim().length !== 0, [form]);
+    const isMultipleChoiceOptionsValid = React.useMemo(
+        () => choices.every((choice) => choice.trim().length !== 0 && choice.length <= CHOICE_MAX_LENGTH),
+        [choices]
+    );
+    const isMultipleChoiceOptionValid = React.useMemo(
+        () => (index: number) => choices[index].length <= CHOICE_MAX_LENGTH,
+        [choices]
+    );
 
     return (
         <Form onSubmit={handleSubmit(onSubmit)}>
@@ -40,6 +61,7 @@ export function LiveFeedbackPromptForm({ onSubmit, onCancel }: LiveFeedbackPromp
                     >
                         <FormControlLabel value='open-ended' control={<Radio />} label='Open Ended' />
                         <FormControlLabel value='vote' control={<Radio />} label='Vote' />
+                        <FormControlLabel value='multiple-choice' control={<Radio />} label='Multiple Choice' />
                     </RadioGroup>
                 </Grid>
                 <TextField
@@ -61,6 +83,31 @@ export function LiveFeedbackPromptForm({ onSubmit, onCancel }: LiveFeedbackPromp
                 >
                     {form.prompt.length}/500
                 </Typography>
+                {form.feedbackType === 'multiple-choice' && (
+                    <React.Fragment>
+                        {choices.map((choice, index) => (
+                            <React.Fragment key={`choice-${index}`}>
+                                <TextField
+                                    key={`choice-${index}`}
+                                    id={`choice-${index}`}
+                                    name={`feedback-multiple-choice-${index}`}
+                                    label={`Choice ${index + 1}`}
+                                    error={!isMultipleChoiceOptionValid(index)}
+                                    required
+                                    value={choice}
+                                    onChange={handleMultipleChoiceChange(index)}
+                                />
+                                <Typography
+                                    variant='caption'
+                                    color={choice.length > CHOICE_MAX_LENGTH ? 'red' : 'black'}
+                                    sx={{ display: 'block', textAlign: 'right' }}
+                                >
+                                    {choice.length}/{CHOICE_MAX_LENGTH}
+                                </Typography>
+                            </React.Fragment>
+                        ))}
+                    </React.Fragment>
+                )}
             </FormContent>
             <FormActions disableGrow gridProps={{ justifyContent: 'flex-end' }}>
                 {onCancel && (
@@ -68,7 +115,12 @@ export function LiveFeedbackPromptForm({ onSubmit, onCancel }: LiveFeedbackPromp
                         Cancel
                     </Button>
                 )}
-                <Button disabled={!isFeedbackPromptValid} type='submit' variant='contained' color='primary'>
+                <Button
+                    disabled={!isFeedbackPromptValid || !isMultipleChoiceOptionsValid}
+                    type='submit'
+                    variant='contained'
+                    color='primary'
+                >
                     Create
                 </Button>
             </FormActions>
