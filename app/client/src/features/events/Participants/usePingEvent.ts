5dec85ab4cab57b1aea011865565711063046e5a
kadey001 <kadey001@ucr.edu>
2024-03-18 18:16:25 -0700

    feat(ping): added pause/resume callbacks

--- app/client/src/features/events/Participants/usePingEvent.ts
+++ app/client/src/features/events/Participants/usePingEvent.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useMutation, graphql } from 'react-relay';
 
 import type { usePingEventMutation } from '@local/__generated__/usePingEventMutation.graphql';
@@ -15,7 +15,15 @@ export const PING_EVENT_MUTATION = graphql`
 // Pings the server every 20 seconds to keep the participant active in the event participants list
 export function usePingEvent(eventId: string) {
     const [commit] = useMutation<usePingEventMutation>(PING_EVENT_MUTATION);
+    const [pingPaused, setPingPaused] = React.useState(false);
     const PING_INTERVAL = 20000; // 20 seconds
+
+    const pausePingEvent = React.useCallback(() => {
+        setPingPaused(true);
+    }, [setPingPaused]);
+    const resumePingEvent = React.useCallback(() => {
+        setPingPaused(false);
+    }, [setPingPaused]);
 
     useEffect(() => {
         commit({
@@ -23,6 +31,7 @@ export function usePingEvent(eventId: string) {
         });
 
         const pingInterval = setInterval(() => {
+            if (pingPaused) return;
             commit({
                 variables: { eventId },
             });
@@ -30,7 +39,7 @@ export function usePingEvent(eventId: string) {
 
         return () => clearInterval(pingInterval);
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [pingPaused, eventId, commit]);
 
-    return { pingEvent: commit };
+    return { pingEvent: commit, pausePingEvent, resumePingEvent };
 }
