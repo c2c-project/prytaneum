fdc8a6ff6a9e8e1be9ea57bc000fadfa1a2875c3
kadey001 <kadey001@ucr.edu>
2024-08-11 16:55:44 -0700

    fix(redis start script): added custom script for ci to fix github actions errors

---
+++ redis/start-redis-ci.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+export REDIS_HOST=redis-server
+export REDIS_PORT=6379
+export REDIS_USERNAME=default
+export REDIS_PASSWORD=redispassword
+
+docker compose -f redis/docker-compose.yml up -d --remove-orphans
