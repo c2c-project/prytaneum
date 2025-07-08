b5ae6cf012e99ca62c0bfb4e6f3237e526222ad1
btrev003 <bentreves4@gmail.com>
2025-07-08 03:34:32 -0700

    Quick fix addressing overly broad topic classification (#764)

--- app/moderation-algo/AlgoStages/classifyQuestion.py
+++ app/moderation-algo/AlgoStages/classifyQuestion.py
@@ -4,13 +4,14 @@ import GoogleGemini as gemini
 
 def DoesQuestionFitCategory(question: str, category: str, description: str) -> list:
     "Use Google Gemini to see if the question fits the given category or not"
-    prompt = 'Does the following comment pertain to the given topic?'
-    prompt += 'Provide your answer as either "yes" or "no".\n'
+    prompt = ''
     if(len(description) > 0):
-        prompt += f'Topic: "{category}", which means: {description}\n'
-    else:
-        prompt += f'Topic: "{category}"\n'
-    prompt += f'Comment: "{question}"'
+        prompt += f'"{category}" is defined as: "{description}".\n\n'
+    prompt += f'Is the following text discussing "{category}"? '
+    prompt += 'Provide your answer as either "yes" or "no".'
+    prompt += '\n\n'
+    prompt += f'The text is: "{question}"'
+
     response = gemini.AskGoogleGemini(prompt)
     if('yes' in response.lower()):
         return True
