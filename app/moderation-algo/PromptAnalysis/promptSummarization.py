1674cbdfbc32634f4a78818fc46e948a6c1d2cce
btrev003 <bentreves4@gmail.com>
2025-07-08 14:58:16 -0700

    Improved behavior of prompt comment summarizations

--- app/moderation-algo/PromptAnalysis/promptSummarization.py
+++ app/moderation-algo/PromptAnalysis/promptSummarization.py
@@ -1,38 +1,50 @@
 import sys
 sys.path.append('../')
 import GoogleGemini as gemini
+from AlgoStages.substantiveness import IsSubstantive
 
-def GetPostViewpoint(topic: str, post: str, subtopic='', force=False) -> list[str]:
-    "Return the viewpoint of the given post on the given topic and optionally the given subtopic"
-    prompt = 'Rephrase the following statement concisely:\n'
-    prompt += f'"{post}"\n'
-
-    response = gemini.AskGoogleGemini(prompt, force=force)
-    return response
-
-# TODO: DELETE THIS remove anything with fixNum
-def SummarizePosts(topic: str, posts: list, subtopic='', force=False) -> list[str]:
+def SummarizePosts(topic: str, posts: list, substantive=False, subtopic='', force=False) -> list[str]:
     "Return a list of the viewpoints discussed by the given posts on the given topic, or its subtopic if provided"
-    # If 5 or less posts are provided simply return reworded versions of them
-    if(len(posts) <= 5):
-        viewpoints = []
+    if(len(posts) < 1):
+        return []
+    
+    if(substantive):
+        posts_ = []
         for post in posts:
-            viewpoint = GetPostViewpoint(topic, post, subtopic, force)
-            viewpoints.append(viewpoint)
-        return viewpoints
+            if(IsSubstantive(post)):
+                posts_.append(post)
+        posts = posts_
 
-    if(len(subtopic) > 0):
-        prompt = f'Summarize the viewpoints on "{subtopic}", within the broad topic of "{topic}",'
+    prompt = ''
+
+    # if(len(subtopic) > 0):
+    #     prompt += f'Summarize the viewpoints on "{subtopic}", within the broad topic of "{topic}", '
+    # else:
+    #     prompt += f'Summarize the viewpoints on "{topic}" '
+    
+    if(len(posts) == 1):
+        prompt += f'Summarize the following statements as exactly one concise '
     else:
-        prompt = f'Summarize the viewpoints on "{topic}" '
-    prompt += 'in the following statements in one to five concise sentences. '
-    prompt += 'Start each sentence on a new line with a dash. '
+        prompt += f'Summarize the following statements as only one to five concise '
+
+    if(len(posts) == 1):
+        prompt += 'single summary. Your summary should be worded as passive tone viewpoints, so that it captures '
+    else:
+        prompt += 'summaries. Your summaries should be worded as passive tone viewpoints, so that they capture '
+    prompt += 'as many viewpoints presented in the given statements as possible. '
+    prompt += 'Use as few summaries as possible to achieve this. '
+
+    if(len(posts) == 1):
+        prompt += 'Start the summary on a new line with a dash. '
+    else:
+        prompt += 'Start each summary on a new line with a dash. '
+
     prompt += 'The statements are:\n'
     for i, post in enumerate(posts):
         prompt += f'Statement {i+1}: "{post}"\n'
-    prompt += 'The viewpoints are:\n'
+        
+    prompt += 'Your summarized viewpoints:\n'
 
-    # Get response and change it to list format
     response = gemini.AskGoogleGemini(prompt, force=force)
     response = response.replace('\n', '')
     response = response.split('- ')
