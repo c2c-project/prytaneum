dae308bb0cd0a709eacdd19e8c4a0d2f76d033ef
btrev003 <bentreves4@gmail.com>
2025-07-09 15:50:35 -0700

    Updated Google Gemini model

--- app/moderation-algo/GoogleGemini.py
+++ app/moderation-algo/GoogleGemini.py
@@ -1,41 +1,54 @@
 import os
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 import hashlib
 import json
 
-def AskGoogleGemini(prompt: str, model='gemini-1.5-pro-002', max_output_tokens=1024, force=False, temperature=0.2, top_k=40) -> str:
+def AskGoogleGemini(prompt: str, model='gemini-2.0-flash-001', max_output_tokens=1024, force=False, temperature=0.2, top_k=40) -> str:
     "Ask a prompt to given Google Cloud model and return the response text and safety ratings."
-    model = genai.GenerativeModel(model)
+    # Get cache folder path of desired model and create one if it does not already exist
+    folder = os.path.dirname(os.path.abspath(__file__)) + '/' # Folder of this script
+    cachefolder = folder + 'GooglegeminiCache/' + model.replace('.', '_') + '/' # Folder names cannot have periods
+    os.makedirs(cachefolder, exist_ok=True)
+
+    # Construct non-limiting safety filters
+    safety_settings = []
+    for category in HarmCategory:
+        safety_settings.append({
+            "category": category,
+            "threshold": HarmBlockThreshold.BLOCK_NONE,
+        })
+    
+    safety_settings=[
+            {
+                "category": HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                "threshold": HarmBlockThreshold.BLOCK_NONE,
+            },
+            {
+                "category": HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                "threshold": HarmBlockThreshold.BLOCK_NONE,
+            },
+            {
+                "category": HarmCategory.HARM_CATEGORY_HARASSMENT,
+                "threshold": HarmBlockThreshold.BLOCK_NONE,
+            },
+            {
+                "category": HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                "threshold": HarmBlockThreshold.BLOCK_NONE,
+            },
+        ]
+
+    # Fetch the requested model with no safety filters
+    model = genai.GenerativeModel(model, safety_settings=safety_settings)
     
     # Check if the prompt has been executed before
-    folder = os.path.dirname(os.path.abspath(__file__)) + '/' # Folder of this script
     response = ''
     hashedPrompt = str(hashlib.md5(prompt.encode('utf-8')).hexdigest()[:8])
-    filepath = folder + 'GooglegeminiCache/' + hashedPrompt
+    filepath = cachefolder + hashedPrompt
     if(os.path.isfile(filepath)):
         with open(filepath, 'r') as f:
             response = f.read()
-        
-    # Set the safety filter to not block anything
-    safety_settings=[
-            {
-                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                "threshold": "BLOCK_NONE"
-            },
-            {
-                "category": "HARM_CATEGORY_HATE_SPEECH",
-                "threshold": "BLOCK_NONE"
-            },
-            {
-                "category": "HARM_CATEGORY_HARASSMENT",
-                "threshold": "BLOCK_NONE"
-            },
-            {
-                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                "threshold": "BLOCK_NONE"
-            },
-        ]
-    
+
     # If force is True, always get a new response from Gemini
     if(force):
         response = ''
@@ -45,11 +58,10 @@ def AskGoogleGemini(prompt: str, model='gemini-1.5-pro-002', max_output_tokens=1
         completion = model.generate_content(
             prompt,
             generation_config=genai.types.GenerationConfig(
-                max_output_tokens=max_output_tokens,
+                #max_output_tokens=max_output_tokens,
                 temperature=temperature, # Randomness: Low temp = low randomness, high temp = high creativity
                 top_k=top_k,
             ),
-            safety_settings=safety_settings
         )
         response = completion.text
         if(response is None):
